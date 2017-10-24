ab850ccca528ca2a5ab733329564763a168430cb
Clement Leger <clement.leger@kalray.eu>
2017-10-24 16:46:12 +0200

    k1c: first port for compilation and link only
    Summary:
    This commit add k1c architecture which allows to build and link a
    kernel. This kernel is of course non runnable yet.
    The default configuration is non-SMP, and non-MMU enable.
    
    Ref T4550
    
    Test Plan:
    Run Using ISS for Coolidge
    
    To compile:
    ```
    make ARCH=k1c default_defconfig
    make ARCH=k1c
    ```
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Maniphest Tasks: T4550
    
    Differential Revision: https://phab.kalray.eu/D46

---
+++ arch/k1c/include/asm/string.h
@@ -0,0 +1,18 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2017 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_STRING_H
+#define _ASM_K1C_STRING_H
+
+#define __HAVE_ARCH_MEMSET
+extern void *memset(void *s, int c, size_t n);
+
+#define __HAVE_ARCH_MEMCPY
+extern void *memcpy(void *dest, const void *src, size_t n);
+
+#endif	/* _ASM_K1C_STRING_H */
