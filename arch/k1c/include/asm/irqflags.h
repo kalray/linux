60207517bd62d4f0f7069227e07b1c448dbe6c75
Clement Leger <clement.leger@kalray.eu>
2017-10-24 16:46:12 +0200

    k1c: first port for compilation and link only
    Summary:
    This commit add k1c architecture which allows to build and link a
    kernel. This kernel is of course non runnable yet.
    The default configuration is non-SMP, and non-MMU enable.
    
    Ref T4550
    
    Test Plan:
    Run Using ISS for Coolidge
    
    To compile:
    ```
    make ARCH=k1c default_defconfig
    make ARCH=k1c
    ```
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Maniphest Tasks: T4550
    
    Differential Revision: https://phab.kalray.eu/D46

---
+++ arch/k1c/include/asm/irqflags.h
@@ -0,0 +1,60 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2017 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_IRQFLAGS_H
+#define _ASM_K1C_IRQFLAGS_H
+
+#include <linux/types.h>
+
+#include <asm/sfr.h>
+
+static inline notrace unsigned long arch_local_save_flags(void)
+{
+	return k1c_sfr_get(K1C_SFR_PS) & (1 << K1C_SHIFT_PS_IE);
+}
+
+static inline notrace unsigned long arch_local_irq_save(void)
+{
+	unsigned long flags = arch_local_save_flags();
+
+	k1c_sfr_clear_bit(K1C_SFR_PS, K1C_SHIFT_PS_IE);
+
+	return flags;
+}
+
+static inline notrace void arch_local_irq_restore(unsigned long flags)
+{
+	/* If flags are set, interrupt are enabled), set the IE bit */
+	if (flags)
+		k1c_sfr_set_bit(K1C_SFR_PS, K1C_SHIFT_PS_IE);
+	else
+		k1c_sfr_clear_bit(K1C_SFR_PS, K1C_SHIFT_PS_IE);
+}
+
+static inline notrace void arch_local_irq_enable(void)
+{
+	k1c_sfr_set_bit(K1C_SFR_PS, K1C_SHIFT_PS_IE);
+}
+
+static inline notrace void arch_local_irq_disable(void)
+{
+	k1c_sfr_clear_bit(K1C_SFR_PS, K1C_SHIFT_PS_IE);
+}
+
+static inline notrace bool arch_irqs_disabled_flags(unsigned long flags)
+{
+	return (flags & (1 << K1C_SHIFT_PS_IE)) == 0;
+}
+
+static inline notrace bool arch_irqs_disabled(void)
+{
+	return arch_irqs_disabled_flags(k1c_sfr_get(K1C_SFR_PS));
+}
+
+
+#endif	/* _ASM_K1C_IRQFLAGS_H */
---
+++ arch/k1c/include/asm/page.h
@@ -0,0 +1,19 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2017 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_PAGE_H
+#define _ASM_K1C_PAGE_H
+
+#define EXCEPTION_STRIDE	0x400
+#define EXCEPTION_ALIGNEMENT	0x1000
+
+#define LOAD_ADDR		(CONFIG_KERNEL_RAM_BASE_ADDRESS)
+
+#include <asm-generic/page.h>
+
+#endif	/* _ASM_K1C_PAGE_H */
