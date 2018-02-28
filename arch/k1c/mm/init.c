ef80853c639bffd7d9f44dbeba343ea9cdb0ce64
Clement Leger <clement.leger@kalray.eu>
2018-02-28 09:52:36 +0100

    k1c: add support to handle initrd
    Summary:
    In order to start a simple init, we must have a initramfs.
    
    Ref T5430
    
    Test Plan: Run linux with an initramfs and see the huge failure when starting init
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Maniphest Tasks: T5430
    
    Differential Revision: https://phab.kalray.eu/D67

--- arch/k1c/mm/init.c
+++ arch/k1c/mm/init.c
@@ -89,6 +89,13 @@ void __init mem_init(void)
 	mem_init_print_info(NULL);
 }
 
+#ifdef CONFIG_BLK_DEV_INITRD
+void __init free_initrd_mem(unsigned long start, unsigned long end)
+{
+	free_reserved_area((void *)start, (void *)end, -1, "initrd");
+}
+#endif
+
 void free_initmem(void)
 {
 #ifdef CONFIG_POISON_INITMEM
