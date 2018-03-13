d58c8b9d512fc5792fc54db069ccd682ad0ce110
Clement Leger <clement.leger@kalray.eu>
2018-03-13 16:23:01 +0100

    k1c: use store octuple instead of store quadruple
    Summary:
    For faster accesses, use store/load octuple (256bit)
    Moreover, add dummy members to structures for faster load/store.
    
    Ref T5337, T5085
    
    Depends on D77
    
    Test Plan: Run linux and check that every thing is still running !
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Maniphest Tasks: T5337, T5085
    
    Differential Revision: https://phab.kalray.eu/D78

--- arch/k1c/kernel/asm-offsets.c
+++ arch/k1c/kernel/asm-offsets.c
@@ -23,47 +23,32 @@ int foo(void)
 	 */
 	DEFINE(PT_SIZE_ON_STACK, sizeof(struct pt_regs));
 
-	OFFSET(PT_R0R1, pt_regs, r0);
-	OFFSET(PT_R2R3, pt_regs, r2);
-	OFFSET(PT_R4R5, pt_regs, r4);
-	OFFSET(PT_R6R7, pt_regs, r6);
-	OFFSET(PT_R8R9, pt_regs, r8);
-	OFFSET(PT_R10R11, pt_regs, r10);
-	OFFSET(PT_R12R13, pt_regs, r12);
 	/* When restoring registers, we do not want to restore r12
 	 * right now since this is our stack pointer. Allow to save
 	 * only $r13 by using this offset
 	 */
 	OFFSET(PT_R13, pt_regs, r13);
 	OFFSET(PT_R14R15, pt_regs, r14);
-	OFFSET(PT_R16R17, pt_regs, r16);
-	OFFSET(PT_R18R19, pt_regs, r18);
-	OFFSET(PT_R20R21, pt_regs, r20);
-	OFFSET(PT_R22R23, pt_regs, r22);
-	OFFSET(PT_R24R25, pt_regs, r24);
-	OFFSET(PT_R26R27, pt_regs, r26);
-	OFFSET(PT_R28R29, pt_regs, r28);
-	OFFSET(PT_R30R31, pt_regs, r30);
-	OFFSET(PT_R32R33, pt_regs, r32);
-	OFFSET(PT_R34R35, pt_regs, r34);
-	OFFSET(PT_R36R37, pt_regs, r36);
-	OFFSET(PT_R38R39, pt_regs, r38);
-	OFFSET(PT_R40R41, pt_regs, r40);
-	OFFSET(PT_R42R43, pt_regs, r42);
-	OFFSET(PT_R44R45, pt_regs, r44);
-	OFFSET(PT_R46R47, pt_regs, r46);
-	OFFSET(PT_R48R49, pt_regs, r48);
-	OFFSET(PT_R50R51, pt_regs, r50);
-	OFFSET(PT_R52R53, pt_regs, r52);
-	OFFSET(PT_R54R55, pt_regs, r54);
-	OFFSET(PT_R56R57, pt_regs, r56);
-	OFFSET(PT_R58R59, pt_regs, r58);
-	OFFSET(PT_R60R61, pt_regs, r60);
-	OFFSET(PT_R62R63, pt_regs, r62);
-	OFFSET(PT_SPCSPS, pt_regs, spc);
-	OFFSET(PT_CSRA, pt_regs, cs);
-	OFFSET(PT_LCLE, pt_regs, lc);
-	OFFSET(PT_LS, pt_regs, ls);
+
+	/* Quad description */
+	OFFSET(PT_Q0, pt_regs, r0);
+	OFFSET(PT_Q4, pt_regs, r4);
+	OFFSET(PT_Q8, pt_regs, r8);
+	OFFSET(PT_Q12, pt_regs, r12);
+	OFFSET(PT_Q16, pt_regs, r16);
+	OFFSET(PT_Q20, pt_regs, r20);
+	OFFSET(PT_Q24, pt_regs, r24);
+	OFFSET(PT_Q28, pt_regs, r28);
+	OFFSET(PT_Q32, pt_regs, r32);
+	OFFSET(PT_Q36, pt_regs, r36);
+	OFFSET(PT_Q40, pt_regs, r40);
+	OFFSET(PT_Q44, pt_regs, r44);
+	OFFSET(PT_Q48, pt_regs, r48);
+	OFFSET(PT_Q52, pt_regs, r52);
+	OFFSET(PT_Q56, pt_regs, r56);
+	OFFSET(PT_Q60, pt_regs, r60);
+	OFFSET(PT_SPC_SPS_CS_RA, pt_regs, spc);
+	OFFSET(PT_LC_LE_LS_DUMMY, pt_regs, lc);
 
 	/*
 	 * Flags in thread info
@@ -77,19 +62,13 @@ int foo(void)
 	OFFSET(TASK_THREAD_USER_SP, task_struct, thread.user_sp);
 
 	/*
-	 * Offsets to save registers in switch_to
+	 * Offsets to save registers in switch_to using quads
 	 */
-	OFFSET(TASK_THREAD_RA, task_struct, thread.ra);
-	OFFSET(TASK_THREAD_R10, task_struct, thread.r10);
-	OFFSET(TASK_THREAD_R15, task_struct, thread.r15);
-	OFFSET(TASK_THREAD_R16R17, task_struct, thread.r16);
-	OFFSET(TASK_THREAD_R18R19, task_struct, thread.r18);
-	OFFSET(TASK_THREAD_R20R21, task_struct, thread.r20);
-	OFFSET(TASK_THREAD_R22R23, task_struct, thread.r22);
-	OFFSET(TASK_THREAD_R24R25, task_struct, thread.r24);
-	OFFSET(TASK_THREAD_R26R27, task_struct, thread.r26);
-	OFFSET(TASK_THREAD_R28R29, task_struct, thread.r28);
-	OFFSET(TASK_THREAD_R30R31, task_struct, thread.r30);
+	OFFSET(TASK_THREAD_RA_KERNELSP_R10_R15, task_struct, thread.ra);
+	OFFSET(TASK_THREAD_Q16, task_struct, thread.r16);
+	OFFSET(TASK_THREAD_Q20, task_struct, thread.r20);
+	OFFSET(TASK_THREAD_Q24, task_struct, thread.r24);
+	OFFSET(TASK_THREAD_Q28, task_struct, thread.r28);
 
 	return 0;
 }
