fee32d0965e84c8da4f26ddad3ff57066a0f6d54
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-04-03 16:40:34 +0200

    k1c: Activate MMU and add stuff to be able to build the kernel
    Summary:
    This patch adds macros and functions for managing the 2 and 3
    level page tables, TLB and MMU. It also fixes some definitions to
    be able to build with MMU enabled. Most of functions are not implemented.
    
    Depends on D97
    
    Test Plan: Boot until VFS panic
    
    Reviewers: O51 linux, cleger
    
    Reviewed By: O51 linux, cleger
    
    Differential Revision: https://phab.kalray.eu/D98

---
+++ arch/k1c/include/asm/tlbflush.h
@@ -0,0 +1,35 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_TLBFLUSH_H
+#define _ASM_K1C_TLBFLUSH_H
+
+static inline void
+flush_tlb_mm(struct mm_struct *mm)
+{
+	panic("%s is not implemented yet\n", __func__);
+}
+
+static inline void flush_tlb_range(struct vm_area_struct *vma,
+		unsigned long start, unsigned long end)
+{
+	panic("%s is not implemented yet\n", __func__);
+}
+
+/* Flush a range of kernel pages */
+static inline void flush_tlb_kernel_range(unsigned long start,
+	unsigned long end)
+{
+	panic("%s is not implemented yet\n", __func__);
+}
+
+#define flush_tlb_all() panic("Unimplemented flush_tlb_all\n")
+#define flush_tlb_page(vma, addr) panic("Unimplemented flush_tlb_page\n")
+
+
+#endif /* _ASM_K1C_TLBFLUSH_H */
