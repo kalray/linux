daee7962072507e954b799a1622dd75311555f7d
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-04-03 16:40:34 +0200

    k1c: Activate MMU and add stuff to be able to build the kernel
    Summary:
    This patch adds macros and functions for managing the 2 and 3
    level page tables, TLB and MMU. It also fixes some definitions to
    be able to build with MMU enabled. Most of functions are not implemented.
    
    Depends on D97
    
    Test Plan: Boot until VFS panic
    
    Reviewers: O51 linux, cleger
    
    Reviewed By: O51 linux, cleger
    
    Differential Revision: https://phab.kalray.eu/D98

---
+++ arch/k1c/mm/ioremap.c
@@ -0,0 +1,43 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#include <linux/io.h>
+
+/*
+ * ioremap     -   map bus memory into CPU space
+ * @offset:    bus address of the memory
+ * @size:      size of the resource to map
+ *
+ * ioremap performs a platform specific sequence of operations to
+ * make bus memory CPU accessible via the readb/readw/readl/writeb/
+ * writew/writel functions and the other mmio helpers. The returned
+ * address is not guaranteed to be usable directly as a virtual
+ * address.
+ *
+ * Must be freed with iounmap.
+ */
+void __iomem *ioremap(phys_addr_t offset, unsigned long size)
+{
+
+	panic("%s is not implemented yet\n", __func__);
+	return NULL;
+}
+EXPORT_SYMBOL(ioremap);
+
+
+/**
+ * iounmap - Free a IO remapping
+ * @addr: virtual address from ioremap_*
+ *
+ * Caller must ensure there is only one unmapping for the same pointer.
+ */
+void iounmap(volatile void __iomem *addr)
+{
+	vunmap((void *)((unsigned long)addr & PAGE_MASK));
+}
+EXPORT_SYMBOL(iounmap);
