1728d981a772a17102e3a295cee998556d919c61
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-04-03 09:53:38 +0200

    k1c: update copyright and remove unneeded headers
    Summary:
    This patch updates copyright from 2017 to 2018 and remove headers that
    are only use generic ones. It also fixes some errors when running lint
    to be able to pass lint.
    
    Test Plan: Boot until not syncing VFS
    
    Reviewers: O51 linux!, cleger
    
    Differential Revision: https://phab.kalray.eu/D102

--- arch/k1c/include/asm/ptrace.h
+++ arch/k1c/include/asm/ptrace.h
@@ -3,7 +3,7 @@
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
- * Copyright (C) 2017 Kalray Inc.
+ * Copyright (C) 2018 Kalray Inc.
  */
 
 #ifndef _ASM_K1C_PTRACE_H
