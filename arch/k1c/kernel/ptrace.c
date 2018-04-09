b2f16993373e6d56faac8fbf3a68e48269af6084
Clement Leger <clement.leger@kalray.eu>
2018-04-09 16:49:01 +0200

    k1c: Implement syscall entry
    Summary:
    This commit implements the syscall entry assembly
    
    A documentation is included which described the coarse grained steps. In  assembly, there is much more comments to clearly explain what is done during the call.
    
    Ref T5691
    
    Test Plan: Wait for user space to be ready, run the code and check syscalls are working
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Maniphest Tasks: T5691
    
    Differential Revision: https://phab.kalray.eu/D89

--- arch/k1c/kernel/ptrace.c
+++ arch/k1c/kernel/ptrace.c
@@ -3,10 +3,19 @@
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
- * Copyright (C) 2017 Kalray Inc.
+ * Copyright 2010 Tilera Corporation. All Rights Reserved.
+ * Copyright 2015 Regents of the University of California
+ * Copyright 2017 SiFive
+ * Copyright (C) 2018 Kalray Inc.
+ *
+ * Partially copied from arch/riscv/kernel/ptrace.c
+ *
  */
 
 #include <linux/sched.h>
+#include <linux/audit.h>
+#include <linux/tracehook.h>
+#include <linux/thread_info.h>
 
 void ptrace_disable(struct task_struct *child)
 {
@@ -17,4 +26,28 @@ long arch_ptrace(struct task_struct *child, long request,
 		unsigned long addr, unsigned long data)
 {
 	return 0;
+}
+
+/*
+ * Allows PTRACE_SYSCALL to work.  These are called from entry.S in
+ * {handle,ret_from}_syscall.
+ */
+int do_syscall_trace_enter(struct pt_regs *regs, unsigned long syscall)
+{
+	int ret = 0;
+
+	if (test_thread_flag(TIF_SYSCALL_TRACE))
+		ret = tracehook_report_syscall_entry(regs);
+
+	audit_syscall_entry(syscall, regs->r0, regs->r1, regs->r2, regs->r3);
+
+	return ret;
+}
+
+void do_syscall_trace_exit(struct pt_regs *regs)
+{
+	if (test_thread_flag(TIF_SYSCALL_TRACE))
+		tracehook_report_syscall_exit(regs, 0);
+
+	audit_syscall_exit(regs);
 }
