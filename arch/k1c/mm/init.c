5a039bfb36130b3efb5672b05de9d3a1248f420d
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-04-12 17:35:25 +0200

    k1c: Add documentation and info about MMU management
    Summary:
    This patch updates documentation about how physical memory is
    managed by Linux and on the MPPA. It also details what are the
    possibilites of levels in page table according to the page size. Finally
    it adds some message during the boot to help the understanding of the
    memory allocation.
    
    Test Plan: Boot until panic on pgd_alloc not implemented
    
    Reviewers: O51 linux!, cleger
    
    Differential Revision: https://phab.kalray.eu/D121

--- arch/k1c/mm/init.c
+++ arch/k1c/mm/init.c
@@ -9,7 +9,6 @@
 /* Memblock header depends on types.h but does not include it ! */
 #include <linux/types.h>
 #include <linux/memblock.h>
-#include <linux/bootmem.h>
 #include <linux/of_fdt.h>
 #include <linux/sched.h>
 #include <linux/init.h>
@@ -75,12 +74,16 @@ static void __init setup_bootmem(void)
 		memory_start = region->base;
 		memory_end = memory_start + region->size;
 
-		pr_info("%s: Memory: 0x%lx-0x%lx\n", __func__,
-			(unsigned long)memory_start,
-			(unsigned long)memory_end);
-
 		/* Check that this memblock includes the kernel */
 		if (memory_start <= kernel_start && kernel_end <= memory_end) {
+
+			pr_info("%s: Memory  : 0x%lx - 0x%lx\n", __func__,
+				(unsigned long)memory_start,
+				(unsigned long)memory_end);
+			pr_info("%s: Reserved: 0x%lx - 0x%lx\n", __func__,
+				(unsigned long)kernel_start,
+				(unsigned long)kernel_end);
+
 			/* Reserve from the start to the end of the kernel. */
 			memblock_reserve(kernel_start,
 					 kernel_end - kernel_start);
@@ -113,8 +116,11 @@ void __init setup_arch_memory(void)
 
 void __init mem_init(void)
 {
-	free_all_bootmem();
+	unsigned long pr;
 
+	pr = memblock_free_all();
+	pr_info("%s: %lu (%lu Mo) pages released\n",
+		__func__, pr, (pr << PAGE_SHIFT) >> 20);
 	mem_init_print_info(NULL);
 }
 
