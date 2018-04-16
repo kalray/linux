ad2b43b3fd12d0b745385ff635f6c17e0a9ec064
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-04-16 14:58:03 +0200

    k1c: First implementation of pgd_alloc and do_page_fault
    Summary:
    This patch does the minimal to be able to create the memory space for
    the init process. In this first implementation we are using a 3 level
    page table with a 4KB page. The user memory space is 512GB.
    To achieve this this patch implements:
      - pte_alloc_one(), __pte_free_tlb()
      - pgd_free(), pgd_alloc()
      - pmd_free(), pmd_alloc()
      - pud_none(), pud_populate(), set_pud()
    It also does some cleanup of the swapper_pg_dir and fixes the value of
    pud returns when calling pud_page_vaddr(). Finally it fills the page
    table with PGD, PMD and PTE entry to be able to reach the do_page_fault()
    handler.
    
    Test Plan: Boot until: "Kernel panic - not syncing: do_page_fault: not yet implemented"
    
    Reviewers: O51 linux!, cleger
    
    Differential Revision: https://phab.kalray.eu/D123

--- arch/k1c/include/asm/traps.h
+++ arch/k1c/include/asm/traps.h
@@ -11,12 +11,33 @@
 
 #include <asm/sfr.h>
 
-#define TRAP_COUNT	0x10
+#define K1C_TRAP_RESET          0x0
+#define K1C_TRAP_OPCODE         0x1
+#define K1C_TRAP_PRIVILEGE      0x2
+#define K1C_TRAP_DMISALIGN      0x3
+#define K1C_TRAP_PSYSERROR      0x4
+#define K1C_TRAP_DSYSERROR      0x5
+#define K1C_TRAP_PDECCERROR     0x6
+#define K1C_TRAP_DDECCERROR     0x7
+#define K1C_TRAP_PPARERROR      0x8
+#define K1C_TRAP_DPARERROR      0x9
+#define K1C_TRAP_PSECERROR      0xA
+#define K1C_TRAP_DSECERROR      0xB
+#define K1C_TRAP_NOMAPPING      0xC
+#define K1C_TRAP_PROTECTION     0xD
+#define K1C_TRAP_WRITETOCLEAN   0xE
+#define K1C_TRAP_ATOMICTOCLEAN  0xF
+
+#define K1C_TRAP_COUNT          0x10
 
 typedef void (*trap_handler_func) (uint64_t es, uint64_t ea,
 				   struct pt_regs *regs);
 
 #define trap_cause(__es) \
 		((__es & K1C_SFR_ES_HTC_MASK) >> K1C_SFR_ES_HTC_SHIFT)
+
+#ifdef CONFIG_MMU
+extern void do_page_fault(uint64_t es, uint64_t ea, struct pt_regs *regs);
+#endif
 
 #endif
