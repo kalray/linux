b90df0a55720383fe1777dee79b8c4f8c12ab4d2
Clement Leger <clement.leger@kalray.eu>
2018-04-25 11:25:18 +0200

    k1c: add a panic handler to emit syscall
    Summary:
    In order to keep validation point in Linux, add a panic handler with an
    expected panic string. If the panic message matches the expected string,
    exit with 0. If not exit with 1.
    Moreover, set the current expected message to pgd_alloc not implemented
    yet. \n is already embedded into panic printed string so remove it from
    pgd_alloc panic message.
    
    Test Plan:
    Integration should be passed and once finish, remove the grep
    from build.rb to see it sill works.
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Differential Revision: https://phab.kalray.eu/D127

---
+++ arch/k1c/include/asm/syscall.h
@@ -0,0 +1,14 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_SYSCALL_H
+#define _ASM_K1C_SYSCALL_H
+
+void scall_machine_exit(unsigned char value);
+
+#endif
