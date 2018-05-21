8c8c068e0c6f29c28135c6d19e2254e66fa0d18a
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-05-21 11:33:50 +0200

    k1c: Start the implementation of the do_page_fault
    Summary:
    This patch implements the case where a PTE entry is present but the
    mapping is not present in the JTLB. We use A-Modified for the entry
    status to avoid to catch a trap WRITETOCLEAN. The trap WRITETOCLEAN is
    used to implement the copy-on-write and we will have to implement it at
    some point. Currently we just prevent to catch ATOMICTOCLEAN and
    WRITETOCLEAN traps to reach the return to user from kernel thread. Note
    that this patch also wraps the do_page_fault() in k1c_trap_nomapping() to
    have something coherant with all other traps like WRITETOCLEAN that we will
    immplement.
    
    Test Plan: Boot until starting init process that will shutdown the
    machine properly.
    
    Reviewers: O51 linux!, cleger
    
    Differential Revision: https://phab.kalray.eu/D170

--- arch/k1c/mm/trap.c
+++ arch/k1c/mm/trap.c
@@ -10,45 +10,84 @@
 #include <linux/kernel.h> // only needed to panic
 #include <linux/printk.h>
 #include <linux/sched.h>
+#include <linux/mm.h>
 
 #include <asm/ptrace.h>
 #include <asm/pgtable.h>
 #include <asm/sfr_defs.h>
 #include <asm/current.h>
+#include <asm/tlbflush.h>
 
-void do_page_fault(uint64_t es, uint64_t ea, struct pt_regs *regs)
+static void do_page_fault(uint64_t es, uint64_t ea, struct pt_regs *regs)
 {
 	struct task_struct *tsk;
-	unsigned int ec;
-
-	/* Check that the exception class (EC) from the exception syndrom
-	 * register (ES) is really an hardware trap.
-	 */
-	ec = es & K1C_SFR_ES_EC_MASK;
-	if (ec != 1) {
-		pr_info("%s: EC is not equal to 1 (%d)\n", __func__, ec);
-		return;
-	}
+	struct mm_struct *mm;
+	struct vm_area_struct *vma;
+	unsigned int flags;
+	pgd_t *pgd = NULL;
+	p4d_t *p4d = NULL;
+	pud_t *pud = NULL;
+	pmd_t *pmd = NULL;
+	pte_t *pte = NULL;
+	pgprot_t prot;
+	int fault;
 
 	tsk = current;
+	BUG_ON(unlikely(!tsk));
 
-	/*
-	 * Information will be removed when do_page_fault() will be
-	 * fully implemented.
-	 */
-	pr_info("%s: Got HW trap\n", __func__);
-	pr_info("%s: HTC: %llu\n", __func__,
-		(es & K1C_SFR_ES_HTC_MASK) >> K1C_SFR_ES_HTC_SHIFT);
-	pr_info("%s: RWX: %llu\n", __func__,
-		(es & K1C_SFR_ES_RWX_MASK) >> K1C_SFR_ES_RWX_SHIFT);
-	pr_info("%s: ea: 0x%llx\n", __func__, ea);
-	pr_info("%s: current: 0x%p\n", __func__, tsk);
+	mm = tsk->mm;
 
 	if (ea >= VMALLOC_START && ea <= VMALLOC_END && !user_mode(regs))
 		panic("%s: vmalloc is not yet implemented", __func__);
 
-	if (user_mode(regs))
-		panic("%s: page fault happened in user space", __func__);
+	/* As we are using 3 or 2 level page table we don't need to check
+	 * if pgd is valid. The value will be pass directly to p4d and then
+	 * to pud. That means that we only need to test the value at the pud
+	 * level. In fact p4d_offset and pud_offset are used as converters.
+	 */
+	pgd = pgd_offset(mm, ea);
+	p4d = p4d_offset(pgd, ea);
+	pud = pud_offset(p4d, ea);
+	if (pud_none(*pud))
+		goto do_something;
 
-	panic("%s is not implemented", __func__);
+	pmd = pmd_offset(pud, ea);
+	if (pmd_none(*pmd))
+		goto do_something;
+
+	pte = pte_offset_kernel(pmd, ea);
+	if (pte_present(*pte)) {
+		update_mmu_cache(NULL, ea, pte);
+		return;
+	}
+
+do_something:
+	vma = find_vma(mm, ea);
+	if (!vma)
+		BUG();
+
+	if (vma->vm_start <= ea)
+		goto good_area;
+
+	panic("%s needs more code", __func__);
+
+good_area:
+	flags = FAULT_FLAG_ALLOW_RETRY | FAULT_FLAG_KILLABLE;
+
+	prot = vm_get_page_prot(vma->vm_flags);
+	if (pgprot_val(prot) & _PAGE_WRITE)
+		flags |= FAULT_FLAG_WRITE;
+
+	fault = handle_mm_fault(vma, ea, flags);
+	/* TODO: check what must be done according to the value of fault */
+}
+
+void k1c_trap_nomapping(uint64_t es, uint64_t ea, struct pt_regs *regs)
+{
+	do_page_fault(es, ea, regs);
+}
+
+void k1c_trap_writetoclean(uint64_t es, uint64_t ea, struct pt_regs *regs)
+{
+	panic("%s not implemented", __func__);
 }
