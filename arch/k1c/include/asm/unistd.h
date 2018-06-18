f77db16ab9a3b98327f982f5951189f5217aaeb8
Clement Leger <clement.leger@kalray.eu>
2018-06-18 17:31:34 +0200

    k1c: Fix empty syscall table
    Summary:
    Due to missing unistd.h in asm, syscall table was empty during
    generation. In order to fix that, we add an unistd.h which also define
    that the arch want sys_clone. This header must not have incldue guard
    since it will be included multiple times !
    When instantiating all the syscalls, some of them were missing (mmap and
    rt_sigreturn). Stub them with panic to compile.
    
    Ref T6203
    
    Test Plan: Integrate and check that the integration is successfull
    
    Reviewers: O51 linux!
    
    Maniphest Tasks: T6203
    
    Differential Revision: https://phab.kalray.eu/D195

---
+++ arch/k1c/include/asm/unistd.h
@@ -0,0 +1,11 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#define __ARCH_WANT_SYS_CLONE
+
+#include <asm-generic/unistd.h>
