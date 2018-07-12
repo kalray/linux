086b3ae56812a40705747d1aeedd28008024bf5e
Guillaume Thouvenin <gthouvenin@kalray.eu>
2018-07-12 14:55:43 +0200

    k1c: Check write permission using VMA flags
    Summary:
    This patch modifies the check of the write permission because pgprot_val
    cannot be compared to _PAGE_WRITE and it seems more obvisous to use the
    flags of VMA directly instead of making a conversion that is not very
    understandable for now.
    
    Test Plan: Be able to run the new initramfs. Data area is now writeable.
    
    Reviewers: O51 linux!, cleger
    
    Differential Revision: https://phab.kalray.eu/D231

--- arch/k1c/mm/trap.c
+++ arch/k1c/mm/trap.c
@@ -29,7 +29,6 @@ static void do_page_fault(uint64_t es, uint64_t ea, struct pt_regs *regs)
 	pud_t *pud = NULL;
 	pmd_t *pmd = NULL;
 	pte_t *pte = NULL;
-	pgprot_t prot;
 	int fault;
 
 	tsk = current;
@@ -74,8 +73,7 @@ do_something:
 good_area:
 	flags = FAULT_FLAG_ALLOW_RETRY | FAULT_FLAG_KILLABLE;
 
-	prot = vm_get_page_prot(vma->vm_flags);
-	if (pgprot_val(prot) & _PAGE_WRITE)
+	if (vma->vm_flags & VM_WRITE)
 		flags |= FAULT_FLAG_WRITE;
 
 	fault = handle_mm_fault(vma, ea, flags);
