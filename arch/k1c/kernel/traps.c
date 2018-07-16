2673826b3db5020127b021287b9076819be2d2da
Clement Leger <clement.leger@kalray.eu>
2018-07-16 09:30:37 +0200

    k1c: Simplify trap handler by setting all traps
    Summary: Set all traps entries to avoid checking if trap was defined.
    
    Test Plan: Run in integration to check everything still works correctly
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Differential Revision: https://phab.kalray.eu/D225

--- arch/k1c/kernel/traps.c
+++ arch/k1c/kernel/traps.c
@@ -6,6 +6,7 @@
  * Copyright (C) 2018 Kalray Inc.
  */
 
+#include <linux/context_tracking.h>
 #include <linux/kallsyms.h>
 #include <linux/printk.h>
 #include <linux/sched.h>
@@ -47,7 +48,15 @@ static const char * const trap_name[] = {
 	"ATOMICTOCLEAN"
 };
 
-inline void register_trap_handler(unsigned int trap_nb, trap_handler_func fn)
+static void default_trap_handler(uint64_t es, uint64_t ea,
+				 struct pt_regs *regs)
+{
+	show_regs(regs);
+	panic("ERROR: TRAP %s received at 0x%.16llx\n",
+	      trap_name[trap_cause(es)], regs->spc);
+}
+
+static void register_trap_handler(unsigned int trap_nb, trap_handler_func fn)
 {
 
 	if (trap_nb >= K1C_TRAP_COUNT || fn == NULL)
@@ -58,7 +67,10 @@ inline void register_trap_handler(unsigned int trap_nb, trap_handler_func fn)
 
 void __init trap_init(void)
 {
+	int i;
 
+	for (i = 0; i < K1C_TRAP_COUNT; i++)
+		register_trap_handler(i, default_trap_handler);
 #ifdef CONFIG_MMU
 	register_trap_handler(K1C_TRAP_NOMAPPING, k1c_trap_nomapping);
 	register_trap_handler(K1C_TRAP_WRITETOCLEAN, k1c_trap_writetoclean);
@@ -153,13 +165,6 @@ void show_stack(struct task_struct *task, unsigned long *sp)
 	show_trace(stack);
 }
 
-static void default_trap_handler(uint64_t es, uint64_t ea,
-				 struct pt_regs *regs)
-{
-	show_regs(regs);
-	panic("ERROR: TRAP %s received at 0x%.16llx\n",
-	      trap_name[trap_cause(es)], regs->spc);
-}
 /**
  * Main trap handler called by the _trap_handler routine in trap_handler.S
  * This handler will redirect to other trap handlers if present
@@ -170,21 +175,14 @@ static void default_trap_handler(uint64_t es, uint64_t ea,
  */
 void trap_handler(uint64_t es, uint64_t ea, struct pt_regs *regs)
 {
-	trap_handler_func trap_func = NULL;
 	int htc = trap_cause(es);
+	trap_handler_func trap_func = trap_handler_table[htc];
 
 	/* Normal traps number should and must be between 0 and 15 included */
-	if (WARN_ON(htc >= K1C_TRAP_COUNT)) {
-		pr_err("Invalid trap number !\n");
+	if (unlikely(htc >= K1C_TRAP_COUNT)) {
+		pr_err("Invalid trap %d !\n", htc);
 		return;
 	}
 
-	/* call the specific trap handler if it exists */
-	trap_func = trap_handler_table[htc];
-	if (trap_func) {
-		trap_func(es, ea, regs);
-		return;
-	}
-
-	default_trap_handler(es, ea, regs);
+	trap_func(es, ea, regs);
 }
