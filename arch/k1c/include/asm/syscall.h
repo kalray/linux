600b14821b5a80b8ca66b21d99aae8b03ec0c414
Clement Leger <clement.leger@kalray.eu>
2018-08-16 16:33:32 +0200

    k1c: add signal handling
    Summary:
    In order to be ready for uClibc, implement signals to  correctly handle
    them. During this implementation, registers in pt_regs have been moved to
    accomodate user_regs. This struct contains only the needed user registers
    The documentation has been modified to explain carefully how everything
    works.
    
    Ref T6696
    
    Test Plan: Wait for signal to be ready before integration
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Maniphest Tasks: T6696
    
    Differential Revision: https://phab.kalray.eu/D252

--- arch/k1c/include/asm/syscall.h
+++ arch/k1c/include/asm/syscall.h
@@ -9,6 +9,32 @@
 #ifndef _ASM_K1C_SYSCALL_H
 #define _ASM_K1C_SYSCALL_H
 
+#include <asm/ptrace.h>
+
 void scall_machine_exit(unsigned char value);
+
+/**
+ * syscall_get_nr - find what system call a task is executing
+ * @task:	task of interest, must be blocked
+ * @regs:	task_pt_regs() of @task
+ *
+ * If @task is executing a system call or is at system call
+ * tracing about to attempt one, returns the system call number.
+ * If @task is not executing a system call, i.e. it's blocked
+ * inside the kernel for a fault or signal, returns -1.
+ *
+ * Note this returns int even on 64-bit machines.  Only 32 bits of
+ * system call number can be meaningful.  If the actual arch value
+ * is 64 bits, this truncates to 32 bits so 0xffffffff means -1.
+ *
+ * It's only valid to call this when @task is known to be blocked.
+ */
+int syscall_get_nr(struct task_struct *task, struct pt_regs *regs)
+{
+	if (!in_syscall(regs))
+		return -1;
+
+	return es_sysno(regs);
+}
 
 #endif
--- arch/k1c/include/uapi/asm/ptrace.h
+++ arch/k1c/include/uapi/asm/ptrace.h
@@ -15,7 +15,83 @@
  * This decouples struct pt_regs from the userspace ABI.
  * struct user_regs_struct must form a prefix of struct pt_regs.
  */
-struct user_regs_struct {
+struct user_pt_regs {
+	/* GPR */
+	unsigned long r0;
+	unsigned long r1;
+	unsigned long r2;
+	unsigned long r3;
+	unsigned long r4;
+	unsigned long r5;
+	unsigned long r6;
+	unsigned long r7;
+	unsigned long r8;
+	unsigned long r9;
+	unsigned long r10;
+	unsigned long r11;
+	union {
+		unsigned long r12;
+		unsigned long sp;
+	};
+	unsigned long r13;
+	unsigned long r14;
+	unsigned long r15;
+	unsigned long r16;
+	unsigned long r17;
+	unsigned long r18;
+	unsigned long r19;
+	unsigned long r20;
+	unsigned long r21;
+	unsigned long r22;
+	unsigned long r23;
+	unsigned long r24;
+	unsigned long r25;
+	unsigned long r26;
+	unsigned long r27;
+	unsigned long r28;
+	unsigned long r29;
+	unsigned long r30;
+	unsigned long r31;
+	unsigned long r32;
+	unsigned long r33;
+	unsigned long r34;
+	unsigned long r35;
+	unsigned long r36;
+	unsigned long r37;
+	unsigned long r38;
+	unsigned long r39;
+	unsigned long r40;
+	unsigned long r41;
+	unsigned long r42;
+	unsigned long r43;
+	unsigned long r44;
+	unsigned long r45;
+	unsigned long r46;
+	unsigned long r47;
+	unsigned long r48;
+	unsigned long r49;
+	unsigned long r50;
+	unsigned long r51;
+	unsigned long r52;
+	unsigned long r53;
+	unsigned long r54;
+	unsigned long r55;
+	unsigned long r56;
+	unsigned long r57;
+	unsigned long r58;
+	unsigned long r59;
+	unsigned long r60;
+	unsigned long r61;
+	unsigned long r62;
+	unsigned long r63;
+
+	/* SFR */
+	unsigned long lc;
+	unsigned long le;
+	unsigned long ls;
+	unsigned long ra;
+
+	unsigned long cs;
 };
 
 
