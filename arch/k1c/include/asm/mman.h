c8f002acb97c8a92ce75e8614c4b2a13ba6e1928
Clement Leger <clement.leger@kalray.eu>
2018-09-18 18:14:57 +0200

    k1c: Set FIRST_USER_ADDRESS and check it
    Summary:
    In order to forbid user elf loading at address less than 2 * PAGE_SIZE,
    set FIRST_USER_ADDRESS and implement arch_mmap_check the same way as
    arm. This allow to deny the mmap of address below that limit.
    
    Test Plan: Run an elf mapped to run at 0x0 and check that it correctly fails
    
    Reviewers: O51 linux!
    
    Differential Revision: https://phab.kalray.eu/D283

---
+++ arch/k1c/include/asm/mman.h
@@ -0,0 +1,17 @@
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_MMAN_H
+#define _ASM_K1C_MMAN_H
+
+#include <asm-generic/mman.h>
+
+#define arch_mmap_check(addr, len, flags) \
+	(((flags) & MAP_FIXED && (addr) < FIRST_USER_ADDRESS) ? -EINVAL : 0)
+
+#endif	/* _ASM_K1C_MMAN_H */
