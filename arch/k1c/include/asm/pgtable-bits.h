b244913cd8f3450226ee87eea36b1562b976cb54
Clement Leger <clement.leger@kalray.eu>
2018-10-02 11:27:50 +0200

    k1c: handle _PAGE_DEVICE in tlb_refill routine
    Summary:
    In order to handle proper ioremap, we need a way to install tlb entries
    using the proper device policy. The policy can be found using the
    pte_entry bit _PAGE_DEVICE which will be set when using ioremap.
    The refill then checks this flag and set the cache policy in the tlb
    entry.
    
    Ref T6955
    
    Test Plan: iWait for ioremap to be ready and check device access to be ok.
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Maniphest Tasks: T6955
    
    Differential Revision: https://phab.kalray.eu/D298

--- arch/k1c/include/asm/pgtable-bits.h
+++ arch/k1c/include/asm/pgtable-bits.h
@@ -13,13 +13,14 @@
  * Protection bit definition
  * As we don't have any hw to handle page table walk, we can define
  * our own PTE format.
- * This implementation is copied from RiscV implementation
+ * This implementation is copied from RiscV implementation as a start
+ * point. We added some new fields to match the k1c needs.
  */
 
 /*
- * RiscV PTE format:
- * | 63   8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0
- *   PFN      D   A   G   U   X   W   R   V
+ * PTE format:
+ * | 63   9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0
+ *   PFN     DEV  D   A   G   U   X   W   R   V
  */
 #define _PAGE_PRESENT   (1 << 0)
 #define _PAGE_READ      (1 << 1)    /* Readable */
@@ -29,7 +30,8 @@
 #define _PAGE_GLOBAL    (1 << 5)    /* Global */
 #define _PAGE_ACCESSED  (1 << 6)    /* Set by hardware on any access */
 #define _PAGE_DIRTY     (1 << 7)    /* Set by hardware on any write */
-#define _PAGE_SOFT      (1 << 8)    /* Reserved for software */
+#define _PAGE_DEVICE    (1 << 8)    /* Device space mapping */
+#define _PAGE_SOFT      (1 << 9)    /* Reserved for software */
 
 #define _PAGE_SPECIAL   _PAGE_SOFT
 
