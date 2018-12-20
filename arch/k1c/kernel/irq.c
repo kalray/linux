3051b17f168d963c361dc1d9d46d26eccf25d0b7
Clement Leger <clement.leger@kalray.eu>
2018-12-20 17:32:54 +0100

    k1c: Multiring port
    Summary:
    In order to handle multiring we need to adapt startup, user and
    MMU management.
    Debug is meant to run in PL0, hence, directly after starting, delegate
    some rights to the kernel (PL1). These rights can be listed by looking at
    privilege.h file.
    Then, after startup, delegate basic rights for the user (hle, ice, use).
    This is only what we expect the user to touch.
    Finally, fix all MMU related code regarding tel and teh modifications.
    
    Test Plan: Run the kernel using the multiring toolchain
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Differential Revision: https://phab.kalray.eu/D269

--- arch/k1c/kernel/irq.c
+++ arch/k1c/kernel/irq.c
@@ -11,6 +11,9 @@
 #include <linux/irqchip.h>
 #include <linux/init.h>
 
+#define IT_MASK(__it) (K1C_SFR_ILL_ ## __it ## _MASK)
+#define IT_LEVEL(__it, __level) \
+	(__level << K1C_SFR_ILL_ ## __it ## _SHIFT)
 
 void do_IRQ(int hwirq, struct pt_regs *regs)
 {
@@ -33,8 +36,26 @@ void do_IRQ(int hwirq, struct pt_regs *regs)
  */
 void k1c_init_core_irq(void)
 {
-	k1c_sfr_set(K1C_SFR_ILL, 0xFFFFFFFFFFFFFFFFULL);
-	k1c_sfr_set(K1C_SFR_ILH, 0xFFFFFFFFFFFFFFFFULL);
+	/*
+	 * On K1, Kernel only care about the following IT:
+	 * - IT0: Timer 0
+	 * - IT2: Watchdog
+	 * - IT4: APIC IT 1
+	 * - IT5: APIC IT 2
+	 */
+	uint64_t mask = IT_MASK(IT0) | IT_MASK(IT2) | IT_MASK(IT4);
+
+	/*
+	 * Specific priorities for ITs:
+	 * - Watchdog has the highest priority: 3
+	 * - Timer has priority 2
+	 * - APIC entries have lowest priority: 1
+	 */
+	uint64_t value = IT_LEVEL(IT0, 0x2) | IT_LEVEL(IT2, 0x3) |
+			IT_LEVEL(IT4, 0x1);
+
+	k1c_sfr_set_mask(K1C_SFR_ILL, mask, value);
+
 	/* Set core level to 0 */
 	k1c_sfr_set_mask(K1C_SFR_PS, K1C_SFR_PS_IL_MASK, 0);
 }
