0803e56da8811720d52ef704fe6fd44f348f78e8
Guillaume Thouvenin <gthouvenin@kalray.eu>
2019-01-08 16:56:12 +0100

    k1c: Implement mmap syscall
    Test Plan:
    Modify initramfs with big malloc to generate a syscall to mmap and
    check that we can read/write the memory.
    
    Reviewers: cleger, O51 linux!
    
    Differential Revision: https://phab.kalray.eu/D447

--- arch/k1c/kernel/sys_k1c.c
+++ arch/k1c/kernel/sys_k1c.c
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
@@ -10,7 +11,14 @@
 
 SYSCALL_DEFINE6(mmap, unsigned long, addr, unsigned long, len,
 	unsigned long, prot, unsigned long, flags,
-	unsigned long, fd, off_t, offset)
+	unsigned long, fd, off_t, off)
 {
-	panic("Unimplemented %s", __func__);
+	/* offset must be a multiple of the page size */
+	if (unlikely(offset_in_page(off) != 0))
+		return -EINVAL;
+
+	/* Unlike mmap2 where the offset is in PAGE_SIZE-byte units, here it
+	 * is in bytes. So we need to use PAGE_SHIFT.
+	 */
+	return ksys_mmap_pgoff(addr, len, prot, flags, fd, off >> PAGE_SHIFT);
 }
