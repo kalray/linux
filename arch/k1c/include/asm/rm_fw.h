84d809c44c986efc6d76184329cec77d3a214136
Clement Leger <clement.leger@kalray.eu>
2019-01-08 11:33:12 +0100

    k1c: Enable L2 coherency if l2 firmware is present
    Summary:
    In order to have a modular rm firmware framework, define a set of
    features support by firmwares. When a firmware starts, it register its
    set of features by using the provided function ($r0).
    
    Test Plan: Build the kernel with the L2 cache firmware to check it works correctly.
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: gthouvenin
    
    Differential Revision: https://phab.kalray.eu/D442

---
+++ arch/k1c/include/asm/rm_fw.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_RM_FW_H
+#define _ASM_K1C_RM_FW_H
+
+#define K1C_FW_NO_FEATURES (0)
+#define K1C_FW_FEATURE_L2 (1 << 0)
+
+#endif /* _ASM_K1C_RM_FW_H */
