4c3b2f0e7b3f84efa68a055b040d8b9d3da1f294
Clement Leger <clement.leger@kalray.eu>
2019-01-15 16:32:30 +0100

    k1c: handle DAME securely.
    Summary:
    In order to handle DAME interrupt securely, we need to guarantee that
    the user triggered DAME interrupts will not be serviced by the kernel in
    kernel context. This implies a few barrier and some checking before
    returning to user.
    
    Ref T7939
    
    Test Plan:
    Manually tested by setting $ilr during debugging and checking that
    we match the expected effects.
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: pgmassas, vray, gthouvenin
    
    Maniphest Tasks: T7939
    
    Differential Revision: https://phab.kalray.eu/D441

---
+++ arch/k1c/include/asm/dame.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2018 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_DAME_H
+#define _ASM_K1C_DAME_H
+
+#include <asm/sfr.h>
+#include <asm/ptrace.h>
+
+static inline void dame_irq_check(struct pt_regs *regs)
+{
+#ifdef CONFIG_SECURE_DAME_HANDLING
+	unsigned long ilr;
+	/* If we are returning to the kernel, no need to check for DAME */
+	if (!user_mode(regs))
+		return;
+
+	/* Else, make sure we do a barrier to trig any pending DAME IRQ */
+	__builtin_k1_barrier();
+
+	/* Check if we triggered a DAME */
+	ilr = k1c_sfr_get(K1C_SFR_ILR);
+	if (ilr & K1C_SFR_ILR_IT16_MASK)
+		panic("DAME error encountered while in kernel !!!!\n");
+#endif
+}
+
+#endif /* _ASM_K1C_DAME_H */
