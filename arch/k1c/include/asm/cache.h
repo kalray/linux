67b6a15af57059e77ddb7fac7e918cc9aa63ca4f
Clement Leger <clement.leger@kalray.eu>
2019-03-05 19:04:18 +0100

    k1c: Implement correct behavior for I-cache  handling
    Summary: Rework all missing unimplemented operations for I-cache maintenance.
    
    Test Plan: Run the kernel and validate it still work correctly.
    
    Reviewers: O51 linux!
    
    Differential Revision: https://phab.kalray.eu/D574

--- arch/k1c/include/asm/cache.h
+++ arch/k1c/include/asm/cache.h
@@ -9,8 +9,28 @@
 #ifndef _ASM_K1C_CACHE_H
 #define _ASM_K1C_CACHE_H
 
-#define L1_CACHE_SHIFT	6
-#define L1_CACHE_BYTES	(1 << L1_CACHE_SHIFT)
+/**
+ * On K1C I$ and D$ have the same size. (16KB)
+ * Caches are 16K bytes big, 4-way set associative, true lru, with 64-byte
+ * lines. The D$ is also write-through.
+ */
+#define K1C_ICACHE_WAY_COUNT	4
+#define K1C_ICACHE_SET_COUNT	64
+#define K1C_ICACHE_LINE_SHIFT	6
+#define K1C_ICACHE_LINE_SIZE	(1 << K1C_ICACHE_LINE_SHIFT)
+#define K1C_ICACHE_SIZE	\
+	(K1C_ICACHE_WAY_COUNT * K1C_ICACHE_SET_COUNT * K1C_ICACHE_LINE_SIZE)
+
+/**
+ * Invalidate the whole I-cache if the size to flush is more than this value
+ */
+#define K1C_ICACHE_INVAL_SIZE	(K1C_ICACHE_SIZE)
+
+#define K1C_DCACHE_LINE_SHIFT	6
+#define K1C_DCACHE_LINE_SIZE	(1 << K1C_DCACHE_LINE_SHIFT)
+
+#define L1_CACHE_SHIFT	K1C_DCACHE_LINE_SHIFT
+#define L1_CACHE_BYTES	K1C_DCACHE_LINE_SIZE
 
 #define ARCH_DMA_MINALIGN L1_CACHE_BYTES
 
