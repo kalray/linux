75b79e933a1908651fe3818619c587b775c82b11
Clement Leger <cleger@kalray.eu>
2019-04-26 11:38:30 +0200

    k1c: Remove useless double initialization.
    Summary:
    JTLB cleanup, PMJ setup and error clearing is already done in head.S in
    early boot. There is no need to do it twice so remove this part of code
    and simply clear SMEM entry as before.
    While removing this part of code, jtlb cleanup is not used anymore in
    this file. Move the jtlb cleanup where it is only needed (ie in tlb.c
    for local_flush_tlb_all).
    
    Test Plan:
    Checked that the cleanup is correctly done using DEBUG config to dump
    TLBs.
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D775

--- arch/k1c/mm/mmu.c
+++ arch/k1c/mm/mmu.c
@@ -37,31 +37,6 @@ dump_tlb_entry(int dump_all, int dump_jtlb, int set, int way,
 			(unsigned long)tlbf.tel.es);
 }
 
-void k1c_mmu_cleanup_jtlb(int verbose)
-{
-	struct k1c_tlb_format tlbe = K1C_EMPTY_TLB_ENTRY;
-	int set, way;
-
-	for (set = 0; set < MMU_JTLB_SETS; set++) {
-		tlbe.teh.pn = set;
-		for (way = 0; way < MMU_JTLB_WAYS; way++) {
-			/* Set is selected automatically according to the
-			 * virtual address.
-			 * With 4K pages the set is the value of the 6 lower
-			 * signigicant bits of the page number.
-			 */
-			k1c_mmu_add_entry(MMC_SB_JTLB, way, tlbe);
-
-			if (k1c_mmc_error(k1c_sfr_get(K1C_SFR_MMC)))
-				panic("Failed to initialize JTLB[s:%02d w:%d]",
-				      set, way);
-		}
-	}
-
-	if (verbose)
-		pr_info("JTLB has been cleaned\n");
-}
-
 void k1c_mmu_dump_ltlb(int dump_all)
 {
 	struct k1c_tlb_format tlbe;
@@ -119,13 +94,6 @@ void k1c_mmu_dump_jtlb(int dump_all)
 
 void __init k1c_mmu_early_setup(void)
 {
-	/* Clean error field in MMC */
-	k1c_sfr_clear_bit(K1C_SFR_MMC, K1C_SFR_MMC_E_SHIFT);
-
-	k1c_sfr_set_mask(K1C_SFR_PS, K1C_SFR_PS_PMJ_MASK,
-		(K1C_SUPPORTED_PSIZE << K1C_SFR_PS_PMJ_SHIFT));
-
-	k1c_mmu_cleanup_jtlb(1);
 	k1c_mmu_remove_ltlb_entry(LTLB_ENTRY_EARLY_SMEM);
 
 #ifdef K1C_MMU_DEBUG
