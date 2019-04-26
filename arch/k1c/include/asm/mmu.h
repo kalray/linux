38603bea336a0b88e5b7c80eab6e197b66c57172
Clement Leger <cleger@kalray.eu>
2019-04-26 11:38:30 +0200

    k1c: Remove useless double initialization.
    Summary:
    JTLB cleanup, PMJ setup and error clearing is already done in head.S in
    early boot. There is no need to do it twice so remove this part of code
    and simply clear SMEM entry as before.
    While removing this part of code, jtlb cleanup is not used anymore in
    this file. Move the jtlb cleanup where it is only needed (ie in tlb.c
    for local_flush_tlb_all).
    
    Test Plan:
    Checked that the cleanup is correctly done using DEBUG config to dump
    TLBs.
    
    Reviewers: O51 linux, gthouvenin
    
    Reviewed By: O51 linux, gthouvenin
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D775

--- arch/k1c/include/asm/mmu.h
+++ arch/k1c/include/asm/mmu.h
@@ -180,7 +180,6 @@ static inline struct k1c_tlb_format tlb_mk_entry(
 	return entry;
 }
 
-extern void k1c_mmu_cleanup_jtlb(int verbose);
 extern void k1c_mmu_early_setup(void);
 extern void k1c_mmu_dump_ltlb(int dump_all);
 extern void k1c_mmu_dump_jtlb(int dump_all);
