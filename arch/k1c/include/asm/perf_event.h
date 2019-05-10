c4254b466d2a81aaf6622affc84d8fb55c3c3f3f
Marius Gligor <mgligor@kalray.eu>
2019-05-10 19:05:14 +0200

    k1c: Add support for hardware breakpoint/watchpoint
    Summary:
    When a hardware watchpoint/breakpoint is owned by a more
    privileged PL (e.g. the k1-jtag-runner debug routines (PL0) own them by
    default) and it is required by gdbserver, ptrace requests it from the
    owner. k1-jtag-runner gives the ownership to the requesting PL if the
    watchpoint/breakpoint is not currently used by PL0, else ptrace informs
    gdbserver that the resource is not available.
    Ref T9529
    
    Test Plan:
    Tested by debugging a linux application. Waiting for a second
    connection between the host and MPPA to add a test in integration.
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T9529
    
    Differential Revision: https://phab.kalray.eu/D900

---
+++ arch/k1c/include/asm/perf_event.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2019 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_PERF_EVENT_H
+#define _ASM_K1C_PERF_EVENT_H
+
+#endif /* _ASM_K1C_PERF_EVENT_H */
