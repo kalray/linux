245b005406f5d8b33e01d8db439771902018a1f4
Clement Leger <cleger@kalray.eu>
2019-05-13 16:06:07 +0200

    k1c: add function to compute cache policy
    Summary:
    In order to add ltlb entries, we will also need to compute cache policy.
    Avoid code duplication and put it in a seprate function to be shared
    with soon to be ltlb entry adding.
    
    Test Plan: Verify that cache policy is correctly computed
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: gthouvenin
    
    Differential Revision: https://phab.kalray.eu/D751

--- arch/k1c/include/asm/tlb.h
+++ arch/k1c/include/asm/tlb.h
@@ -15,4 +15,15 @@ static void tlb_flush(struct mmu_gather *tlb);
 
 #include <asm-generic/tlb.h>
 
+static inline unsigned int pgprot_cache_policy(unsigned long flags)
+{
+	if (flags & _PAGE_DEVICE)
+		return TLB_CP_D_U;
+
+	if (flags & _PAGE_UNCACHED)
+		return TLB_CP_U_U;
+
+	return TLB_CP_W_C;
+}
+
 #endif /* _ASM_K1C_TLB_H */
