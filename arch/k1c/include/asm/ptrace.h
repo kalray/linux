bc3cf89f03ac71a38cee0cedd728acccf0a8e545
Clement Leger <cleger@kalray.eu>
2019-05-15 11:32:21 +0200

    k1c: Implement ELF_CORE_COPY_REGS
    Summary:
    On k1c, user_pt_regs are different from pt_regs. Generic code
    elf_core_copy_regs expect elf_gregset_t to be the size of pt_regs.
    In our implementation, elf_gregset_t is the size of user_pt_regs since
    this is what we expect to be relevant for the user.
    In order to correctly copy user_pt_regs into elf_gregset_t, fix the
    expected size of elf_gregset_t and define ELF_CORE_COPY_REGS to simply
    copy the user_pt_regs into elf_gregset_t. Doing so also requires to
    implement dump_fpu which is empty on k1c.
    
    Ref T9514
    
    Test Plan:
    Ran failing waitpid05 test which is now ok:
    ```
     waitpid05    1  TPASS  :  received expected pid.
     waitpid05    2  TPASS  :  received expected exit number.
     waitpid05    3  TPASS  :  received expected pid.
     waitpid05    4  TPASS  :  received expected exit number.
     waitpid05    5  TPASS  :  received expected pid.
     waitpid05    6  TPASS  :  received expected exit number.
     waitpid05    7  TPASS  :  received expected pid.
     waitpid05    8  TPASS  :  received expected exit number.
     waitpid05    9  TPASS  :  received expected pid.
     waitpid05   10  TPASS  :  received expected exit number.
     waitpid05   11  TPASS  :  received expected pid.
     waitpid05   12  TPASS  :  received expected exit number.
     waitpid05   13  TPASS  :  received expected pid.
     waitpid05   14  TPASS  :  received expected exit number.
     waitpid05   15  TPASS  :  received expected pid.
     waitpid05   16  TPASS  :  received expected exit number.
     waitpid05   17  TPASS  :  received expected pid.
     waitpid05   18  TPASS  :  received expected exit number.
     waitpid05   19  TPASS  :  received expected pid.
     waitpid05   20  TPASS  :  received expected exit number.
     waitpid05   21  TPASS  :  received expected pid.
     waitpid05   22  TPASS  :  received expected exit number.
     waitpid05   23  TPASS  :  received expected pid.
     waitpid05   24  TPASS  :  received expected exit number.
    ```
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: gthouvenin, #linux_coolidge_cc
    
    Maniphest Tasks: T9514
    
    Differential Revision: https://phab.kalray.eu/D925

--- arch/k1c/include/asm/ptrace.h
+++ arch/k1c/include/asm/ptrace.h
@@ -44,90 +44,94 @@
  * of this struct 1:1
  */
 struct pt_regs {
-	/* GPR */
-	uint64_t r0;
-	uint64_t r1;
-	uint64_t r2;
-	uint64_t r3;
-	uint64_t r4;
-	uint64_t r5;
-	uint64_t r6;
-	uint64_t r7;
-	uint64_t r8;
-	uint64_t r9;
-	uint64_t r10;
-	uint64_t r11;
 	union {
-		uint64_t r12;
-		uint64_t sp;
-	};
-	union {
-		uint64_t r13;
-		uint64_t tp;
-	};
-	union {
-		uint64_t r14;
-		uint64_t fp;
-	};
-	uint64_t r15;
-	uint64_t r16;
-	uint64_t r17;
-	uint64_t r18;
-	uint64_t r19;
-	uint64_t r20;
-	uint64_t r21;
-	uint64_t r22;
-	uint64_t r23;
-	uint64_t r24;
-	uint64_t r25;
-	uint64_t r26;
-	uint64_t r27;
-	uint64_t r28;
-	uint64_t r29;
-	uint64_t r30;
-	uint64_t r31;
-	uint64_t r32;
-	uint64_t r33;
-	uint64_t r34;
-	uint64_t r35;
-	uint64_t r36;
-	uint64_t r37;
-	uint64_t r38;
-	uint64_t r39;
-	uint64_t r40;
-	uint64_t r41;
-	uint64_t r42;
-	uint64_t r43;
-	uint64_t r44;
-	uint64_t r45;
-	uint64_t r46;
-	uint64_t r47;
-	uint64_t r48;
-	uint64_t r49;
-	uint64_t r50;
-	uint64_t r51;
-	uint64_t r52;
-	uint64_t r53;
-	uint64_t r54;
-	uint64_t r55;
-	uint64_t r56;
-	uint64_t r57;
-	uint64_t r58;
-	uint64_t r59;
-	uint64_t r60;
-	uint64_t r61;
-	uint64_t r62;
-	uint64_t r63;
+		struct user_pt_regs user_regs;
+		struct {
+			/* GPR */
+			uint64_t r0;
+			uint64_t r1;
+			uint64_t r2;
+			uint64_t r3;
+			uint64_t r4;
+			uint64_t r5;
+			uint64_t r6;
+			uint64_t r7;
+			uint64_t r8;
+			uint64_t r9;
+			uint64_t r10;
+			uint64_t r11;
+			union {
+				uint64_t r12;
+				uint64_t sp;
+			};
+			union {
+				uint64_t r13;
+				uint64_t tp;
+			};
+			union {
+				uint64_t r14;
+				uint64_t fp;
+			};
+			uint64_t r15;
+			uint64_t r16;
+			uint64_t r17;
+			uint64_t r18;
+			uint64_t r19;
+			uint64_t r20;
+			uint64_t r21;
+			uint64_t r22;
+			uint64_t r23;
+			uint64_t r24;
+			uint64_t r25;
+			uint64_t r26;
+			uint64_t r27;
+			uint64_t r28;
+			uint64_t r29;
+			uint64_t r30;
+			uint64_t r31;
+			uint64_t r32;
+			uint64_t r33;
+			uint64_t r34;
+			uint64_t r35;
+			uint64_t r36;
+			uint64_t r37;
+			uint64_t r38;
+			uint64_t r39;
+			uint64_t r40;
+			uint64_t r41;
+			uint64_t r42;
+			uint64_t r43;
+			uint64_t r44;
+			uint64_t r45;
+			uint64_t r46;
+			uint64_t r47;
+			uint64_t r48;
+			uint64_t r49;
+			uint64_t r50;
+			uint64_t r51;
+			uint64_t r52;
+			uint64_t r53;
+			uint64_t r54;
+			uint64_t r55;
+			uint64_t r56;
+			uint64_t r57;
+			uint64_t r58;
+			uint64_t r59;
+			uint64_t r60;
+			uint64_t r61;
+			uint64_t r62;
+			uint64_t r63;
 
-	/* SFR */
-	uint64_t lc;
-	uint64_t le;
-	uint64_t ls;
-	uint64_t ra;
+			/* SFR */
+			uint64_t lc;
+			uint64_t le;
+			uint64_t ls;
+			uint64_t ra;
 
-	uint64_t cs;
-	uint64_t spc;
-	/* Up to here, this is registers needed for user_pt_regs */
+			uint64_t cs;
+			uint64_t spc;
+		};
+	};
 	uint64_t sps;
 	uint64_t es;
 
