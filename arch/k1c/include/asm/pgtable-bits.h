59cbd14da67a09779640e67359a3929da3c8ef80
Guillaume Thouvenin <gthouvenin@kalray.eu>
2019-05-15 14:30:28 +0200

    k1c: remove _PAGE_USER in PTE
    Summary:
    User and kernel pages can be differentiate with the bit _PAGE_GLOBAL. If a
    page is global it belongs to the kernel, otherwise it belongs to user.
    
    Test Plan: Run tests as before
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D942

--- arch/k1c/include/asm/pgtable-bits.h
+++ arch/k1c/include/asm/pgtable-bits.h
@@ -4,13 +4,13 @@
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
- * Copyright (C) 2018 Kalray Inc.
+ * Copyright (C) 2018-2019 Kalray Inc.
  */
 
 #ifndef _ASM_K1C_PGTABLE_BITS_H
 #define _ASM_K1C_PGTABLE_BITS_H
 
-/**
+/*
  * Protection bit definition
  * As we don't have any hw to handle page table walk, we can define
  * our own PTE format.
@@ -18,43 +18,45 @@
  * point. We added some new fields to match the k1c needs.
  */
 
-#define _PAGE_GLOBAL_SHIFT	5 /* Global */
+#define _PAGE_GLOBAL_SHIFT	4 /* Global */
+
 /*
  * PTE format:
- *  +-----------+----------+---+---+---+---+---+---+---+---+---+---+
- *  | 63 ... 12 | 11 .. 10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
- *  +-----------+----------+---+---+---+---+---+---+---+---+---+---+
- *       PFN       Unused   UNC DEV  D   A   G   U   X   W   R   P
+ *  +-----------+---------+---+---+---+---+---+---+---+---+---+
+ *  | 63 ... 12 | 11 .. 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+ *  +-----------+---------+---+---+---+---+---+---+---+---+---+
+ *       PFN       Unused  UNC DEV  D   A   G   X   W   R   P
  */
-#define _PAGE_PRESENT   (1 << 0)
+#define _PAGE_PRESENT   (1 << 0)    /* Present */
 #define _PAGE_READ      (1 << 1)    /* Readable */
 #define _PAGE_WRITE     (1 << 2)    /* Writable */
 #define _PAGE_EXEC      (1 << 3)    /* Executable */
-#define _PAGE_USER      (1 << 4)    /* User */
 #define _PAGE_GLOBAL    (1 << _PAGE_GLOBAL_SHIFT)    /* Global */
-#define _PAGE_ACCESSED  (1 << 6)    /* Set by hardware on any access */
-#define _PAGE_DIRTY     (1 << 7)    /* Set by hardware on any write */
-#define _PAGE_DEVICE    (1 << 8)    /* Device space mapping */
-#define _PAGE_UNCACHED  (1 << 9)    /* Uncached mapping */
-#define _PAGE_SOFT      (1 << 10)   /* Reserved for software */
+#define _PAGE_ACCESSED  (1 << 5)    /* Set by hardware on any access */
+#define _PAGE_DIRTY     (1 << 6)    /* Set by hardware on any write */
+#define _PAGE_DEVICE    (1 << 7)    /* Device space mapping */
+#define _PAGE_UNCACHED  (1 << 8)    /* Uncached mapping */
+#define _PAGE_SOFT      (1 << 9)    /* Reserved for software */
 
 #define _PAGE_SPECIAL   _PAGE_SOFT
 
-#define K1C_ACCESS_PERMS_BITS	5
+#define K1C_ACCESS_PERMS_BITS	4
 #define K1C_ACCESS_PERMS_OFFSET	1
 #define K1C_ACCESS_PERMS_SIZE	(1 << K1C_ACCESS_PERMS_BITS)
-#define K1C_ACCESS_PERMS_INDEX(x) \
-	((unsigned int)(x & 0x3E) >> K1C_ACCESS_PERMS_OFFSET)
 
 #define K1C_ACCESS_PERM_START_BIT	K1C_ACCESS_PERMS_OFFSET
 #define K1C_ACCESS_PERM_STOP_BIT \
 	(K1C_ACCESS_PERMS_OFFSET + K1C_ACCESS_PERMS_BITS - 1)
+#define K1C_ACCESS_PERMS_MASK \
+	GENMASK(K1C_ACCESS_PERM_STOP_BIT, K1C_ACCESS_PERM_START_BIT)
+#define K1C_ACCESS_PERMS_INDEX(x) \
+	((unsigned int)(x & K1C_ACCESS_PERMS_MASK) >> K1C_ACCESS_PERMS_OFFSET)
 
 /**
  * Set of bits to preserve across pte_modify()
  * TODO: Check this !
  */
 #define _PAGE_CHG_MASK  (~(unsigned long)(_PAGE_PRESENT | _PAGE_READ |	\
-					  _PAGE_WRITE | _PAGE_EXEC |	\
-					  _PAGE_USER | _PAGE_GLOBAL))
+					  _PAGE_WRITE | _PAGE_EXEC | \
+					  _PAGE_GLOBAL))
 #endif
