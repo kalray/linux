20dc3eb205486a6062ab7feed5721af520f5a2ff
Clement Leger <cleger@kalray.eu>
2019-05-27 15:42:44 +0200

    k1c: Display memory error in DAME interrupt
    Summary:
    Display memory error status when interrupt happens. es.iti contains a
    copy of $mes when interrupt was trigerred. Display this error to obtain
    more information when such error happens.
    
    Test Plan:
    Trigged a DAME interrupt using
    
      (gdb) p/x $ilr.it16 = 1
      $1 = 0x1
      (gdb) p/x $es.iti = 0xFF
      $2 = 0xff
      (gdb) c
      Continuing.
      [    3.020000] Memory Error:
      [    3.020000] - PSE
      [    3.020000] - PILSY
      [    3.020000] - PILDE
      [    3.020000] - PILPA
      [    3.020000] - DSE
      [    3.020000] - DILSY
      [    3.020000] - DILDE
      [    3.020000] - DILPA
      [    3.020000] Kernel panic - not syncing: DAME error encountered while in kernel !!!!
      [Inferior 1 (process 1) exited with code 01]
    
    Reviewers: O51 Linux Coolidge, pkerbrat
    
    Reviewed By: O51 Linux Coolidge, pkerbrat
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D1013

--- arch/k1c/kernel/dame_handler.c
+++ arch/k1c/kernel/dame_handler.c
@@ -19,14 +19,37 @@
 
 static unsigned int k1c_dame_irq;
 
+static const char *error_str[K1C_SFR_ES_ITI_WIDTH] = {
+	"PSE",
+	"PILSY",
+	"PILDE",
+	"PILPA",
+	"DSE",
+	"DILSY",
+	"DILDE",
+	"DILPA",
+	"DDEE",
+	"DSYE"
+};
+
 irqreturn_t dame_irq_handler(int irq, void *dev_id)
 {
+	int bit;
+	struct pt_regs *regs = get_irq_regs();
+	unsigned long error_status = k1c_sfr_field_val(regs->es, ES, ITI);
+
+	if (error_status) {
+		pr_err("Memory Error:\n");
+		for_each_set_bit(bit, &error_status, K1C_SFR_ES_ITI_WIDTH)
+			pr_err("- %s\n", error_str[bit]);
+	}
+
 	/*
 	 * If the DAME happened in user mode, we can handle it properly
 	 * by killing the user process.
 	 * Otherwise, if we are in kernel, we are fried...
 	 */
-	if (user_mode(get_irq_regs())) {
+	if (user_mode(regs)) {
 		force_sig_fault(SIGBUS, BUS_ADRERR,
 				(void __user *) NULL, current);
 	} else {
