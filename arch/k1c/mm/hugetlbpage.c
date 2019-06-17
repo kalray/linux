a835bc8ddc47a6916acdc7396161db2f7385e467
Guillaume Thouvenin <gthouvenin@kalray.eu>
2019-06-17 11:22:46 +0200

    k1c: Manage all huge page size
    Summary:
    This patch adds huge page of 64Ko and 512Mo. To support this size we use
    several contiguous entries in the page table. For huge pages of 64Ko we
    will need to use 16 entries in the PTE and for huge pages of 512Mo we
    will use 256 entries in PMD.
    
    Ref T9355
    
    Test Plan: Add a test that uses pages of 64Ko in coolval
    
    Reviewers: O51 Linux Coolidge, cleger
    
    Subscribers: cleger, #linux_coolidge_cc
    
    Maniphest Tasks: T9355
    
    Differential Revision: https://phab.kalray.eu/D1184

--- arch/k1c/mm/hugetlbpage.c
+++ arch/k1c/mm/hugetlbpage.c
@@ -7,10 +7,63 @@
  * Copyright (C) 2019 Kalray Inc.
  */
 
+#include <asm/page.h>
+
+#include <linux/bits.h>
 #include <linux/mm.h>
 #include <linux/hugetlb.h>
 
-#define K1C_PAGE_2M        UL(TLB_PS_2M)
+/**
+ * get_nr_cont_huge_pages - return the number of contiguous pages
+ * @ptev: the value of the PTE
+ *
+ * Return: it reads the page size and depending of the value it returns the
+ * number of contiguous entries used by the huge page.
+ */
+static unsigned int get_nr_cont_huge_pages(unsigned long ptev)
+{
+	unsigned int psize, nr_cont;
+
+	psize = (ptev & K1C_PAGE_SZ_MASK) >> K1C_PAGE_SZ_SHIFT;
+
+	if (psize == TLB_PS_64K)
+		/* Huge page of 64K are hold in PTE table */
+		nr_cont = 1UL << (K1C_PAGE_64K_SHIFT - PAGE_SHIFT);
+	else if (psize == TLB_PS_512M)
+		/* Huge page of 512M are hold in PMD table */
+		nr_cont = 1UL << (K1C_PAGE_512M_SHIFT - PMD_SHIFT);
+	else
+		/* Only page of 64Ko and 512Mo require more than 1 entry */
+		nr_cont = 1;
+
+	return nr_cont;
+}
+
+void set_huge_pte_at(struct mm_struct *mm, unsigned long addr,
+		     pte_t *ptep, pte_t pte)
+{
+	unsigned int nr_cont, i;
+
+	nr_cont = get_nr_cont_huge_pages(pte_val(pte));
+
+	for (i = 0; i < nr_cont; i++, ptep++)
+		set_pte_at(mm, addr, ptep, pte);
+}
+
+pte_t huge_ptep_get_and_clear(struct mm_struct *mm,
+			      unsigned long addr,
+			      pte_t *ptep)
+{
+	unsigned int nr_cont, i;
+	pte_t pte = *ptep;
+
+	nr_cont = get_nr_cont_huge_pages(pte_val(pte));
+
+	for (i = 0; i < nr_cont; i++, ptep++)
+		pte_clear(mm, addr, ptep);
+
+	return pte;
+}
 
 pte_t arch_make_huge_pte(pte_t entry, struct vm_area_struct *vma,
 			 struct page *page, int writable)
@@ -20,10 +73,17 @@ pte_t arch_make_huge_pte(pte_t entry, struct vm_area_struct *vma,
 
 	ptev = pte_val(entry) & ~(K1C_PAGE_SZ_MASK);
 
-	/* Currently only huge page of 2M are supported */
-	if (shift == K1C_PAGE_2M_SHIFT) {
-		ptev = pte_val(entry) | (K1C_PAGE_2M << K1C_PAGE_SZ_SHIFT);
-	} else {
+	switch (shift) {
+	case K1C_PAGE_64K_SHIFT:
+		ptev |= (TLB_PS_64K << K1C_PAGE_SZ_SHIFT);
+		break;
+	case K1C_PAGE_2M_SHIFT:
+		ptev |= (TLB_PS_2M << K1C_PAGE_SZ_SHIFT);
+		break;
+	case K1C_PAGE_512M_SHIFT:
+		ptev |= (TLB_PS_512M << K1C_PAGE_SZ_SHIFT);
+		break;
+	default:
 		ptev = 0;
 		pr_err("huge page shift %d not supported\n", shift);
 		BUG();
@@ -56,10 +116,11 @@ pte_t *huge_pte_alloc(struct mm_struct *mm,
  * @addr: the address
  * @size: size of the memory area
  *
- * On k1c the huge page are backed on PMD. So we need to find in the offset
- * int the PMD.
+ * On k1c the huge page are backed on PMD or PTE depending of the size of the
+ * huge page. Huge pages larger or equal to 2Mo are backed on PMD, smaller are
+ * backed on PTE.
  *
- * Return: the offset in the PMD of the huge page if it exists, NULL otherwise.
+ * Return: pointer to the huge page if it exists, NULL otherwise.
  */
 pte_t *huge_pte_offset(struct mm_struct *mm,
 		       unsigned long addr,
@@ -67,15 +128,40 @@ pte_t *huge_pte_offset(struct mm_struct *mm,
 {
 	pgd_t *pgd;
 	pud_t *pud;
-	pmd_t *pmd = NULL;
+	pmd_t *pmd;
+	pte_t *pte;
+
+	/*
+	 * 64Ko and 512Mo huge pages require the usage of contiguous page so
+	 * we need to be aligned for this pages in order to get the right
+	 * offset (ie the offset of the first index of contiguous pages).
+	 */
+	if (size == K1C_PAGE_64K_SIZE)
+		addr &= K1C_PAGE_64K_MASK;
+	else if (size == K1C_PAGE_512M_SIZE)
+		addr &= K1C_PAGE_512M_MASK;
 
 	pgd = pgd_offset(mm, addr);
-	if (pgd_present(*pgd)) {
-		pud = pud_offset(pgd, addr);
-		if (pud_present(*pud))
-			pmd = pmd_offset(pud, addr);
+	if (!pgd_present(*pgd))
+		return NULL;
+
+	pud = pud_offset(pgd, addr);
+	if (!pud_present(*pud))
+		return NULL;
+
+	pmd = pmd_offset(pud, addr);
+
+	if (size == K1C_PAGE_64K_SIZE) {
+		/* we need to go deeper in the page table */
+		if (pmd_present(*pmd))
+			pte = pte_offset_kernel(pmd, addr);
+		else
+			pte = NULL;
+	} else {
+		pte = (pte_t *)pmd;
 	}
-	return (pte_t *) pmd;
+
+	return pte;
 }
 
 int pmd_huge(pmd_t pmd)
@@ -101,7 +187,9 @@ static void __init add_huge_page_size(unsigned long size)
 static int __init hugetlbpage_init(void)
 {
 #if defined(CONFIG_K1C_4K_PAGES)
-	add_huge_page_size(1UL << K1C_PAGE_2M_SHIFT);
+	add_huge_page_size(K1C_PAGE_64K_SIZE);
+	add_huge_page_size(K1C_PAGE_2M_SIZE);
+	add_huge_page_size(K1C_PAGE_512M_SIZE);
 #else
 	WARN(1, "Huge page not supported yet for 64Ko base page size.\n");
 #endif
