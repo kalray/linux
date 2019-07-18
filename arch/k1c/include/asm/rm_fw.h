66848be7032db820e5cf67c5ca577c8a83fafb33
Clement Leger <cleger@kalray.eu>
2019-07-18 10:06:54 +0200

    k1c: Add L2 cache driver
    Summary:
    In order to correctly handle the L2 cache and request writebacks, we need
    to communicate with it. This requires a few modifications to be able to
    configure and retrieve informations on the L2 cache.
    First of all, the device tree is now passed at boot time to the L2$ firmware.
    This is done by passing the DT address through $r1 and the register zone through
    $r0. Once the firmware is started, it will setup it status to ready. The driver
    will wait for this bit to start and get the mailbox address used to communicate
    with it through registers.
    
    Test Plan: Run using SMP + devices
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: pgmassas, ysionneau, alfred, vchardon, gthouvenin, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D1012

--- arch/k1c/include/asm/rm_fw.h
+++ arch/k1c/include/asm/rm_fw.h
@@ -4,13 +4,22 @@
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
- * Copyright (C) 2018 Kalray Inc.
+ * Copyright (C) 2019 Kalray Inc.
  */
 
 #ifndef _ASM_K1C_RM_FW_H
 #define _ASM_K1C_RM_FW_H
 
-#define K1C_FW_NO_FEATURES (0)
-#define K1C_FW_FEATURE_L2 (1 << 0)
+#include <linux/sizes.h>
+
+#define K1C_RM_ID	16
+
+#define RM_FIRMWARE_REGS_SIZE	(SZ_4K)
+
+#ifndef __ASSEMBLY__
+
+extern char *__rm_firmware_regs_start;
+
+#endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_K1C_RM_FW_H */
