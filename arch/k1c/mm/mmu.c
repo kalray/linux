5f21fb8c5e6f139113c40e280dfc2a2d9a675124
Clement Leger <cleger@kalray.eu>
2019-08-20 15:47:35 +0200

    k1c: Split update_mmu_cache
    Summary:
    When handling vmalloc fault, we end up calling update_mmu_cache
    with NULL value. This was done to update the JTLB cache in order to
    insert the entry that missed. However, the vma is used leading to a
    crash.
    Since update_mmu_cache does some checks and then refill the JTLB,
    split it and extract the refill part in another function. Ths function
    is now called directly in handle_vmalloc_fault to insert the faulting
    entry.
    
    Test Plan: Run it with 5.2 kernel in smp mode.
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: gthouvenin, alfred, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D1564

--- arch/k1c/mm/mmu.c
+++ arch/k1c/mm/mmu.c
@@ -18,12 +18,47 @@
 #include <asm/mmu.h>
 #include <asm/tlb.h>
 #include <asm/page_size.h>
+#include <asm/mmu_context.h>
 
 #define DUMP_LTLB 0
 #define DUMP_JTLB 1
 
+DEFINE_PER_CPU_ALIGNED(uint8_t[MMU_JTLB_SETS], jtlb_current_set_way);
 static struct k1c_tlb_format ltlb_entries[MMU_LTLB_WAYS];
 static unsigned long ltlb_entries_bmp;
+
+/*
+ * 4 bits are used to index the K1C access permissions. Bytes are used as
+ * follow:
+ *
+ *   +---------------+------------+-------------+------------+
+ *   |     Bit 3     |   Bit 2    |   Bit 1     |   Bit 0    |
+ *   |---------------+------------+-------------+------------|
+ *   |  _PAGE_GLOBAL | _PAGE_EXEC | _PAGE_WRITE | _PAGE_READ |
+ *   +---------------+------------+-------------+------------+
+ *
+ * If _PAGE_GLOBAL is set then the page belongs to the kernel. Otherwise it
+ * belongs to the user. When the page belongs to user we set the same
+ * rights to kernel.
+ */
+uint8_t k1c_access_perms[K1C_ACCESS_PERMS_SIZE] = {
+	TLB_PA_NA_NA,
+	TLB_PA_R_R,     /* 1: User R */
+	TLB_PA_NA_NA,
+	TLB_PA_RW_RW,   /* 3: User RW */
+	TLB_PA_NA_NA,
+	TLB_PA_RX_RX,   /* 5: User RX */
+	TLB_PA_NA_NA,
+	TLB_PA_RWX_RWX, /* 7: User RWX */
+	TLB_PA_NA_NA,
+	TLB_PA_NA_R,    /* 9: Kernel R */
+	TLB_PA_NA_NA,
+	TLB_PA_NA_RW,   /* 11: Kernel RW */
+	TLB_PA_NA_NA,
+	TLB_PA_NA_RX,   /* 13: Kernel RX */
+	TLB_PA_NA_NA,
+	TLB_PA_NA_RWX,  /* 15: Kernel RWX */
+};
 
 /**
  * k1c_mmu_ltlb_add_entry - Add a kernel entry in the LTLB
@@ -75,6 +110,73 @@ void k1c_mmu_ltlb_add_entry(unsigned long vaddr, phys_addr_t paddr,
 		panic("Failed to write entry to the JTLB");
 
 	local_irq_restore(irqflags);
+}
+
+/**
+ * k1c_mmu_jtlb_add_entry - Add an entry into JTLB
+ *
+ * JTLB is used both for kernel and user entries.
+ *
+ * @address: Virtual address for the entry (must be aligned according to tlb_ps)
+ * @ptep: pte entry pointer
+ * @asn: ASN (if pte entry is not global)
+ */
+void k1c_mmu_jtlb_add_entry(unsigned long address, pte_t *ptep,
+			    unsigned int asn)
+{
+	unsigned int shifted_addr, set, way;
+	unsigned long flags, pte_val;
+	struct k1c_tlb_format tlbe;
+	unsigned int pa, cp, ps;
+	phys_addr_t pfn;
+
+	pte_val = pte_val(*ptep);
+
+	pfn = (phys_addr_t)pte_pfn(*ptep);
+	if (!pfn_valid(pfn))
+		/* Not sure if it is normal. In doubt we panic and we will
+		 * debug.
+		 */
+		panic("%s: pfn %lx is not valid\n",
+		      __func__, (unsigned long)pfn);
+
+	asn &= MM_CTXT_ASN_MASK;
+
+	/* Set page as accessed */
+	pte_val(*ptep) |= _PAGE_ACCESSED;
+
+	BUILD_BUG_ON(K1C_PAGE_SZ_SHIFT != K1C_SFR_TEL_PS_SHIFT);
+
+	ps = (pte_val & K1C_PAGE_SZ_MASK) >> K1C_PAGE_SZ_SHIFT;
+	pa = (unsigned int) k1c_access_perms[K1C_ACCESS_PERMS_INDEX(pte_val)];
+	cp = pgprot_cache_policy(pte_val);
+
+	tlbe = tlb_mk_entry(
+		(void *)pfn_to_phys(pfn),
+		(void *)address,
+		ps,
+		(pte_val & _PAGE_GLOBAL) ? TLB_G_GLOBAL : TLB_G_USE_ASN,
+		pa,
+		cp,
+		asn,
+		TLB_ES_A_MODIFIED);
+
+	shifted_addr = address >> get_page_size_shift(ps);
+	set = shifted_addr & MMU_JTLB_SET_MASK;
+
+	local_irq_save(flags);
+
+	way = get_cpu_var(jtlb_current_set_way)[set]++;
+	put_cpu_var(jtlb_current_set_way);
+
+	way &= MMU_JTLB_WAY_MASK;
+
+	k1c_mmu_add_entry(MMC_SB_JTLB, way, tlbe);
+
+	if (k1c_mmc_error(k1c_sfr_get(K1C_SFR_MMC)))
+		panic("Failed to write entry to the JTLB");
+
+	local_irq_restore(flags);
 }
 
 static void
