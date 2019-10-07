2db573af8f5c8b4213d58f24657488e4be854a21
Benjamin Mugnier <bmugnier@kalray.eu>
2019-10-07 16:14:47 +0200

    k1c: eth: Full PFC sysfs implementation
    Summary:
    Implements PFC properties in sysfs, including pfc classes.
    
    Depends on: D1398 D1399
    
    Test Plan: sysfs tests
    
    Reviewers: O39 ethernet!, O51 Linux Coolidge, pkerbrat, cleger
    
    Reviewed By: O51 Linux Coolidge, cleger
    
    Subscribers: jmaselbas, cleger, pkerbrat, tcostis, alfred, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D1534
    
    Conflicts:
    	drivers/net/ethernet/kalray/k1c-net-hw.c
    	drivers/net/ethernet/kalray/k1c-net-hw.h
    	drivers/net/ethernet/kalray/k1c-net-sysfs.c
    	drivers/net/ethernet/kalray/k1c-net-sysfs.h
    	drivers/net/ethernet/kalray/k1c-net-tx.c
    	drivers/net/ethernet/kalray/k1c-net.c

--- drivers/net/ethernet/kalray/k1c-net-fs.c
+++ drivers/net/ethernet/kalray/k1c-net-fs.c
@@ -20,11 +20,10 @@ static ssize_t s##_attr_show(struct kobject *kobj, \
 { \
 	struct sysfs_##s##_entry *entry = container_of(attr, \
 					 struct sysfs_##s##_entry, attr); \
-	struct k1c_eth_lane_cfg *cfg = container_of(kobj, \
-					struct k1c_eth_lane_cfg, s.kobj); \
+	struct k1c_eth_##s *p = container_of(kobj, struct k1c_eth_##s, kobj); \
 	if (!entry->show) \
 		return -EIO; \
-	return entry->show(cfg, buf); \
+	return entry->show(p, buf); \
 }
 
 #define SYSFS_STRUCT_STORE(s) \
@@ -33,40 +32,63 @@ static ssize_t s##_attr_store(struct kobject *kobj, \
 { \
 	struct sysfs_##s##_entry *entry = container_of(attr, \
 					 struct sysfs_##s##_entry, attr); \
+	struct k1c_eth_##s *p = container_of(kobj, struct k1c_eth_##s, kobj); \
 	struct k1c_eth_lane_cfg *cfg = container_of(kobj, \
-					struct k1c_eth_lane_cfg, s.kobj); \
+					    struct k1c_eth_lane_cfg, s.kobj); \
 	if (!entry->store) \
 		return -EIO; \
-	return entry->store(cfg, buf, count); \
+	return entry->store(cfg, p, buf, count); \
+}
+
+#define SYSFS_STRUCT_IDX_STORE(s) \
+static ssize_t s##_attr_store(struct kobject *kobj, struct attribute *attr, \
+			      const char *buf, size_t count) \
+{ \
+	struct sysfs_##s##_entry *entry = container_of(attr, \
+					 struct sysfs_##s##_entry, attr); \
+	struct k1c_eth_##s *p = container_of(kobj, struct k1c_eth_##s, kobj); \
+	struct k1c_eth_lane_cfg *cfg = container_of(kobj, \
+					struct k1c_eth_lane_cfg, s[0].kobj); \
+	if (!entry->store) \
+		return -EIO; \
+	return entry->store(cfg, p, buf, count); \
 }
 
 #define DECLARE_SYSFS_ENTRY(s) \
 struct sysfs_##s##_entry { \
 	struct attribute attr; \
-	ssize_t (*show)(struct k1c_eth_lane_cfg *cfg, char *buf); \
-	ssize_t (*store)(struct k1c_eth_lane_cfg *cfg, const char *buf, \
-			 size_t s); \
+	ssize_t (*show)(struct k1c_eth_##s *p, char *buf); \
+	ssize_t (*store)(struct k1c_eth_lane_cfg *cfg, struct k1c_eth_##s *p, \
+			 const char *buf, size_t s); \
 }
 
-#define STRUCT_SYSFS_ENTRY(s) \
-SYSFS_STRUCT_SHOW(s) \
-SYSFS_STRUCT_STORE(s) \
-	const struct sysfs_ops s##_sysfs_ops = { \
+#define SYSFS_TYPES(s) \
+const struct sysfs_ops s##_sysfs_ops = { \
 	.show  = s##_attr_show, \
-	.store  = s##_attr_store, \
+	.store = s##_attr_store, \
 }; \
 struct kobj_type s##_ktype = { \
 	.sysfs_ops = &s##_sysfs_ops, \
 	.default_attrs = s##_attrs, \
 }
 
+#define STRUCT_SYSFS_ENTRY(s) \
+SYSFS_STRUCT_SHOW(s) \
+SYSFS_STRUCT_STORE(s) \
+SYSFS_TYPES(s)
+
+#define STRUCT_SYSFS_IDX_ENTRY(s) \
+SYSFS_STRUCT_SHOW(s) \
+SYSFS_STRUCT_IDX_STORE(s) \
+SYSFS_TYPES(s)
+
 #define FIELD_RW_ENTRY(s, f, min, max) \
-static ssize_t f##_show(struct k1c_eth_lane_cfg *cfg, char *buf) \
+static ssize_t f##_show(struct k1c_eth_##s *p, char *buf) \
 { \
-	return scnprintf(buf, STR_LEN, "%i\n", cfg->s.f); \
+	return scnprintf(buf, STR_LEN, "%i\n", p->f); \
 } \
-static ssize_t f##_store(struct k1c_eth_lane_cfg *cfg, \
-		const char *buf, size_t count) \
+static ssize_t f##_store(struct k1c_eth_lane_cfg *cfg, struct k1c_eth_##s *p, \
+			 const char *buf, size_t count) \
 { \
 	ssize_t ret; \
 	unsigned int val; \
@@ -75,7 +97,7 @@ static ssize_t f##_store(struct k1c_eth_lane_cfg *cfg, \
 		return ret; \
 	if (val < min || val > max) \
 		return -EINVAL; \
-	cfg->s.f = val; \
+	p->f = val; \
 	k1c_eth_##s##_cfg(cfg->hw, cfg); \
 	return count; \
 } \
@@ -83,6 +105,8 @@ static struct sysfs_##s##_entry f##_attr = __ATTR_RW(f)
 
 DECLARE_SYSFS_ENTRY(lb_f);
 DECLARE_SYSFS_ENTRY(tx_f);
+DECLARE_SYSFS_ENTRY(pfc_f);
+DECLARE_SYSFS_ENTRY(cl_f);
 
 FIELD_RW_ENTRY(lb_f, default_dispatch_policy,
 	       0, DEFAULT_DISPATCH_POLICY_NB);
@@ -119,8 +143,36 @@ static struct attribute *tx_f_attrs[] = {
 	NULL,
 };
 
+FIELD_RW_ENTRY(pfc_f, global_release_level, 0, K1C_ETH_MAX_LEVEL);
+FIELD_RW_ENTRY(pfc_f, global_drop_level, 0, K1C_ETH_MAX_LEVEL);
+FIELD_RW_ENTRY(pfc_f, global_alert_level, 0, K1C_ETH_MAX_LEVEL);
+FIELD_RW_ENTRY(pfc_f, global_pfc_en, 0, 1);
+FIELD_RW_ENTRY(pfc_f, global_pause_en, 0, 1);
+
+static struct attribute *pfc_f_attrs[] = {
+	&global_release_level_attr.attr,
+	&global_drop_level_attr.attr,
+	&global_alert_level_attr.attr,
+	NULL,
+};
+
+FIELD_RW_ENTRY(cl_f, release_level, 0, K1C_ETH_MAX_LEVEL);
+FIELD_RW_ENTRY(cl_f, drop_level, 0, K1C_ETH_MAX_LEVEL);
+FIELD_RW_ENTRY(cl_f, alert_level, 0, K1C_ETH_MAX_LEVEL);
+FIELD_RW_ENTRY(cl_f, pfc_ena, 0, 1);
+
+static struct attribute *cl_f_attrs[] = {
+	&release_level_attr.attr,
+	&drop_level_attr.attr,
+	&alert_level_attr.attr,
+	&pfc_ena_attr.attr,
+	NULL,
+};
+
 STRUCT_SYSFS_ENTRY(lb_f);
 STRUCT_SYSFS_ENTRY(tx_f);
+STRUCT_SYSFS_ENTRY(pfc_f);
+STRUCT_SYSFS_IDX_ENTRY(cl_f);
 
 /**
  * struct sysfs_type
@@ -139,6 +191,32 @@ static const struct sysfs_type t[] = {
 		.type = &lb_f_ktype },
 	{.name = "tx", .offset = offsetof(struct k1c_eth_lane_cfg, tx_f.kobj),
 		.type = &tx_f_ktype },
+	{.name = "pfc", .offset = offsetof(struct k1c_eth_lane_cfg, pfc_f.kobj),
+		.type = &pfc_f_ktype },
+	{.name = "pfc_cl_0",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[0].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_1",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[1].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_2",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[2].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_3",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[3].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_4",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[4].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_5",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[5].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_6",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[6].kobj),
+		.type = &cl_f_ktype },
+	{.name = "pfc_cl_7",
+		.offset = offsetof(struct k1c_eth_lane_cfg, cl_f[7].kobj),
+		.type = &cl_f_ktype },
 };
 
 static int k1c_eth_kobject_add(struct net_device *netdev,
