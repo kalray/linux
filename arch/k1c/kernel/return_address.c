61a6aedcc4621cda3989d5f89abb2ef10f2db2e3
Clement Leger <cleger@kalray.eu>
2019-11-07 14:42:43 +0100

    k1c: Add return_address implementation
    Summary:
    Implement return address based (almost completely) on arm64 code.
    This uses the backtrace mecanism implemented with stack frame to iterate
    on the stack frames until we hit the desired level.
    This is used by all CALLER_ADDRx macro to get caller address as
    indicated by their name.
    
    Ref T10724
    
    Depends On D2009
    
    Test Plan: Test will added added in coolval module.
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: alfred, #linux_coolidge_cc
    
    Maniphest Tasks: T10724
    
    Differential Revision: https://phab.kalray.eu/D2010

---
+++ arch/k1c/kernel/return_address.c
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * This code is taken from arm64 port
+ *
+ * Copyright (C) 2013 Linaro Limited
+ * Author: AKASHI Takahiro <takahiro.akashi@linaro.org>
+ */
+
+#include <linux/export.h>
+#include <linux/ftrace.h>
+#include <linux/kprobes.h>
+
+#include <asm/stacktrace.h>
+
+struct return_address_data {
+	unsigned int level;
+	void *addr;
+};
+
+static bool save_return_addr(unsigned long pc, void *d)
+{
+	struct return_address_data *data = d;
+
+	/* We hit the desired level, return the address */
+	if (data->level == 0) {
+		data->addr = (void *) pc;
+		return true;
+	}
+
+	data->level--;
+	return false;
+}
+NOKPROBE_SYMBOL(save_return_addr);
+
+void *return_address(unsigned int level)
+{
+	struct return_address_data data;
+	struct stackframe frame;
+
+	/* Skip this function + caller */
+	data.level = level + 2;
+	data.addr = NULL;
+
+	start_stackframe(&frame,
+			 (unsigned long) __builtin_frame_address(0),
+			 (unsigned long) return_address);
+	walk_stackframe(current, &frame, save_return_addr, &data);
+
+	if (!data.level)
+		return data.addr;
+	else
+		return NULL;
+}
+EXPORT_SYMBOL_GPL(return_address);
+NOKPROBE_SYMBOL(return_address);
