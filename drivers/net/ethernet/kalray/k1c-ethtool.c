223d808423d82ef2597224dafa9438eb92e700ac
Benjamin Mugnier <bmugnier@kalray.eu>
2019-11-29 16:53:16 +0100

    k1c-net: Program parsers with ethtool
    Summary:
    This commit allows ethtool -U/-N flow_type to set rules in parsers.
    It uses the location parameter as an index for k1c parsers, only one
    rule per parser. The driver will override any duplicated parser rule.
    Currently support get and set for TCP ports only.
    Action has been restricted to -1 for drop policy, and 0 (default) for
    round robin poliy. Hash lut will be done separately.
    
    Depends on D1375 D2069
    
    Ref T11035
    
    Test Plan:
    ```
    ethtool -U eth0 flow-type tcp4 dst-port 22 loc 0
    ethtool -u eth0
    ethtool -U eth0 flow-type tcp4 dst-port 22 loc 1
    ethtool -U eth0 flow-type tcp4 dst-port 23 loc 0
    ethtool -u eth0
    ethtool -U eth0 delete 0
    ethtool -u eth0
    ethtool -U eth0 flow-type tcp4 dst-port 23 loc 0 action -1
    ethtool -u eth0
    ethtool -U eth0 delete 0
    ethtool -U eth0 delete 1
    ```
    
    Reviewers: O39 ethernet, O51 Linux Coolidge, cleger, tcostis
    
    Reviewed By: O39 ethernet, O51 Linux Coolidge, cleger, tcostis
    
    Subscribers: jmaselbas, tcostis, vchardon, cleger, alfred, #linux_coolidge_cc
    
    Maniphest Tasks: T11035
    
    Differential Revision: https://phab.kalray.eu/D2263
    
    Conflicts:
    	drivers/net/ethernet/kalray/k1c-net-parsing.c

--- drivers/net/ethernet/kalray/k1c-ethtool.c
+++ drivers/net/ethernet/kalray/k1c-ethtool.c
@@ -193,15 +193,258 @@ static int k1c_eth_get_sset_count(struct net_device *netdev, int sset)
 	}
 }
 
+static int find_rule(struct k1c_eth_netdev *ndev, int parser_id,
+		struct ethtool_rx_flow_spec **found_rule)
+{
+	struct k1c_eth_parsing *parsing = &ndev->hw->parsing;
+	int i;
+
+	if (parser_id < 0 || parser_id >= K1C_ETH_PARSER_NB) {
+		netdev_err(ndev->netdev, "Invalid parser identifier in location parameter (max: %d)\n",
+				K1C_ETH_PARSER_NB);
+		return -EINVAL;
+	}
+
+	if (!parsing->parsers[parser_id].enabled)
+		return -ENOENT;
+
+	for (i = K1C_NET_LAYER_NB - 1; i >= 0; i--) {
+		struct k1c_eth_filter *filter =
+			&parsing->parsers[parser_id].filters[i];
+		if (filter->rule_spec != NULL) {
+			if (found_rule != NULL)
+				*found_rule = (struct ethtool_rx_flow_spec *)
+					filter->rule_spec;
+			return 0;
+		}
+	}
+
+	return -ENOENT;
+}
+
+static int k1c_eth_get_rule(struct k1c_eth_netdev *ndev,
+		struct ethtool_rxnfc *cmd, int location)
+{
+	int err;
+	struct ethtool_rx_flow_spec *rule_spec, *fsp = &cmd->fs;
+
+	err = find_rule(ndev, location, &rule_spec);
+	if (!err) {
+		memcpy(fsp, rule_spec, sizeof(*rule_spec));
+		return 0;
+	}
+	return -ENOENT;
+}
+
+static int k1c_eth_get_all_rules_loc(struct k1c_eth_netdev *ndev,
+			    struct ethtool_rxnfc *cmd, u32 *rule_locs)
+{
+	int idx = 0;
+	int err = 0;
+	int i;
+
+	cmd->data = ndev->hw->parsing.active_filters_nb;
+	for (i = 0; i < K1C_ETH_PARSER_NB; i++) {
+		err = find_rule(ndev, i, NULL);
+		if (!err)
+			rule_locs[idx++] = i;
+	}
+	if (idx != ndev->hw->parsing.active_filters_nb) {
+		/* This should never happen and there is a bug */
+		netdev_err(ndev->netdev, "Fetched rules number differs from internally saved rule number, this should never happen.\n");
+		return -EINVAL;
+	}
+	return 0;
+}
+
+static inline int delete_rule(struct k1c_eth_netdev *ndev, unsigned int
+		parser_index, enum k1c_eth_layer layer)
+{
+	struct k1c_eth_hw *hw = ndev->hw;
+	struct k1c_eth_filter *filter;
+
+	if (parser_index >= K1C_ETH_PARSER_NB)
+		return -EINVAL;
+
+	filter = &hw->parsing.parsers[parser_index].filters[layer];
+
+	if (filter->desc == NULL)
+		return -EINVAL;
+
+	/* Free hw resource */
+	kfree(filter->desc);
+	filter->desc = NULL;
+	/* Free copied matching ethtool rule */
+	kfree(filter->rule_spec);
+	filter->rule_spec = NULL;
+
+	hw->parsing.parsers[parser_index].enabled = 0;
+
+	hw->parsing.active_filters_nb--;
+
+	return 0;
+}
+
+static int k1c_eth_parse_ethtool_rule(struct k1c_eth_netdev *ndev,
+				struct ethtool_rx_flow_spec *fs,
+				unsigned int parser_index)
+{
+	struct k1c_eth_hw *hw = ndev->hw;
+	enum k1c_eth_layer layer = 0;
+	struct ethtool_rx_flow_spec *rule;
+	int ret;
+
+	switch (fs->flow_type & ~(FLOW_EXT | FLOW_MAC_EXT)) {
+	case TCP_V4_FLOW:
+		{
+		union tcp_filter_desc *tcp_filter = NULL;
+		struct ethtool_tcpip4_spec *l4_mask = &fs->m_u.tcp_ip4_spec;
+		struct ethtool_tcpip4_spec *l4_val  = &fs->h_u.tcp_ip4_spec;
+		int dst_port = ntohs(l4_val->pdst);
+		int dst_mask = ntohs(l4_mask->pdst);
+
+		layer = K1C_NET_LAYER_4;
+		ret = delete_rule(ndev, parser_index,
+				layer);
+		if (ret == 0)
+			netdev_warn(ndev->netdev, "Filter for index %d (layer %d) already present in parser %d, overriding.\n",
+					layer, INDEX_TO_LAYER(layer),
+					parser_index);
+		tcp_filter = kmalloc(sizeof(*tcp_filter),
+				GFP_KERNEL);
+		if (tcp_filter == NULL)
+			return -ENOMEM;
+		memcpy(tcp_filter, &tcp_filter_default,
+				sizeof(*tcp_filter));
+
+		tcp_filter->dst_min_port = dst_port;
+		tcp_filter->dst_max_port = dst_port;
+		tcp_filter->dst_hash_mask = dst_mask;
+		tcp_filter->dst_ctrl = K1C_ETH_CTRL_MATCH_EQUAL;
+
+		hw->parsing.parsers[parser_index].filters[layer].desc =
+			(union filter_desc *) tcp_filter;
+		hw->parsing.parsers[parser_index].enabled = 1;
+		break;
+		}
+	default:
+		netdev_err(ndev->netdev, "Only TCP transport is supported\n");
+		return -EINVAL;
+	}
+
+	/* Copy ethtool rule for retrieving it when needed */
+	rule = kmalloc(sizeof(*rule), GFP_KERNEL);
+	memcpy(rule, fs, sizeof(*rule));
+	hw->parsing.parsers[parser_index].filters[layer].rule_spec =
+		(void *) rule;
+
+	hw->parsing.active_filters_nb++;
+
+	return 0;
+}
+
+static int delete_parser_cfg(struct k1c_eth_netdev *ndev, int location)
+{
+	int i, err, found = 0;
+
+	/* Delete all the parser rules */
+	for (i = 0; i < K1C_NET_LAYER_NB; i++) {
+		err = delete_rule(ndev, location, i);
+		if (err == 0)
+			found = 1;
+	}
+
+	/* If we have not found any matching rule, we exit */
+	if (!found)
+		return -EINVAL;
+
+	/* Disable parser */
+	err = parser_disable(ndev->hw, location);
+	if (err)
+		return err;
+
+	return 0;
+}
+
+static int add_parser_filter(struct k1c_eth_netdev *ndev,
+				 struct ethtool_rxnfc *cmd)
+{
+	int err;
+	int action = cmd->fs.ring_cookie;
+	unsigned int parser_index = cmd->fs.location;
+	enum parser_dispatch_policy dispatch_policy = PARSER_ROUND_ROBIN;
+
+	if (parser_index < 0 || parser_index >= K1C_ETH_PARSER_NB) {
+		netdev_err(ndev->netdev, "Invalid parser identifier in location parameter (max: %d)\n",
+				K1C_ETH_PARSER_NB);
+		return -EINVAL;
+	}
+	if (action < ETHTOOL_RXNTUPLE_ACTION_DROP || action > 0) {
+		netdev_warn(ndev->netdev, "Unsupported action, please use default or -1 for drop policy\n");
+		return -EINVAL;
+	}
+
+	/* Parse flow */
+	err = k1c_eth_parse_ethtool_rule(ndev, &cmd->fs, parser_index);
+	if (err != 0)
+		return -EINVAL;
+
+	/* Check drop action */
+	if (action == ETHTOOL_RXNTUPLE_ACTION_DROP)
+		dispatch_policy = PARSER_DROP;
+
+	/* Write flow to hardware */
+	if (parser_config(ndev->hw, &ndev->cfg, parser_index,
+				ndev->hw->parsing.parsers[parser_index].filters,
+				K1C_NET_LAYER_NB, dispatch_policy) != 0) {
+		delete_parser_cfg(ndev, parser_index);
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
+static int k1c_eth_set_rxnfc(struct net_device *dev, struct ethtool_rxnfc *cmd)
+{
+	struct k1c_eth_netdev *ndev = netdev_priv(dev);
+	int ret = -EOPNOTSUPP;
+
+	switch (cmd->cmd) {
+	case ETHTOOL_SRXCLSRLINS:
+		ret = add_parser_filter(ndev, cmd);
+		break;
+	case ETHTOOL_SRXCLSRLDEL:
+		ret = delete_parser_cfg(ndev, cmd->fs.location);
+		break;
+	default:
+		break;
+	}
+	return ret;
+}
+
 static int k1c_eth_get_rxnfc(struct net_device *netdev,
 			     struct ethtool_rxnfc *cmd, u32 *rule_locs)
 {
+	struct k1c_eth_netdev *ndev = netdev_priv(netdev);
+	struct k1c_eth_hw *hw = ndev->hw;
 	int ret = -EOPNOTSUPP;
 
 	switch (cmd->cmd) {
 	case ETHTOOL_GRXRINGS:
 		cmd->data = RX_DISPATCH_TABLE_ENTRY_ARRAY_SIZE;
 		ret = 0;
+		break;
+	case ETHTOOL_GRXCLSRLCNT:
+		cmd->rule_cnt = hw->parsing.active_filters_nb;
+		cmd->data = ARRAY_SIZE(hw->parsing.parsers) *
+			ARRAY_SIZE(hw->parsing.parsers[0].filters);
+		ret = 0;
+		break;
+	case ETHTOOL_GRXCLSRLALL:
+		ret = k1c_eth_get_all_rules_loc(ndev, cmd, rule_locs);
+		break;
+	case ETHTOOL_GRXCLSRULE:
+		ret = k1c_eth_get_rule(ndev, cmd, cmd->fs.location);
 		break;
 	}
 	return ret;
@@ -282,6 +525,7 @@ static const struct ethtool_ops k1c_ethtool_ops = {
 	.get_strings         = k1c_eth_get_strings,
 	.get_sset_count      = k1c_eth_get_sset_count,
 	.get_rxnfc           = k1c_eth_get_rxnfc,
+	.set_rxnfc           = k1c_eth_set_rxnfc,
 	.get_rxfh_indir_size = k1c_eth_rss_indir_size,
 	.get_rxfh_key_size   = k1c_eth_get_rxfh_key_size,
 	.get_rxfh            = k1c_eth_get_rxfh,
