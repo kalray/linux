70040c466891091b045cfd8f7983d54160aee368
Thomas Costis <tcostis@kalray.eu>
2019-12-04 13:58:13 +0100

    k1c: dma-noc: disable irq per channel by default
    Summary:
    Prevent race condition in smp to avoid releasing queues and while an
    irq is stil pending.
    
    Ref: T10153
    
    Test Plan: modprobe k1c_dma_noc_test
    
    Reviewers: O39 ethernet, O51 Linux Coolidge, bmugnier, cleger
    
    Reviewed By: O39 ethernet, O51 Linux Coolidge, bmugnier, cleger
    
    Subscribers: cleger, alfred, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D2474

--- drivers/dma/k1/k1c-irqs.c
+++ drivers/dma/k1/k1c-irqs.c
@@ -12,17 +12,12 @@
 #include "k1c-dma.h"
 #include "k1c-dma-regs.h"
 
-struct k1c_dma_irq_param {
-	struct k1c_dma_dev *dev;
-	struct k1c_dma_phy *phy;
-};
-
 /* Returns phy with id and type from msi_index starting from 0
  * Assuming phy_id RX [0, K1C_DMA_RX_CHANNEL_NUMBER - 1]
  * phy_id TX [K1C_DMA_RX_CHANNEL_NUMBER,
  *            K1C_DMA_RX_CHANNEL_NUMBER + K1C_DMA_TX_JOB_QUEUE_NUMBER - 1]
  */
-struct k1c_dma_phy *k1c_dma_get_phy_id(struct msi_desc *msi)
+static struct k1c_dma_phy *k1c_dma_get_phy_id(struct msi_desc *msi)
 {
 	struct device *dev = msi_desc_to_dev(msi);
 	struct k1c_dma_dev *d = dev_get_drvdata(dev);
@@ -44,47 +39,54 @@ struct k1c_dma_phy *k1c_dma_get_phy_id(struct msi_desc *msi)
 	return &d->phy[type][idx];
 }
 
-static u64 msi_mb_paddr;
-static u64 msi_mb_dmaaddr;
-
 static void k1c_dma_write_msi_msg(struct msi_desc *msi, struct msi_msg *msg)
 {
 	struct device *dev = msi_desc_to_dev(msi);
-	struct k1c_dma_phy *phy;
-	u64 new_mb_paddr = 0;
+	struct k1c_dma_dev *d = dev_get_drvdata(dev);
+	struct k1c_dma_phy *phy = NULL;
+	u64 mb_dmaaddr = 0;
+	int i = 0;
 
-	dev_dbg(dev, "address_lo: 0x%x, address_hi: 0x%x, data: 0x%x\n",
-	   msg->address_lo, msg->address_hi, msg->data);
-	phy = k1c_dma_get_phy_id(msi);
-	if (phy) {
-		new_mb_paddr = (u64)(msg->address_hi) << 32 | msg->address_lo;
-		/* Remap only if needed */
-		if (msi_mb_paddr != new_mb_paddr) {
-			dev_dbg(dev, "new_mb_paddr: 0x%llx new_mb_paddr: 0x%llx\n",
-				 msi_mb_paddr, new_mb_paddr);
-			msi_mb_dmaaddr = dma_map_resource(dev, new_mb_paddr,
-							  sizeof(u64),
-							  DMA_FROM_DEVICE, 0);
-			msi_mb_paddr = new_mb_paddr;
+	for (i = 0; i < K1C_DMA_RX_CHANNEL_NUMBER; ++i) {
+		phy = &d->phy[K1C_DMA_DIR_TYPE_RX][i];
+		if (phy->msi_cfg.msi_index == msi->platform.msi_index)
+			break;
+	}
+	if (i == K1C_DMA_RX_CHANNEL_NUMBER) {
+		for (i = 0; i < K1C_DMA_TX_JOB_QUEUE_NUMBER; ++i) {
+			phy = &d->phy[K1C_DMA_DIR_TYPE_TX][i];
+			if (phy->msi_cfg.msi_index == msi->platform.msi_index)
+				break;
 		}
-		phy->msi_mb_paddr = msi_mb_dmaaddr;
-		phy->msi_data = msg->data;
-		phy->irq = msi->irq;
+	}
+
+	if (phy->msi_cfg.msi_index == msi->platform.msi_index) {
+		mb_dmaaddr = (u64)(msg->address_hi) << 32 | msg->address_lo;
+		/* Called from devm_free_irq */
+		if (!mb_dmaaddr)
+			return;
+		dev_dbg(dev, "%s msi_index: %d dma map mb_dmaaddr: 0x%llx dir: %d\n",
+			__func__, phy->msi_cfg.msi_index, mb_dmaaddr, phy->dir);
+
+		phy->msi_cfg.msi_mb_dmaaddr = mb_dmaaddr;
+		phy->msi_cfg.msi_data = msg->data;
+	} else {
+		dev_err(dev, "%s phy not found\n", __func__);
 	}
 }
 
 static irqreturn_t k1c_dma_irq_handler(int chirq, void *arg)
 {
-	struct k1c_dma_irq_param *p = arg;
-	struct k1c_dma_dev *dev = p->dev;
-	struct k1c_dma_phy *phy = p->phy;
+	struct k1c_dma_phy *phy = (struct k1c_dma_phy *)arg;
+	struct tasklet_struct *task = (struct tasklet_struct *)phy->msi_cfg.ptr;
 
 	/* Update software counters to match hw ones */
 	u64 comp_count = k1c_dma_get_comp_count(phy);
 
 	/* Schedule a tasklet to complete descriptors and push new desc */
 	if (phy->comp_count < comp_count) {
-		tasklet_schedule(&dev->task);
+		if (task)
+			tasklet_schedule(task);
 		if (phy->irq_handler) {
 			phy->comp_count = comp_count;
 			phy->irq_handler(phy->irq_data);
@@ -96,16 +98,28 @@ static irqreturn_t k1c_dma_irq_handler(int chirq, void *arg)
 	return IRQ_HANDLED;
 }
 
+int k1c_dma_request_irq(struct k1c_dma_phy *phy)
+{
+	return devm_request_irq(phy->dev, phy->msi_cfg.irq,
+			       k1c_dma_irq_handler, 0, NULL, phy);
+}
+
+void k1c_dma_free_irq(struct k1c_dma_phy *phy)
+{
+	devm_free_irq(phy->dev, phy->msi_cfg.irq, phy);
+}
+
 int k1c_dma_request_msi(struct platform_device *pdev)
 {
 	struct k1c_dma_dev *dev = platform_get_drvdata(pdev);
-	struct msi_desc *failed_desc = NULL;
+	struct k1c_dma_phy *phy;
 	struct msi_desc *msi;
 	int rc;
 
 	if (!dev)
 		return -EINVAL;
 
+	/* MSI for all those irq must be on 1 page only */
 	rc = platform_msi_domain_alloc_irqs(&pdev->dev,
 					    K1C_DMA_RX_CHANNEL_NUMBER +
 					    K1C_DMA_TX_JOB_QUEUE_NUMBER,
@@ -115,50 +129,16 @@ int k1c_dma_request_msi(struct platform_device *pdev)
 			 "platform_msi_domain_alloc_irqs failed\n");
 		return rc;
 	}
-
 	for_each_msi_entry(msi, &pdev->dev) {
-		struct k1c_dma_irq_param *p = devm_kzalloc(&pdev->dev,
-						  sizeof(*p), GFP_KERNEL);
-		if (!p)
-			break;
-		p->dev = dev;
-		p->phy = k1c_dma_get_phy_id(msi);
-		rc = devm_request_irq(&pdev->dev, msi->irq,
-				      k1c_dma_irq_handler,
-				      0, NULL, p);
-		if (rc) {
-			failed_desc = msi;
-			break;
-		}
+		phy = k1c_dma_get_phy_id(msi);
+		phy->msi_cfg.irq = msi->irq;
+		phy->msi_cfg.msi_index = msi->platform.msi_index;
 	}
 
-	if (rc) {
-		/* free allocated MSI interrupts above */
-		for_each_msi_entry(msi, &pdev->dev) {
-			if (msi == failed_desc)
-				break;
-			devm_free_irq(&pdev->dev, msi->irq, dev);
-		}
-		k1c_dma_free_msi(pdev);
-	}
-	if (rc)
-		dev_warn(&pdev->dev,
-			 "failed to request MSI irq, fallback to wired IRQ\n");
 	return rc;
 }
 
 void k1c_dma_free_msi(struct platform_device *pdev)
 {
-	struct msi_desc *msi;
-	struct k1c_dma_phy *phy;
-	struct device *dev;
-
-	for_each_msi_entry(msi, &pdev->dev) {
-		dev = msi_desc_to_dev(msi);
-		phy = k1c_dma_get_phy_id(msi);
-		if (phy)
-			dma_unmap_resource(dev, phy->msi_mb_paddr,
-					 sizeof(u64), DMA_FROM_DEVICE, 0);
-	}
 	platform_msi_domain_free_irqs(&pdev->dev);
 }
