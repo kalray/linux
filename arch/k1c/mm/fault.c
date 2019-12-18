38255184abcb63bfcbeedb299af5ae3f663af447
Clement Leger <cleger@kalray.eu>
2019-12-18 14:44:43 +0100

    k1c: Add calls to perf_sw_events
    Summary: Add missing calls to perf_sw_events to track faults.
    
    Test Plan: Not much except checking with gdb that flag is correctly set.
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D3015

--- arch/k1c/mm/fault.c
+++ arch/k1c/mm/fault.c
@@ -10,6 +10,7 @@
 #include <linux/uaccess.h>
 #include <linux/kernel.h> // only needed to panic
 #include <linux/printk.h>
+#include <linux/perf_event.h>
 #include <linux/sched.h>
 #include <linux/sched/debug.h>
 #include <linux/sched/signal.h>
@@ -106,6 +107,8 @@ void do_page_fault(uint64_t es, uint64_t ea, struct pt_regs *regs)
 	/* By default we retry and fault task can be killed */
 	flags = FAULT_FLAG_ALLOW_RETRY | FAULT_FLAG_KILLABLE;
 
+	perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS, 1, regs, ea);
+
 retry:
 	down_read(&mm->mmap_sem);
 
@@ -174,10 +177,15 @@ good_area:
 
 	if (flags & FAULT_FLAG_ALLOW_RETRY) {
 		/* To avoid updating stats twice for retry case */
-		if (fault & VM_FAULT_MAJOR)
+		if (fault & VM_FAULT_MAJOR) {
 			current->maj_flt++;
-		else
+			perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS_MAJ, 1, regs,
+				      ea);
+		} else {
 			current->min_flt++;
+			perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS_MIN, 1, regs,
+				      ea);
+		}
 
 		if (fault & VM_FAULT_RETRY) {
 			/* Clear FAULT_FLAG_ALLOW_RETRY to avoid any risk
