53ee3222b3a0ca6b30c2eeee8326a75dfec24f7d
Clement Leger <cleger@kalray.eu>
2019-12-20 21:21:11 +0100

    k1c: move insn patching out of jump_label
    Summary:
    Since instruction patching will be used outside of just jump label, move
    the patching specific part into a seperate insns.c file. During this
    refactoring, since GOTO will also be used for code patching later
    (kernel rwx and refill routine), use generated insns from processor
    description to be available outside of jump label.
    The instructions definitions will be extended later with more
    instructions (eBPF JIT support for instance).
    
    Test Plan:
    Checked (using gdb) that generated insn for goto is ok. Executed whole boot
    to check that it executes correctly.
    
    Reviewers: O51 Linux Coolidge, mpoulhies, vchardon, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, mpoulhies, vchardon, gthouvenin
    
    Subscribers: gthouvenin, vchardon, ysionneau, alfred, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D2265

--- arch/k1c/kernel/jump_label.c
+++ arch/k1c/kernel/jump_label.c
@@ -15,80 +15,8 @@
 #include <linux/types.h>
 
 #include <asm/cacheflush.h>
-
-/*
- * 'goto' insn uses the following encoding:
- * |   P   |   00   |   01   |  pcrel  |
- * | 1 bit | 2 bits | 2 bits | 27 bits |
- *
- * Parallel bit (P) is set to 0 for last instruction of a bundle.
- * Next PC is computed by adding pcrel to the current PC after being sign
- * extended and scaled by 4.
- */
-#define GOTO_OFFSET_MASK	(BIT(27) - 1)
-#define GOTO_SIGN_MASK		(~(GOTO_OFFSET_MASK >> 1))
-#define GOTO_ALIGN_MASK		0x3
-
-#define GOTO_INSN 0x10000000
-#define NOP_INSN 0x7FFFFFFF
-
-struct insn_patch {
-	atomic_t cpu_count;
-	u32 *addr;
-	u32 code;
-};
-
-static void write_u32_insn_code(u32 insn_code, u32 *insn_addr)
-{
-	unsigned long laddr = (unsigned long) insn_addr;
-
-	/* Patch insn */
-	*insn_addr = insn_code;
-
-	/* Flush & invalidate icache to reload instructions from memory */
-	local_flush_icache_range(laddr, laddr + JUMP_LABEL_NOP_SIZE);
-}
-
-static int patch_jump_label_stop_machine(void *data)
-{
-	struct insn_patch *ip = data;
-
-	if (atomic_inc_return(&ip->cpu_count) == 1) {
-		write_u32_insn_code(ip->code, ip->addr);
-		/* Additionnal up to release other processors */
-		atomic_inc(&ip->cpu_count);
-	} else {
-		unsigned long insn_addr = (unsigned long) ip->addr;
-		/* Wait for first processor to update insn code */
-		while (atomic_read(&ip->cpu_count) <= num_online_cpus())
-			cpu_relax();
-
-		/* Simply invalidate L1 I-cache to reload from L2 or memory */
-		l1_inval_icache_range(insn_addr,
-				      insn_addr + JUMP_LABEL_NOP_SIZE);
-	}
-	return 0;
-}
-
-static void patch_jump_label(u32 insn_code, u32 *insn_addr)
-{
-	if (IS_ENABLED(CONFIG_SMP)) {
-		struct insn_patch ip = {
-			.cpu_count = ATOMIC_INIT(0),
-			.addr = insn_addr,
-			.code = insn_code,
-		};
-
-		stop_machine_cpuslocked(patch_jump_label_stop_machine,
-					&ip, NULL);
-	} else {
-		unsigned long flags;
-
-		local_irq_save(flags);
-		write_u32_insn_code(insn_code, insn_addr);
-		local_irq_restore(flags);
-	}
-}
+#include <asm/insns.h>
+#include <asm/insns_defs.h>
 
 void arch_jump_label_transform(struct jump_entry *e,
 			       enum jump_label_type type)
@@ -98,23 +26,14 @@ void arch_jump_label_transform(struct jump_entry *e,
 	u32 *insn_addr = (u32 *) jump_entry_code(e);
 
 	if (type == JUMP_LABEL_JMP) {
-		/*
-		 * Offset must be aligned on 4 since it will be shifted by 2
-		 * in the goto insn.
-		 */
-		BUG_ON(off & GOTO_ALIGN_MASK);
-		off >>= 2;
+		BUG_ON(K1C_INSN_GOTO_PCREL27_CHECK(off));
 
-		/* Check if remaining offset fits in the 27 bits */
-		BUG_ON(!((off & GOTO_SIGN_MASK) == 0 ||
-			 (off & GOTO_SIGN_MASK) == GOTO_SIGN_MASK));
-
-		insn_code = GOTO_INSN | (off & GOTO_OFFSET_MASK);
+		K1C_INSN_GOTO(&insn_code, K1C_INSN_PARALLEL_EOB, off);
 	} else {
-		insn_code = NOP_INSN;
+		K1C_INSN_NOP(&insn_code, K1C_INSN_PARALLEL_EOB);
 	}
 
-	patch_jump_label(insn_code, insn_addr);
+	k1c_insns_write(&insn_code, JUMP_LABEL_NOP_SIZE, insn_addr);
 }
 
 void arch_jump_label_transform_static(struct jump_entry *entry,
