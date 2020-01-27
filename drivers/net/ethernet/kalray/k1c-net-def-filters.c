a30729f48ecabb5d39fd7e5dbac4e438f57449fe
Benjamin Mugnier <bmugnier@kalray.eu>
2020-01-27 17:19:15 +0100

    k1c-net: Support all protocols in ethtool
    Summary:
    This commit extends ethtool support with TCP port src/dest, IP src/dest,
    masc src/dest.
    It changes the device model by using only one rule for a parser as it is
    meant to be by ethtool.
    
    Test Plan: Userspace programs in coolval
    
    Reviewers: O39 ethernet, O51 Linux Coolidge, cleger, tcostis
    
    Reviewed By: O39 ethernet, O51 Linux Coolidge, cleger, tcostis
    
    Subscribers: cleger, alfred, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D2667

--- drivers/net/ethernet/kalray/k1c-net-def-filters.c
+++ drivers/net/ethernet/kalray/k1c-net-def-filters.c
@@ -24,9 +24,9 @@ const union mac_filter_desc mac_filter_default = {
 	.min_max_swap        = 0,
 	.pfc_en              = 0,
 	.vlan_ctrl           = 0,
-	.da_cmp_polarity     = 1,
+	.da_cmp_polarity     = 0,
 	.da                  = 0,
-	.sa_cmp_polarity     = 1,
+	.sa_cmp_polarity     = 0,
 	.sa                  = 0x000000000000ULL,
 	.etype_cmp_polarity  = 0,
 	.etype               = 0x800,
@@ -34,55 +34,14 @@ const union mac_filter_desc mac_filter_default = {
 	.tci1_cmp_polarity   = 0,
 	.tci0                = 0,
 	.tci1                = 0,
-	.da_mask             = 0xFFFFFFFFFFFFULL,
-	.da_hash_mask        = 0xFFFFFFFFFFFFULL,
-	.sa_mask             = 0xFFFFFFFFFFFFULL,
-	.sa_hash_mask        = 0xFFFFFFFFFFFFULL,
+	.da_mask             = 0x000000000000ULL,
+	.da_hash_mask        = 0x000000000000ULL,
+	.sa_mask             = 0x000000000000ULL,
+	.sa_hash_mask        = 0x000000000000ULL,
 	.tci0_mask           = 0,
 	.tci0_hash_mask      = 0,
 	.tci1_mask           = 0,
 	.tci1_hash_mask      = 0,
-};
-
-/* TODO fill with default data */
-const union vxlan_filter_desc vxlan_filter_default = {
-	.word                  = {0},
-	.ptype                 = PTYPE_VXLAN,
-	.add_metadata_index    = 1,
-	.vxlan_header_check_en = 1,
-	.vni_cmp_polarity      = 1,
-	.vni                   = 0xCAFEBE,
-	.vni_mask              = 0xFFFFFF,
-	.vni_hash_mask         = 0xFFFFFF,
-	.skip_length          = 3,
-};
-
-/* TODO fill with default data */
-const union skip_filter_desc skip_filter_default = {
-	.word          = {0},
-	.ptype         = PTYPE_SKIP,
-	.skip_byte_nb  = 20,
-	.skip_length   = 3,
-};
-
-/* TODO fill with default data */
-const union custom_filter_desc custom_filter_default = {
-	.ptype              = PTYPE_CUSTOM,
-	.add_metadata_index = 1,
-	.length             = PARSER_CUSTOM_VAL_NB,
-	.expected_value0    = 0x01020304,
-	.expected_value1    = 0x05060708,
-	.expected_value2    = 0X090A0B0C,
-	.equal_mask0        = 0xFFFFFFFF,
-	.equal_mask1        = 0xFFFFFFFF,
-	.equal_mask2        = 0xFFFFFFFF,
-	.diff_mask0         = 0xFFFFFFFF,
-	.diff_mask1         = 0xFFFFFFFF,
-	.diff_mask2         = 0xFFFFFFFF,
-	.hash_mask0         = 0xFFFFFFFF,
-	.hash_mask1         = 0xFFFFFFFF,
-	.hash_mask2         = 0xFFFFFFFF,
-	.end_of_rule        = 0,
 };
 
 const union ipv4_filter_desc ipv4_filter_default = {
@@ -115,47 +74,45 @@ const union ipv4_filter_desc ipv4_filter_default = {
 	.end_of_rule           = 0,
 };
 
-/* TODO fill with default data */
 const struct ipv6_filter_desc ipv6_filter_default = {
 	.d0 = {
 		.ptype              = PTYPE_IP_V6,
 		.add_metadata_index = 1,
-		.min_max_swap_en    = 1,
-		.tc_cmp_polarity    = 1,
-		.tc                 = 0x99,
-		.tc_mask            = 0xFF,
-		.tc_hash_mask       = 0xFF,
-		.fl_cmp_polarity    = 1,
-		.fl                 = 0xBCDEF,
-		.fl_mask            = 0xFFFFF,
-		.fl_hash_mask       = 0xFFFFF,
-		.nh_cmp_polarity    = 1,
-		.nh                 = 66,
-		.nh_mask            = 0xFF,
-		.nh_hash_mask       = 0xFF,
+		.min_max_swap_en    = 0,
+		.tc_cmp_polarity    = 0,
+		.tc                 = 0x00,
+		.tc_mask            = 0x00,
+		.tc_hash_mask       = 0x00,
+		.fl_cmp_polarity    = 0,
+		.fl                 = 0x00000,
+		.fl_mask            = 0x00000,
+		.fl_hash_mask       = 0x00000,
+		.nh_cmp_polarity    = 0,
+		.nh                 = 0,
+		.nh_mask            = 0x00,
+		.nh_hash_mask       = 0x00,
 		.skip_length        = 2,
 	},
 	.d1 = {
-		.src_cmp_polarity   = 1,
-		.src_lsb            = 0x00000001A0000001ULL,
-		.src_msb            = 0x2011000000000000ULL,
-		.src_lsb_mask       = 0xFFFFFFFFFFFFFFFFULL,
-		.src_msb_mask       = 0xFFFFFFFFFFFFFFFFULL,
-		.src_lsb_hash_mask  = 0xFFFFFFFFFFFFFFFFULL,
-		.src_msb_hash_mask  = 0xFFFFFFFFFFFFFFFFULL,
+		.src_cmp_polarity   = 0,
+		.src_lsb            = 0x0000000000000000ULL,
+		.src_msb            = 0x0000000000000000ULL,
+		.src_lsb_mask       = 0x0000000000000000ULL,
+		.src_msb_mask       = 0x0000000000000000ULL,
+		.src_lsb_hash_mask  = 0x0000000000000000ULL,
+		.src_msb_hash_mask  = 0x0000000000000000ULL,
 	},
 	.d2 = {
-		.dst_cmp_polarity  = 1,
-		.dst_lsb           = 0x00000001A0000002ULL,
-		.dst_msb           = 0x2011000000000000ULL,
-		.dst_lsb_mask      = 0xFFFFFFFFFFFFFFFFULL,
-		.dst_msb_mask      = 0xFFFFFFFFFFFFFFFFULL,
-		.dst_lsb_hash_mask = 0xFFFFFFFFFFFFFFFFULL,
-		.dst_msb_hash_mask = 0xFFFFFFFFFFFFFFFFULL,
+		.dst_cmp_polarity  = 0,
+		.dst_lsb           = 0x0000000000000000ULL,
+		.dst_msb           = 0x0000000000000000ULL,
+		.dst_lsb_mask      = 0x0000000000000000ULL,
+		.dst_msb_mask      = 0x0000000000000000ULL,
+		.dst_lsb_hash_mask = 0x0000000000000000ULL,
+		.dst_msb_hash_mask = 0x0000000000000000ULL,
 	},
 };
 
-/* TODO fill with default data */
 const union udp_filter_desc udp_filter_default = {
 	.word                  = {0},
 	.ptype                 = PTYPE_UDP,
@@ -192,63 +149,4 @@ const union tcp_filter_desc tcp_filter_default = {
 	.flags_mask            = 0x000,
 	.flags_hash_mask       = 0x000,
 	.skip_length           = 2,
-};
-
-const union nvme_tcp_filter_desc nvme_tcp_filter_default = {
-	.word               = {0},
-	.ptype              = PTYPE_NVME_TCP,
-	.add_metadata_index = 1,
-	.check_ddgst        = 1,
-	.expected_pdu_type  = 1,
-	.pdu_hash_en        = 1,
-	.flags_cmp_polarity = 1,
-	.expected_flags     = 0xF0,
-	.flag_mask          = 0xFF,
-	.skip_length        = 3,
-};
-
-/* TODO fill with default data */
-const union roce_filter_desc roce_filter_default = {
-	.word               = {0},
-	.ptype              = PTYPE_ROCE,
-	.add_metadata_index = 1,
-	.roce_version       = 1,
-	.check_icrc         = 1,
-	.qpair_cmp_polarity = 1,
-	.qpair              = 0xABCDEF,
-	.qpair_mask         = 0xFFFFFF,
-	.qpair_hash_mask    = 0xFFFFFF,
-	.skip_length        = 3,
-};
-
-/* TODO fill with default data */
-const union mpls_filter_desc mpls_filter_default = {
-	.word               = {0},
-	.ptype              = PTYPE_MPLS,
-	.add_metadata_index = 1,
-	.label_cmp_polarity = 1,
-	.label              = 0xABCDE,
-	.label_mask         = 0xFFFFF,
-	.label_hash_mask    = 0xFFFFF,
-	.tc_cmp_polarity    = 1,
-	.tc                 = 5,
-	.tc_mask            = 0x7,
-	.tc_hash_mask       = 0x7,
-	.skip_length        = 3,
-};
-
-/* TODO fill with default data */
-const union gre_filter_desc gre_filter_default = {
-	.word                  = {0},
-	.ptype                 = PTYPE_GRE,
-	.add_metadata_index    = 1,
-	.protocol_cmp_polarity = 1,
-	.protocol              = 0x800,
-	.protocol_mask         = 0xFFFF,
-	.protocol_hash_mask    = 0xFFFF,
-	.key_cmp_polarity      = 1,
-	.key                   = 0xBEEF,
-	.key_mask              = 0xFFFF,
-	.key_hash_mask         = 0xFFFF,
-	.skip_length           = 3,
 };
