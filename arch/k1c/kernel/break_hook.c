901ceabd97597af1adca8fb382f3a40c83b915de
Clement Leger <cleger@kalray.eu>
2020-01-29 17:09:00 +0100

    k1c: Introduce break hooks and convert BUG
    Summary:
    On k1c, we currently rely on errop with a particular immediate to catch BUG.
    In near future, we will have KGDB and kprobes support which will use the
    same mecanism.
    Using set $vsfr is currently used by breakpoints and is a bit more
    reliable than using an errop so the break hook mecanism is based on
    that rather than errop.
    To make this mecanism easily available for such handlers, introduce
    break hook mecanism which allows to register handler for break. This
    will also be adapted to V2 when break instruction will be reintroduced.
    This mecanism is similar to the one available on arm64 but without
    different method for each break hook. A single break hook registration
    function is available and will add the hook correctly depending on the
    type of break hook. To differentiate the cause of break, we use the
    source register of the set operation.
    Once triggered, break_hook_handler is now invoked in order to
    distinguish the cause of the break. If a hook is detected for the break,
    then, hook handler will be called. BUG and breakpoint implementation
    have been modified to use break hooks instead of previous custom mecanism.
    
    Ref T10482, T11647
    
    Test Plan: Add A BUG_ON in some code and checked that it works as expected.
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: gthouvenin, #linux_coolidge_cc
    
    Maniphest Tasks: T11647, T10482
    
    Differential Revision: https://phab.kalray.eu/D3150

---
+++ arch/k1c/kernel/break_hook.c
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2020 Kalray Inc.
+ */
+
+#include <linux/list.h>
+#include <linux/rculist.h>
+#include <linux/spinlock.h>
+#include <linux/rcupdate.h>
+
+#include <asm/insns.h>
+#include <asm/traps.h>
+#include <asm/processor.h>
+#include <asm/break_hook.h>
+
+static DEFINE_SPINLOCK(debug_hook_lock);
+static LIST_HEAD(user_break_hook);
+static LIST_HEAD(kernel_break_hook);
+
+void k1c_skip_break_insn(struct pt_regs *regs)
+{
+	regs->spc += K1C_BREAK_INSN_SIZE;
+}
+
+int break_hook_handler(uint64_t es, uint64_t ea, struct pt_regs *regs)
+{
+	int (*fn)(u64 es, u64 ea, struct pt_regs *regs) = NULL;
+	struct break_hook *hook;
+	struct list_head *list;
+	unsigned long flags;
+	u32 idx;
+
+	if (trap_sfri(es) != K1C_TRAP_SFRI_SET ||
+	    trap_sfrp(es) != K1C_SFR_VSFR0)
+		return BREAK_HOOK_ERROR;
+
+	idx = trap_gprp(es);
+	list = user_mode(regs) ? &user_break_hook : &kernel_break_hook;
+
+	local_irq_save(flags);
+	list_for_each_entry_rcu(hook, list, node) {
+		if (idx == hook->id)
+			fn = hook->handler;
+	}
+	local_irq_restore(flags);
+
+	if (fn)
+		return fn(es, ea, regs);
+
+	return BREAK_HOOK_ERROR;
+}
+
+void break_hook_register(struct break_hook *brk_hook)
+{
+	struct list_head *list;
+
+	if (brk_hook->mode == BREAK_MODE_USER)
+		list = &user_break_hook;
+	else
+		list = &kernel_break_hook;
+
+	spin_lock(&debug_hook_lock);
+	list_add_rcu(&brk_hook->node, list);
+	spin_unlock(&debug_hook_lock);
+}
+
+void break_hook_unregister(struct break_hook *brk_hook)
+{
+	spin_lock(&debug_hook_lock);
+	list_del_rcu(&brk_hook->node);
+	spin_unlock(&debug_hook_lock);
+	synchronize_rcu();
+}
