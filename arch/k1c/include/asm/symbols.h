72168793f88a9332b25002a527e7687ee4b92977
Clement Leger <cleger@kalray.eu>
2020-01-29 22:59:25 +0100

    k1c: implement strict kernel RWX
    Summary:
    Currently, we used a single mapping for the whole kernel. While being
    convenient for performance, there is no security at all and CPU can
    execute data or text can be overwritten by mistake without no barrier at
    all.
    In order to allow setting up this protection, various mechanism have been
    implemented. The kernel text and data are now mapped in the page table
    with correct policy. Additionally, memblocks are also mapped using RW
    policy. This allows to protect the kernel code and data.
    In order to reduce pressure on page table walking, use huge pages
    whenever possible. All page sizes are used if possible to fill in the
    page table.
    Once page table is filled, the refill handler is patched to avoid using
    the "performance" refill handling kernel addresses. The patched
    instruction allows to jump to "standard" refill handler and handle the
    fault by walking the page table.
    Finally, the full kernel mapping stored in LTLB is replaced by a minimal
    mapping with only exceptions handlers. This allows to always keep the
    refill routines in LTLB which is obviously necessary to allow them being
    present on exception taken.
    
    Ref T10855
    
    Test Plan:
    Run kernel validation completely. Must still be run on extensive testsuite to
    validate there is no regression/
    
    Reviewers: O51 Linux Coolidge, vchardon, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, vchardon, gthouvenin
    
    Subscribers: vchardon, #linux_coolidge_cc
    
    Maniphest Tasks: T10855
    
    Differential Revision: https://phab.kalray.eu/D2395

---
+++ arch/k1c/include/asm/symbols.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2019 Kalray Inc.
+ */
+#ifndef _ASM_K1C_SYMBOLS_H
+#define _ASM_K1C_SYMBOLS_H
+
+/* Symbols to patch TLB refill handler */
+extern char k1c_perf_tlb_refill[], k1c_std_tlb_refill[];
+
+#endif
