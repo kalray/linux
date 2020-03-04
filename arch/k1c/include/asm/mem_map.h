e02557a58fb6f7325f8c034d0865abafa82301dc
Clement Leger <cleger@kalray.eu>
2020-03-04 16:53:42 +0100

    k1c: Use remaining space for vmalloc
    Summary:
    Previously, we were using a fixed size vmalloc space. This revealed to
    be too tight when using systems with a big number of devices. In order
    to be more flexible, simply use the whole remaining memory starting from
    kernel direct memory base up to fixmap start. While doing this change,
    add a guard of 1G to allow catching erroneous accesses. With the current
    setup, this lead to ~445G of vmalloc space.
    
    Test Plan:
    Run linux valdiation (and particularly modules) and enumerate lot of
    PCIe devices.
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D4398

--- arch/k1c/include/asm/mem_map.h
+++ arch/k1c/include/asm/mem_map.h
@@ -10,8 +10,10 @@
 #define _ASM_K1C_MEM_MAP_H
 
 #include <linux/const.h>
+#include <linux/sizes.h>
 
 #include <asm/page.h>
+#include <asm/fixmap.h>
 
 /**
  * K1C memory mapping defines
@@ -21,17 +23,23 @@
  * All _BASE defines are relative to PAGE_OFFSET
  */
 
+/* Guard between various memory map zones */
+#define MAP_GUARD_SIZE	SZ_1G
+
 /**
  * Kernel direct memory mapping
  */
 #define KERNEL_DIRECT_MEMORY_MAP_BASE	PAGE_OFFSET
 #define KERNEL_DIRECT_MEMORY_MAP_SIZE	UL(0x1000000000)
+#define KERNEL_DIRECT_MEMORY_MAP_END \
+		(KERNEL_DIRECT_MEMORY_MAP_BASE + KERNEL_DIRECT_MEMORY_MAP_SIZE)
 
 /**
- * Vmalloc mapping
+ * Vmalloc mapping (goes from kernel direct memory map up to fixmap start -
+ * guard size)
  */
-#define KERNEL_VMALLOC_MAP_BASE \
-	(KERNEL_DIRECT_MEMORY_MAP_BASE + KERNEL_DIRECT_MEMORY_MAP_SIZE)
-#define KERNEL_VMALLOC_MAP_SIZE	0x40000000
+#define KERNEL_VMALLOC_MAP_BASE (KERNEL_DIRECT_MEMORY_MAP_END + MAP_GUARD_SIZE)
+#define KERNEL_VMALLOC_MAP_SIZE	\
+		(FIXADDR_START - KERNEL_VMALLOC_MAP_BASE - MAP_GUARD_SIZE)
 
 #endif
