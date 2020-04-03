96cac2717d84028bbfec5d0bb63870e5324de168
Clement Leger <cleger@kalray.eu>
2020-04-03 08:47:45 +0200

    kvx: clear power controller wup after set
    Summary:
    In order to allow processor to enable idle modes, we need to clear the
    power controller wakeup. This needs to be done for PE0 after booting and
    for all other PEs avec wakeup.
    
    Ref T11976
    
    Test Plan: Checked that all processor are really in stop state.
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T11976
    
    Differential Revision: https://phab.kalray.eu/D4975

--- arch/kvx/include/asm/pwr_ctrl.h
+++ arch/kvx/include/asm/pwr_ctrl.h
@@ -21,6 +21,7 @@ void kvx_pwr_ctrl_cpu_poweron(unsigned int cpu);
 /* Power controller vector register definitions */
 #define KVX_PWR_CTRL_VEC_OFFSET 0x1000
 #define KVX_PWR_CTRL_VEC_WUP_SET_OFFSET     0x10
+#define KVX_PWR_CTRL_VEC_WUP_CLEAR_OFFSET     0x20
 
 /* Power controller PE reset PC register definitions */
 #define KVX_PWR_CTRL_RESET_PC_OFFSET               0x2000
@@ -33,6 +34,10 @@ void kvx_pwr_ctrl_cpu_poweron(unsigned int cpu);
 #define PWR_CTRL_WUP_SET_OFFSET  \
 		(KVX_PWR_CTRL_VEC_OFFSET + \
 		 KVX_PWR_CTRL_VEC_WUP_SET_OFFSET)
+
+#define PWR_CTRL_WUP_CLEAR_OFFSET  \
+		(KVX_PWR_CTRL_VEC_OFFSET + \
+		 KVX_PWR_CTRL_VEC_WUP_CLEAR_OFFSET)
 
 #define PWR_CTRL_GLOBAL_CONFIG_OFFSET \
 		(KVX_PWR_CTRL_GLOBAL_OFFSET + \
