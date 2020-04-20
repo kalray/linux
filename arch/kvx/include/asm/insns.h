7335a06ac44ed6d8254cfb41ed2adef2a8ad4a2d
Clement Leger <cleger@kalray.eu>
2020-04-20 11:53:20 +0200

    kvx: export kvx_write_insns_nostop
    Summary:
    In KGDB support, kgdb_arch_set_breakpoint needs to set a breakpoint by
    patching code. When this function is called, all CPUs are already stop
    and we must not try to stop them again or they will be unresponsive due
    to interrupts being disabled. Export this function which allow to
    directly write to code without syncronization with other CPUs.
    
    Depends On D5335
    
    Test Plan: Compile
    
    Reviewers: O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D5336

--- arch/kvx/include/asm/insns.h
+++ arch/kvx/include/asm/insns.h
@@ -5,6 +5,8 @@
 #ifndef _ASM_KVX_INSNS_H
 #define _ASM_KVX_INSNS_H
 
+int kvx_insns_write_nostop(u32 *insns, u8 insns_len, u32 *insn_addr);
+
 int kvx_insns_write(u32 *insns, unsigned long insns_len, u32 *addr);
 
 int kvx_insns_read(u32 *insns, unsigned long insns_len, u32 *addr);
