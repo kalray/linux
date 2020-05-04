389c46b15a842aa584c962c8375978b9412560f1
Clement Leger <cleger@kalray.eu>
2020-05-04 23:12:55 +0200

    kvx: add KGDB support
    Summary:
    In order to do debugging on target without having JTAG and allow
    accessing module and other dynamic things, add KGDB support. This
    support is based on break and debug infrastructure to allow catching
    both of these events. The KGDBTS testsuite is passed and it allows
    breaking and single stepping kernel code.
    
    Depends On D5347
    
    Test Plan:
    Execute KGDBTS testsuite, then trig a kdb prompt and tried various
    commands.
    
    Reviewers: O51 Linux Coolidge, mgligor, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: gthouvenin, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D5348

---
+++ arch/kvx/include/asm/kgdb.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2019 Kalray Inc.
+ */
+
+#ifndef _ASM_K1C_KGDB_H
+#define _ASM_K1C_KGDB_H
+
+#include <linux/types.h>
+
+#include <asm/insns.h>
+#include <asm/ptrace.h>
+#include <asm/break_hook.h>
+
+#define CACHE_FLUSH_IS_SAFE  1
+
+#define KGDB_DYN_BREAK_INSN	KVX_BREAK_INSN(BREAK_CAUSE_KGDB_DYN)
+#define KGDB_COMP_BREAK_INSN	KVX_BREAK_INSN(BREAK_CAUSE_KGDB_COMP)
+#define BREAK_INSTR_SIZE	KVX_BREAK_INSN_SIZE
+
+#define GDB_MAX_SFR_REGS	6
+/*
+ * general purpose registers size in bytes.
+ */
+#define GP_REG_BYTES		(GPR_COUNT * REG_SIZE)
+#define DBG_MAX_REG_NUM		(GPR_COUNT + GDB_MAX_SFR_REGS)
+
+/*
+ * Size of I/O buffer for gdb packet.
+ * considering to hold all register contents, size is set
+ */
+#define BUFMAX			2048
+
+/*
+ * Number of bytes required for gdb_regs buffer which is matching exactly the
+ * user_pt_regs structure
+ */
+#define NUMREGBYTES	(DBG_MAX_REG_NUM * REG_SIZE)
+
+
+static inline void arch_kgdb_breakpoint(void)
+{
+	__asm__ __volatile__ (".word " __stringify(KGDB_COMP_BREAK_INSN) "\n");
+}
+
+#endif
