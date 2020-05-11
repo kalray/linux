da0493a21d4055cd2d2a75336c7ae55b379582a9
Clement Leger <cleger@kalray.eu>
2020-05-11 10:02:45 +0200

    kvx: add custom io memcpy
    Summary:
    On kvx, the device are mapped using DEVICE MMU policy which means we
    can not do unaligned accesses. Currently, io memcpy is using the
    standard memcpy which can do unaligned copies. In order to avoid that,
    use the ARM io memcpy code which always make sure it is aligned on a
    proper boundary.
    
    Ref T13150
    
    Test Plan: Check that memcpy_to/from io are compiled.
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T13150
    
    Differential Revision: https://phab.kalray.eu/D6018

--- arch/kvx/include/asm/io.h
+++ arch/kvx/include/asm/io.h
@@ -18,6 +18,19 @@
 
 #define ioremap_nocache(addr, size)	ioremap_prot((addr), (size), _PAGE_KERNEL_NOCACHE)
 
+/*
+ * String version of I/O memory access operations.
+ */
+extern void __memcpy_fromio(void *to, const volatile void __iomem *from,
+			    size_t count);
+extern void __memcpy_toio(volatile void __iomem *to, const void *from,
+			  size_t count);
+extern void __memset_io(volatile void __iomem *dst, int c, size_t count);
+
+#define memset_io(c, v, l)	__memset_io((c), (v), (l))
+#define memcpy_fromio(a, c, l)	__memcpy_fromio((a), (c), (l))
+#define memcpy_toio(c, a, l)	__memcpy_toio((c), (a), (l))
+
 #include <asm-generic/io.h>
 
 extern int devmem_is_allowed(unsigned long pfn);
