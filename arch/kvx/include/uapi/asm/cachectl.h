b5d41c88f69c807810373b141575646ce9eee3d4
Clement Leger <cleger@kalray.eu>
2020-06-15 15:51:07 +0200

    kvx: add cachectl syscall
    Summary:
    Add a cachectl syscall to allow user to invalidate/writeback the cache.
    This syscall handle both virt/phys addresses and allows to
    invalidate/writeback the dcache.
    
    Ref T10801
    
    Depends On D6324
    
    Test Plan: Add a syscall test in coolval
    
    Reviewers: rgauguey, O51 Linux Coolidge, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, gthouvenin
    
    Subscribers: ysionneau, #linux_coolidge_cc
    
    Maniphest Tasks: T10801
    
    Differential Revision: https://phab.kalray.eu/D6325

---
+++ arch/kvx/include/uapi/asm/cachectl.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/*
+ * Copyright (C) 2020 Kalray Inc.
+ */
+
+#ifndef _UAPI_ASM_KVX_CACHECTL_H
+#define _UAPI_ASM_KVX_CACHECTL_H
+
+/*
+ * Cache type for cachectl system call
+ */
+#define CACHECTL_CACHE_DCACHE		(1 << 0)
+
+/*
+ * Flags for cachectl system call
+ */
+#define CACHECTL_FLAG_OP_INVAL		(1 << 0)
+#define CACHECTL_FLAG_OP_WB		(1 << 1)
+#define CACHECTL_FLAG_OP_MASK		(CACHECTL_FLAG_OP_INVAL | \
+					 CACHECTL_FLAG_OP_WB)
+
+#endif /* _UAPI_ASM_KVX_CACHECTL_H */
