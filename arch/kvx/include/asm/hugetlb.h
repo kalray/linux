7889639145caa9c9c1238bf4a9c8ed8b367cc82e
Guillaume Thouvenin <gthouvenin@kalray.eu>
2020-07-06 13:50:28 +0200

    kvx: set access flags and wprotect for contiguous huge page
    Summary:
    We need to set the dirty and write flags for all contiguous huge
    pages that belongs to the same physical TLB for coherency. When flags
    are set entries are now flushed and all entries are synchronized.
    
    Ref T13802
    
    Test Plan: Run hugemap test in a loop and check that it is running smoothly
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T13802
    
    Differential Revision: https://phab.kalray.eu/D7170

--- arch/kvx/include/asm/hugetlb.h
+++ arch/kvx/include/asm/hugetlb.h
@@ -13,15 +13,25 @@
 #include <asm/pgtable.h>
 
 #define __HAVE_ARCH_HUGE_SET_HUGE_PTE_AT
-#define __HAVE_ARCH_HUGE_PTEP_GET_AND_CLEAR
-
-#include <asm-generic/hugetlb.h>
-
 extern void set_huge_pte_at(struct mm_struct *mm, unsigned long addr,
 			    pte_t *ptep, pte_t pte);
 
+#define __HAVE_ARCH_HUGE_PTEP_GET_AND_CLEAR
 extern pte_t huge_ptep_get_and_clear(struct mm_struct *mm,
 				     unsigned long addr, pte_t *ptep);
+
+#define __HAVE_ARCH_HUGE_PTEP_SET_ACCESS_FLAGS
+extern int huge_ptep_set_access_flags(struct vm_area_struct *vma,
+				      unsigned long addr, pte_t *ptep,
+				      pte_t pte, int dirty);
+
+#define __HAVE_ARCH_HUGE_PTEP_SET_WRPROTECT
+extern void huge_ptep_set_wrprotect(struct mm_struct *mm,
+				    unsigned long addr, pte_t *ptep);
+
+#define __HAVE_ARCH_HUGE_PTEP_CLEAR_FLUSH
+extern void huge_ptep_clear_flush(struct vm_area_struct *vma,
+				  unsigned long addr, pte_t *ptep);
 
 static inline int is_hugepage_only_range(struct mm_struct *mm,
 					 unsigned long addr,
@@ -33,5 +43,7 @@ static inline int is_hugepage_only_range(struct mm_struct *mm,
 static inline void arch_clear_hugepage_flags(struct page *page)
 {
 }
+
+#include <asm-generic/hugetlb.h>
 
 #endif /* _ASM_KVX_HUGETLB_H */
