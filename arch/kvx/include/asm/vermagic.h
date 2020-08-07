9532f358a6d008f4a0db7bac6df7a953f8252fc9
Clement Leger <cleger@kalray.eu>
2020-08-07 17:46:55 +0200

    kvx: add missing vermagic.h file
    Summary:
    This file defines the module version magic
    
    Ref T13785
    
    Test Plan: Integrate kernel 5.8
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T13785
    
    Differential Revision: https://phab.kalray.eu/D7661

---
+++ arch/kvx/include/asm/vermagic.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Kalray Inc.
+ */
+
+#ifndef _ASM_KVX_VERMAGIC_H
+#define _ASM_KVX_VERMAGIC_H
+
+#define MODULE_ARCH_VERMAGIC    "kvx"
+
+#endif /* _ASM_KVX_VERMAGIC_H */
