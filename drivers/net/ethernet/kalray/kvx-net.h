6f9d2aa411e1606876cfaa68209fdcb66dff2c03
Benjamin Mugnier <bmugnier@kalray.eu>
2020-09-23 09:25:22 +0200

    kvx: eth: Implement autoneg/link training workaround
    Summary:
    Auto negociation is used to get highest common speed between 2 devices.
    Once speed is identified, the link quality is tuned with link training.
    Link training is mandatory in KR and CR type links (see clause 73).
    
    Kalray boards retimers prevent proper link training tuning for the MPPA
    phy, therefore this workaround tell the link partner we will handle our
    signal quality by ourselves in link training language.
    
    Co-authored-by: tcostis
    Co-authored-by: vchardon
    
    Test Plan: AN + LT can etablish link up
    
    Reviewers: O39 ethernet, O51 Linux Coolidge, tcostis, ysionneau, cleger, vchardon
    
    Reviewed By: O39 ethernet, O51 Linux Coolidge, tcostis, ysionneau, cleger, vchardon
    
    Subscribers: ysionneau, vchardon, cleger, tcostis, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D7908

--- drivers/net/ethernet/kalray/kvx-net.h
+++ drivers/net/ethernet/kalray/kvx-net.h
@@ -140,6 +140,8 @@ int kvx_eth_hw_sysfs_init(struct kvx_eth_hw *hw);
 int kvx_eth_netdev_sysfs_init(struct kvx_eth_netdev *ndev);
 void kvx_eth_netdev_sysfs_uninit(struct kvx_eth_netdev *ndev);
 
+int configure_rtm(struct kvx_eth_hw *hw, unsigned int rtm, unsigned int speed);
+
 #ifdef CONFIG_DCB
 void kvx_set_dcb_ops(struct net_device *netdev);
 #else
