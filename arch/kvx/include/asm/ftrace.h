8e3fc552813cd5cb40bb6ae1a0d4fa5effcebd09
Guillaume Thouvenin <gthouvenin@kalray.eu>
2020-10-28 11:00:18 +0100

    kvx: ftrace: Add support for HAVE_DYNAMIC_FTRACE
    Summary:
    This patch adds the support for dynamic ftrace. It also adds the support
    of HAVE_FTRACE_MCOUNT_RECORD that is required by the dynamic ftrace. The
    mcount is used to replace all call to __mcount by a NOP. And dynamic
    ftrace is patching this nop to call different functions.
    
    Ref T9308
    Ref T12412
    Ref T13870
    
    Test Plan:
    Test the function tracer and function graph tracer is still
    working. They will use the dynamic ftrace patching call.
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: cleger, #linux_coolidge_cc
    
    Maniphest Tasks: T13870, T12411
    
    Differential Revision: https://phab.kalray.eu/D7728

--- arch/kvx/include/asm/ftrace.h
+++ arch/kvx/include/asm/ftrace.h
@@ -10,6 +10,28 @@
 #ifndef _ASM_KVX_FTRACE_H
 #define _ASM_KVX_FTRACE_H
 
+#include <asm/insns_defs.h>
+
+#define INSN_MAKE_IMM64_SYLLABLE_SIZE  INSN_SIZE(MAKE_IMM64)
+#define INSN_ICALL_SYLLABLE_SIZE       INSN_SIZE(ICALL)
+#define INSN_IGOTO_SYLLABLE_SIZE       INSN_SIZE(IGOTO)
+#define INSN_CALL_SYLLABLE_SIZE        INSN_SIZE(CALL)
+#define INSN_NOP_SYLLABLE_SIZE         INSN_SIZE(NOP)
+
+#define INSN_ICALL_REG_MASK  0x3f
+
+#define MCOUNT_ADDR	((unsigned long)(__mcount))
+#define MCOUNT_INSN_SIZE   INSN_CALL_SYLLABLE_SIZE /* sizeof mcount call */
+
+extern void ftrace_graph_call(void);
+
+#ifdef CONFIG_DYNAMIC_FTRACE
+extern unsigned long ftrace_call_adjust(unsigned long addr);
+struct dyn_arch_ftrace {
+	unsigned int insn;
+};
+#endif
+
 extern void *return_address(unsigned int level);
 #define ftrace_return_address(n) return_address(n)
 
