f460d965f600340d89fe5eba6105da5b7e128877
Clement Leger <cleger@kalray.eu>
2020-11-04 10:17:27 +0100

    kvx: update all copyrights
    Summary: NA
    
    Test Plan: Execute spdxcheck.py
    
    Reviewers: O51 Linux Coolidge, ysionneau
    
    Subscribers: ysionneau, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D7466

--- arch/kvx/include/asm/clocksource.h
+++ arch/kvx/include/asm/clocksource.h
@@ -1,3 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2018-2020 Kalray Inc.
+ * Author: Clement Leger
+ */
+
 #ifndef _ASM_KVX_CLOCKSOURCE_H
 #define _ASM_KVX_CLOCKSOURCE_H
 
--- drivers/soc/kvx/kvx-socinfo.c
+++ drivers/soc/kvx/kvx-socinfo.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright 2019 NXP.
+ * Copyright (C) 2020 Kalray Inc., 
+ * Author: Clement Leger
  */
 
 #include <linux/io.h>
