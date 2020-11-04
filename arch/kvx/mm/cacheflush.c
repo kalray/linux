57cccd0007350accb7a26026568ddbfa9b5d1f59
Clement Leger <cleger@kalray.eu>
2020-11-04 10:17:27 +0100

    kvx: update all copyrights
    Summary: NA
    
    Test Plan: Execute spdxcheck.py
    
    Reviewers: O51 Linux Coolidge, ysionneau
    
    Subscribers: ysionneau, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D7466

--- arch/kvx/mm/cacheflush.c
+++ arch/kvx/mm/cacheflush.c
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (C) 2020 Kalray Inc.
+ * Author: Clement Leger
  */
 
 #include <linux/smp.h>
--- drivers/watchdog/kvx_wdt.c
+++ drivers/watchdog/kvx_wdt.c
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
- *
  * Copyright (C) 2019 Kalray Inc.
+ * Author: Clement Leger
  *
  * KVX watchdog driver
  *
