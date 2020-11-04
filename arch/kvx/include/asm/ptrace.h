f4908ca7d65b2f28975a624edd52e1db0df2226c
Clement Leger <cleger@kalray.eu>
2020-11-04 10:17:27 +0100

    kvx: update all copyrights
    Summary: NA
    
    Test Plan: Execute spdxcheck.py
    
    Reviewers: O51 Linux Coolidge, ysionneau
    
    Subscribers: ysionneau, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D7466

--- arch/kvx/include/asm/ptrace.h
+++ arch/kvx/include/asm/ptrace.h
@@ -1,9 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
- *
- * Copyright (C) 2018 Kalray Inc.
+ * Copyright (C) 2018-2020 Kalray Inc.
+ * Author: Marius Gligor
  */
 
 #ifndef _ASM_KVX_PTRACE_H
--- arch/kvx/platform/ipi.c
+++ arch/kvx/platform/ipi.c
@@ -1,10 +1,7 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
- *
- * Copyright (C) 2018 Kalray Inc.
+ * Copyright (C) 2018-2020 Kalray Inc.
+ * Author: Clement Leger
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
