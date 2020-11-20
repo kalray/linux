8a431d88ced86d6f0827774a7c28038d4eb42aaf
Clement Leger <cleger@kalray.eu>
2020-11-20 13:52:24 +0100

    kvx: enable sparsemem support
    Summary:
    Currently, DDR is configured in banked mode which means that the RAM is
    split in two sections of 4G with a huge hole between them. Tihis is not
    optimal using a flatmem [1] model leading to a lot of memory lost for this
    hole. This patch enables sparsemem which allows to reduce the mem_map
    size by using sections.
    
    [1] https://www.kernel.org/doc/html/latest/vm/memory-model.html
    
    Test Plan: Compile and run
    
    Reviewers: O51 Linux Coolidge, jmaselbas, gthouvenin
    
    Reviewed By: O51 Linux Coolidge, jmaselbas, gthouvenin
    
    Subscribers: gthouvenin, jmaselbas, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D9362

---
+++ arch/kvx/include/asm/sparsemem.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _ASM_KVX_SPARSEMEM_H
+#define _ASM_KVX_SPARSEMEM_H
+
+#ifdef CONFIG_SPARSEMEM
+#define MAX_PHYSMEM_BITS	40
+#define SECTION_SIZE_BITS	30
+#endif /* CONFIG_SPARSEMEM */
+
+#endif /* _ASM_KVX_SPARSEMEM_H */
