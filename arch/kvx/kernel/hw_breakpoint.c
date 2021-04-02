f9634886b96818538702a9c7779a982449fb9163
Marius Gligor <mgligor@kalray.eu>
2021-04-02 13:02:18 +0200

    Add support for Coolidge V2 hardware watchpoints and breakpoints
    Summary:
    For Coolidge V2, the range field of the hardware watchpoints was replaced
    with the size and the type fields.
    
    Test Plan: The gdb tests should pass
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D11961
    
    (cherry picked from commit c9825e028705b94d1582c317eb18393b5b308c35)

--- arch/kvx/kernel/hw_breakpoint.c
+++ arch/kvx/kernel/hw_breakpoint.c
@@ -26,7 +26,13 @@
 #define WATCHPOINT_STEPPED	1
 #define WATCHPOINT_GDB_HIT	2
 
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 #define hw_breakpoint_remap(idx) (KVX_HW_BREAKPOINT_COUNT - 1 - (idx))
+#define hw_watchpoint_remap(idx) (idx)
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+#define hw_breakpoint_remap(idx) (idx + 2)
+#define hw_watchpoint_remap(idx) (idx + 2)
+#endif
 
 /* Breakpoint currently in use */
 static DEFINE_PER_CPU(struct perf_event *, hbp_on_reg[KVX_HW_BREAKPOINT_COUNT]);
@@ -35,6 +41,9 @@ static DEFINE_PER_CPU(struct perf_event *, hbp_on_reg[KVX_HW_BREAKPOINT_COUNT]);
 static DEFINE_PER_CPU(struct perf_event *, hwp_on_reg[KVX_HW_WATCHPOINT_COUNT]);
 
 /* Get and set function of the debug hardware registers */
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
+
+/* Coolidge V1 */
 #define gen_set_hw_sfr(__name, __sfr) \
 static inline void set_hw_ ## __name(int idx, u64 addr) \
 { \
@@ -63,16 +72,83 @@ static inline u32 get_hw_ ## __name(int idx) \
 				 __field ## 1); \
 }
 
-gen_set_hw_sfr(bp_addr, DBA);
-gen_set_hw_sfr(wp_addr, DWA);
-gen_set_hw_sfr_field(bp_range, DC, BR);
-gen_set_hw_sfr_field(wp_range, DC, WR);
-gen_set_hw_sfr_field(bp_enable, DC, BE);
-gen_set_hw_sfr_field(wp_enable, DC, WE);
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+
+/* Coolidge V2 */
+#define gen_set_hw_sfr(__name, __sfr) \
+static inline void set_hw_ ## __name(int idx, u64 addr) \
+{ \
+	if (idx == 0) \
+		kvx_sfr_set(__sfr ## 0, addr); \
+	else if (idx == 1) \
+		kvx_sfr_set(__sfr ## 1, addr); \
+	else if (idx == 2) \
+		kvx_sfr_set(__sfr ## 2, addr); \
+	else \
+		kvx_sfr_set(__sfr ## 3, addr); \
+}
+
+#define gen_set_hw_sfr_field(__name, __sfr, __field) \
+static inline void set_hw_ ## __name(int idx, u32 value) \
+{ \
+	if (idx == 0) \
+		kvx_sfr_set_field(__sfr, __field ## 0, value); \
+	else if (idx == 1) \
+		kvx_sfr_set_field(__sfr, __field ## 1, value); \
+	else if (idx == 2) \
+		kvx_sfr_set_field(__sfr, __field ## 2, value); \
+	else \
+		kvx_sfr_set_field(__sfr, __field ## 3, value); \
+}
+
+#define gen_set_hw_dc_field(__name, __sfr, __field) \
+static inline void set_hw_ ## __name(int idx, u32 value) \
+{ \
+	if (idx == 0) \
+		kvx_sfr_set_field(DC0, __field, value); \
+	else if (idx == 1) \
+		kvx_sfr_set_field(DC1, __field, value); \
+	else if (idx == 2) \
+		kvx_sfr_set_field(DC2, __field, value); \
+	else \
+		kvx_sfr_set_field(DC3, __field, value); \
+}
+
+#define gen_get_hw_sfr_field(__name, __sfr, __field) \
+static inline u32 get_hw_ ## __name(int idx) \
+{ \
+	if (idx == 0) \
+		return kvx_sfr_field_val(kvx_sfr_get(__sfr), \
+					 __sfr, __field ## 0); \
+	else if (idx == 1) \
+		return kvx_sfr_field_val(kvx_sfr_get(__sfr), \
+					 __sfr, __field ## 1); \
+	else if (idx == 2) \
+		return kvx_sfr_field_val(kvx_sfr_get(__sfr), \
+					 __sfr, __field ## 2); \
+	return kvx_sfr_field_val(kvx_sfr_get(__sfr), \
+				 __sfr, __field ## 3); \
+}
+
+#endif
+
 gen_set_hw_sfr_field(bp_owner, DOW, B);
 gen_set_hw_sfr_field(wp_owner, DOW, W);
 gen_get_hw_sfr_field(bp_owner, DO, B);
 gen_get_hw_sfr_field(wp_owner, DO, W);
+gen_set_hw_sfr(bp_addr, DBA);
+gen_set_hw_sfr(wp_addr, DWA);
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
+gen_set_hw_sfr_field(bp_range, DC, BR);
+gen_set_hw_sfr_field(wp_range, DC, WR);
+gen_set_hw_sfr_field(bp_enable, DC, BE);
+gen_set_hw_sfr_field(wp_enable, DC, WE);
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+gen_set_hw_dc_field(bp_enable, DC, BE);
+gen_set_hw_dc_field(wp_size, DC, WSZ);
+gen_set_hw_dc_field(wp_type, DC, WTYP);
+gen_set_hw_dc_field(wp_enable, DC, WE);
+#endif
 
 /**
  * hw_breakpoint_slots() - obtain the available number of hardware resources
@@ -104,6 +180,7 @@ int arch_check_bp_in_kernelspace(struct arch_hw_breakpoint *hw)
 	return hw->addr >= PAGE_OFFSET;
 }
 
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 /*
  * compute_hw_watchpoint_range() - compute the watchpoint hardware registers to
  * cover attr->bp_len bytes beginning from attr->bp_addr
@@ -145,32 +222,33 @@ static void compute_hw_watchpoint_range(const struct perf_event_attr *attr,
 	 * (addr + len -1) is in next cache line
 	 */
 	if (begin < addr_l1_aligned) {
-		hw->wp.hw_wp_addr[0] = begin;
-		hw->wp.hw_wp_range[0] = fls64(begin ^ (addr_l1_aligned - 1));
+		hw->wp.hw_addr[0] = begin;
+		hw->wp.hw_range[0] = fls64(begin ^ (addr_l1_aligned - 1));
 
 		hw->wp.use_wp1 = 1;
-		hw->wp.hw_wp_addr[1] = addr_l1_aligned;
-		hw->wp.hw_wp_range[1] = fls64(addr_l1_aligned ^ end);
+		hw->wp.hw_addr[1] = addr_l1_aligned;
+		hw->wp.hw_range[1] = fls64(addr_l1_aligned ^ end);
 	} else if (addr_l1_aligned != end_l1_aligned) {
-		hw->wp.hw_wp_addr[0] = addr_l1_aligned;
-		hw->wp.hw_wp_range[0] =
+		hw->wp.hw_addr[0] = addr_l1_aligned;
+		hw->wp.hw_range[0] =
 			fls64(addr_l1_aligned ^ (end_l1_aligned - 1));
 
 		hw->wp.use_wp1 = 1;
-		hw->wp.hw_wp_addr[1] = end_l1_aligned;
-		hw->wp.hw_wp_range[1] = fls64(end_l1_aligned ^ end);
+		hw->wp.hw_addr[1] = end_l1_aligned;
+		hw->wp.hw_range[1] = fls64(end_l1_aligned ^ end);
 	} else {
 		hw->wp.use_wp1 = 0;
-		hw->wp.hw_wp_addr[0] = addr_l1_aligned;
-		hw->wp.hw_wp_range[0] = fls64(addr_l1_aligned ^ end);
+		hw->wp.hw_addr[0] = addr_l1_aligned;
+		hw->wp.hw_range[0] = fls64(addr_l1_aligned ^ end);
 	}
 
 	if (!hw->wp.use_wp1) {
-		hw->wp.hw_wp_addr[1] = 0;
-		hw->wp.hw_wp_range[1] = 0;
+		hw->wp.hw_addr[1] = 0;
+		hw->wp.hw_range[1] = 0;
 	}
 	hw->wp.hit_info = 0;
 }
+#endif
 
 /*
  * hw_breakpoint_arch_parse() - Construct an arch_hw_breakpoint from
@@ -195,12 +273,20 @@ int hw_breakpoint_arch_parse(struct perf_event *bp,
 
 		hw->type = KVX_HW_BREAKPOINT_TYPE;
 		hw->bp.hw_addr = attr->bp_addr;
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 		hw->bp.hw_range = HW_BREAKPOINT_RANGE;
+#endif
 		break;
 	case HW_BREAKPOINT_W:
 		hw->type = KVX_HW_WATCHPOINT_TYPE;
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 		if (!attr->disabled)
 			compute_hw_watchpoint_range(attr, hw);
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+		hw->wp.hw_addr = attr->bp_addr;
+		hw->wp.hw_size = attr->bp_len;
+		hw->wp.hw_type = WATCHPOINT_TYPE_WRITE;
+#endif
 		break;
 	default:
 		return -EINVAL;
@@ -239,7 +325,9 @@ static void enable_hw_breakpoint(int idx, int enable, struct perf_event *bp)
 	idx = hw_breakpoint_remap(idx);
 	if (enable) {
 		set_hw_bp_addr(idx, info->bp.hw_addr);
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 		set_hw_bp_range(idx, info->bp.hw_range);
+#endif
 	}
 
 	set_hw_bp_enable(idx, enable);
@@ -292,27 +380,36 @@ static int reserve_one_hw_watchpoint(int idx)
  * Return: 0 for success
  *
  * Observation: Because of the kvx watchpoint range problem, both hardware
- * watchpoints are used for index 0
+ * watchpoints are used for index 0 for Coolidge V1
  */
 int ptrace_request_hw_watchpoint(int idx)
 {
 	int res;
 
-	/* Request the both watchpoints. First W0 */
 	res = reserve_one_hw_watchpoint(idx);
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 	if (res)
 		return res;
 
-	/* W0 is ours, request W1 */
+	/* Request the both watchpoints for Coolidge V1. W0 is ours, now W1 */
 	return reserve_one_hw_watchpoint(idx + 1);
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+	return res;
+#endif
 }
 
 static void enable_one_watchpoint(int idx, int sub_idx, int enable,
 				  struct arch_hw_breakpoint *info)
 {
 	if (enable) {
-		set_hw_wp_addr(idx, info->wp.hw_wp_addr[sub_idx]);
-		set_hw_wp_range(idx, info->wp.hw_wp_range[sub_idx]);
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
+		set_hw_wp_addr(idx, info->wp.hw_addr[sub_idx]);
+		set_hw_wp_range(idx, info->wp.hw_range[sub_idx]);
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+		set_hw_wp_addr(idx, info->wp.hw_addr);
+		set_hw_wp_size(idx, info->wp.hw_size);
+		set_hw_wp_type(idx, info->wp.hw_type);
+#endif
 	}
 
 	set_hw_wp_enable(idx, enable);
@@ -325,9 +422,12 @@ static void enable_hw_watchpoint(int idx, int enable, struct perf_event *bp)
 	if (idx < 0 || idx >= KVX_HW_WATCHPOINT_COUNT)
 		return;
 
+	idx = hw_watchpoint_remap(idx);
 	enable_one_watchpoint(idx, 0, enable, info);
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 	if (info->wp.use_wp1)
 		enable_one_watchpoint(idx + 1, 1, enable, info);
+#endif
 }
 
 static void get_hw_pt_list(int type, struct perf_event ***p, int *count)
@@ -485,7 +585,11 @@ int check_hw_watchpoint(struct pt_regs *regs, u64 ea)
 {
 	struct perf_event **wp;
 	struct arch_hw_breakpoint *info;
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
 	u64 mask;
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+	u64 access_size;
+#endif
 	int i, ret = 0;
 
 	wp = this_cpu_ptr(hwp_on_reg);
@@ -494,19 +598,30 @@ int check_hw_watchpoint(struct pt_regs *regs, u64 ea)
 			continue;
 
 		info = counter_arch_bp(wp[i]);
-		mask = ~(BIT_ULL(info->wp.hw_wp_range[0]) - 1);
-		if ((info->wp.hw_wp_addr[0] & mask) == (ea & mask)) {
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
+		mask = ~(BIT_ULL(info->wp.hw_range[0]) - 1);
+		if ((info->wp.hw_addr[0] & mask) == (ea & mask)) {
 			ret = 1;
 			watchpoint_triggered(wp[i], regs, i, ea);
 		}
 
 		if (info->wp.use_wp1) {
-			mask = ~(BIT_ULL(info->wp.hw_wp_range[1]) - 1);
-			if ((info->wp.hw_wp_addr[1] & mask) == (ea & mask)) {
+			mask = ~(BIT_ULL(info->wp.hw_range[1]) - 1);
+			if ((info->wp.hw_addr[1] & mask) == (ea & mask)) {
 				ret = 1;
 				watchpoint_triggered(wp[i], regs, i, ea);
 			}
 		}
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+		access_size = kvx_sfr_field_val(regs->es, ES, AS);
+		if (access_size > 32)
+			access_size = 64;
+		if (info->wp.hw_addr + info->wp.hw_size > ea &&
+		    info->wp.hw_addr < ea + access_size) {
+			ret = 1;
+			watchpoint_triggered(wp[i], regs, i, ea);
+		}
+#endif
 	}
 
 	return ret;
