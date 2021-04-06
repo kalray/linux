9d046a659bc1a1a44f927e10f033c2901a01bde2
Jules Maselbas <jmaselbas@kalray.eu>
2021-04-06 22:23:58 +0200

    kvx: Fix sync_dma_for_cpu
    Summary:
    L2 cache lines are bigger than L1 cache lines, which can cause
    a "false sharing" situation where two L1 cache lines share the
    same L2 cache line. There is a high chance that the L2 cache
    line has already been refilled by a previous memory access.
    
    Thus an extra dinval is required before sync_for_cpu.
    On a dinval a writeback will be trigger if the buffer is not aligned
    on a L2 cache line. The L2 write back will only write dirty bytes,
    hence it will not overwrite the data from device.
    
    ref T16115
    
    Test Plan: USB host enumeration works
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: ysionneau, #linux_coolidge_cc
    
    Maniphest Tasks: T16115
    
    Differential Revision: https://phab.kalray.eu/D11985

--- arch/kvx/mm/dma-mapping.c
+++ arch/kvx/mm/dma-mapping.c
@@ -53,8 +53,18 @@ void arch_sync_dma_for_cpu(phys_addr_t paddr, size_t size,
 {
 	switch (dir) {
 	case DMA_TO_DEVICE:
-	/* kvx does not do speculative loads by itself */
+		break;
 	case DMA_FROM_DEVICE:
+#ifdef CONFIG_L2_CACHE
+		/* kvx does not do speculative loads by itself, however
+		 * L2 cache lines are bigger than L1 cache lines, which can
+		 * cause a "false sharing" situation where two L1 cache lines
+		 * share the same L2 cache line. There is a high chance that
+		 * the L2 cache line has already been refilled by a previous
+		 * memory access.
+		 */
+		inval_dcache_range(paddr, size);
+#endif
 		break;
 
 	case DMA_BIDIRECTIONAL:
