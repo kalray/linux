a8e25511485bac356058d98744c232fef747a1f9
Thomas Costis <tcostis@kalray.eu>
2021-06-23 10:57:24 +0200

    kvx: eth: fix check on eeprom read
    Summary:
    Fix error checking on eeprom read that can lead to wrong transceiver_id value.
    Read only first page of EEPROM (other are not needed) in order to check
    cable is actually qsfp (SFF-8636).
    
    Ref T16509
    
    Test Plan: enmppa4 link up
    
    Reviewers: O39 ethernet!
    
    Maniphest Tasks: T16509
    
    Differential Revision: https://phab.kalray.eu/D13568
    
    (cherry picked from commit a1e91ba5aae913b457ddf6a5c3e71de047a66b54)

--- drivers/net/ethernet/kalray/kvx-ethtool.c
+++ drivers/net/ethernet/kalray/kvx-ethtool.c
@@ -1319,15 +1319,15 @@ int kvx_eth_get_module_transceiver(struct net_device *netdev,
 	u8 data[ETH_MODULE_SFF_8636_MAX_LEN] = {0};
 	struct ethtool_eeprom ee = {
 		.cmd = ETHTOOL_GEEPROM,
-		.len = ETH_MODULE_SFF_8636_LEN,
-		.offset = SFP_PAGE + 1,
+		.len = 256,
+		.offset = 0,
 	};
 	u8 *id, tech;
 	int ret = 0;
 
 	if (netdev->sfp_bus) {
 		ret = sfp_get_module_eeprom(netdev->sfp_bus, &ee, data);
-		if (ret)
+		if (ret < 0)
 			return ret;
 	}
 
