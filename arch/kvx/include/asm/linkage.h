ab75b03374717dbf9792b2ade4a1d34910cb812d
Yann Sionneau <ysionneau@kalray.eu>
2021-07-22 14:21:41 +0200

    kvx: add linkage header to use specific ALIGN
    Summary: Add linkage header to use specific ALIGN.
    
    Test Plan: CI
    
    Reviewers: O51 Linux Coolidge, cleger, jmaselbas
    
    Reviewed By: O51 Linux Coolidge, cleger, jmaselbas
    
    Subscribers: jmaselbas, cleger, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D9537

---
+++ arch/kvx/include/asm/linkage.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2020 Kalray Inc.
+ */
+
+#ifndef __ASM_KVX_LINKAGE_H
+#define __ASM_KVX_LINKAGE_H
+
+#define __ALIGN		.align 4
+#define __ALIGN_STR	".align 4"
+
+#endif
