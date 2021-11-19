aa44e32154932864e1b2977d88c791db58b677d2
Yann Sionneau <ysionneau@kalray.eu>
2021-11-19 11:06:14 +0100

    kvx: wd_disabler: add driver to disable early watchdogs when needed
    Summary: Add driver to disable early watchdogs when needed.
    
    Test Plan: n/a
    
    Reviewers: O51 Linux Coolidge, jmaselbas
    
    Reviewed By: O51 Linux Coolidge, jmaselbas
    
    Subscribers: jmaselbas, lmorhet, #linux_coolidge_cc
    
    Maniphest Tasks: T17605
    
    Differential Revision: https://phab.kalray.eu/D16454

---
+++ arch/kvx/kernel/wd_disabler.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <linux/of.h>
+#include <asm/sfr_defs.h>
+
+static void kvx_cpu_wdt_stop(void *data)
+{
+	kvx_sfr_set_field(TCR, WCE, 0);
+}
+
+static int __init wd_disabler_init(void)
+{
+	struct device_node *np;
+
+	np = of_find_compatible_node(NULL, NULL, "kalray,kvx-core-watchdog");
+
+	if (np && of_device_is_available(np))
+		return 0;
+
+	pr_info("Disabling PE early watchdogs");
+	on_each_cpu(kvx_cpu_wdt_stop, NULL, 0);
+
+	return 0;
+}
+
+early_initcall(wd_disabler_init);
