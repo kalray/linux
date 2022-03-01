26859f814ac524467cbeb58680876d27116e972b
Thomas Costis <tcostis@kalray.eu>
2022-03-01 11:46:40 +0100

    kvx: eth: delay qsfp tuning
    Summary:
    QSFP parameter tuning should not be done in netdev probe as sfp driver
    may still access eeprom.
    So delay qsfp tuning in netdev start.
    Also add check on cable to prevent qsfp tuning on copper cables
    
    Ref T18414
    
    Test Plan: fiber qsfp param tuning
    
    Reviewers: O39 ethernet!
    
    Maniphest Tasks: T18414
    
    Differential Revision: https://phab.kalray.eu/D18887

--- drivers/net/ethernet/kalray/kvx-sfp.h
+++ drivers/net/ethernet/kalray/kvx-sfp.h
@@ -18,16 +18,20 @@
 #define SFF8636_TX_APP_SELECT_OFFSET        94
 #define SFF8636_TX_CDR_OFFSET               98
 
-/* All following offsets are shifted of 128 */
-#define SFF8636_DEVICE_TECH_OFFSET          19
+#define SFF8636_DEVICE_ID_OFFSET            128
+#define SFF8636_DEVICE_TECH_OFFSET          147
 #define SFF8636_TRANS_TECH_MASK             0xF0
-#define SFF8636_TRANS_COPPER_LNR_EQUAL     (15 << 4)
-#define SFF8636_TRANS_COPPER_NEAR_EQUAL    (14 << 4)
-#define SFF8636_TRANS_COPPER_FAR_EQUAL     (13 << 4)
-#define SFF8636_TRANS_COPPER_LNR_FAR_EQUAL (12 << 4)
-#define SFF8636_TRANS_COPPER_PAS_EQUAL     (11 << 4)
-#define SFF8636_TRANS_COPPER_PAS_UNEQUAL   (10 << 4)
-#define SFF8636_COMPLIANCE_CODES_OFFSET     3
+#define SFF8636_TRANS_TECH_TUNABLE_MASK     BIT(0)
+#define SFF8636_VENDOR_OUI_OFFSET           165
+#define SFF8636_VENDOR_PN_OFFSET            168
+#define SFF8636_VENDOR_SN_OFFSET            196
+#define SFF8636_TRANS_COPPER_LNR_EQUAL      (15 << 4)
+#define SFF8636_TRANS_COPPER_NEAR_EQUAL     (14 << 4)
+#define SFF8636_TRANS_COPPER_FAR_EQUAL      (13 << 4)
+#define SFF8636_TRANS_COPPER_LNR_FAR_EQUAL  (12 << 4)
+#define SFF8636_TRANS_COPPER_PAS_EQUAL      (11 << 4)
+#define SFF8636_TRANS_COPPER_PAS_UNEQUAL    (10 << 4)
+#define SFF8636_COMPLIANCE_CODES_OFFSET     131
 #define SFF8636_COMPLIANCE_10GBASE_LRM      BIT(6)
 #define SFF8636_COMPLIANCE_10GBASE_LR       BIT(5)
 #define SFF8636_COMPLIANCE_10GBASE_SR       BIT(4)
@@ -35,8 +39,8 @@
 #define SFF8636_COMPLIANCE_40GBASE_SR4      BIT(2)
 #define SFF8636_COMPLIANCE_40GBASE_LR4      BIT(1)
 #define SFF8636_COMPLIANCE_40G_XLPPI        BIT(0)
-#define SFF8636_NOMINAL_BITRATE             12
-#define SFF8636_NOMINAL_BITRATE_250         94
+#define SFF8636_NOMINAL_BITRATE             140
+#define SFF8636_NOMINAL_BITRATE_250         222
 
 struct kvx_eth_netdev;
 struct kvx_eth_hw;
@@ -44,22 +48,26 @@ struct kvx_eth_hw;
 /** struct kvx_transceiver_type - Transceiver info
  * @oui: Cable constructor OUI
  * @pn: Cable part number
- * @id: sff identifier
+ * @sn: Cable serial number
+ * @id: Dev sff identifier
  * @compliance_code: byte 131 of EEPROM
- * @copper: 1 if copper cable, else 0
+ * @tech: cable techology
  * @qsfp: 1 cage is qsfp, else 0
  * @nominal_br: Nominal bitrate supported by the cable
  */
 struct kvx_transceiver_type {
+	u8 id;
 	u8 oui[3];
 	u8 pn[16];
-	u8 id;
+	u8 sn[16];
 	u8 compliance_code;
-	u8 copper;
+	u8 tech;
 	u8 qsfp;
 	u32 nominal_br;
 };
 
+bool is_cable_connected(struct kvx_transceiver_type *t);
+bool is_cable_copper(struct kvx_transceiver_type *t);
 int ee_select_page(struct i2c_adapter *i2c, u8 page);
 int kvx_eth_qsfp_ee_read(struct i2c_adapter *i2c, u8 *buf, u8 *page,
 			 int *off, size_t l);
