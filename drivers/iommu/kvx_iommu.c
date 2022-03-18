ba7bb8a41db169b97694b1ab5eedafb89e94754b
Julian Vetter <jvetter@kalrayinc.com>
2022-03-18 15:06:35 +0100

    kvx: fixed license headers for drivers and periph
    Summary:
    * Updated all headers to be in line with description on
      https://www.kernel.org/doc/html/v4.18/process/license-rules.html.
    * Added second header with all contributors in order of number of contributions
      for the given file
    * Added a line like derived from xxx if the file was derived from an other file
      in the source tree
    * Updated copyright notice from 2017 - 2022
    
    Ref T18699
    
    Test Plan: CI
    
    Reviewers: ysionneau, jmaselbas, tcostis, O39 ethernet!, O82 pcie root complex!, O51 Linux Coolidge!
    
    Subscribers: #ethernet, #linux_coolidge_cc
    
    Maniphest Tasks: T18699
    
    Differential Revision: https://phab.kalray.eu/D19313

--- drivers/iommu/kvx_iommu.c
+++ drivers/iommu/kvx_iommu.c
@@ -1,7 +1,14 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (C) 2019 Kalray Inc.
- * Author: Guillaume Thouvenin
+ * Copyright (c) 2017 - 2022 Kalray Inc.
+ * Author(s): Guillaume Thouvenin
+ *            Clement Leger
+ *            Vincent Chardon
+ *            Guillaume Missonnier
+ *            Yann Sionneau
+ *            Julian Vetter
+ *            Jérémy Fanguède
+ *            Jules Maselbas
  */
 
 #include <linux/device.h>
