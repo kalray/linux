875f58a10f4838f0213de0f1bf56f1afe2e8ed55
Yann Sionneau <ysionneau@kalray.eu>
2022-05-30 15:43:33 +0200

    kvx: Add signal handling support
    Add sigcontext definition and signal handling support for
    kvx.
    
    Signed-off-by: Yann Sionneau <ysionneau@kalray.eu>

---
+++ arch/kvx/include/uapi/asm/sigcontext.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
+/*
+ * Copyright (C) 2017-2022 Kalray Inc.
+ * Author(s): Clement Leger
+ */
+
+#ifndef _UAPI_ASM_KVX_SIGCONTEXT_H
+#define _UAPI_ASM_KVX_SIGCONTEXT_H
+
+#include <asm/ptrace.h>
+
+struct sigcontext {
+	struct user_pt_regs sc_regs;
+};
+
+#endif	/* _UAPI_ASM_KVX_SIGCONTEXT_H */
