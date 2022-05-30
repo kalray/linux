5b28cd3376ce7dce3c3e91100d8867dc9d0fb76d
Yann Sionneau <ysionneau@kalray.eu>
2022-05-30 15:19:43 +0200

    kvx: Add boot and setup routines
    Add basic boot, setup and reset routines for kvx.
    
    CC: linux-kernel@vger.kernel.org
    Co-developed-by: Alex Michon <amichon@kalray.eu>
    Signed-off-by: Alex Michon <amichon@kalray.eu>
    Co-developed-by: Clement Leger <clement.leger@bootlin.com>
    Signed-off-by: Clement Leger <clement.leger@bootlin.com>
    Co-developed-by: Guillaume Missonnier <gmissonnier@kalray.eu>
    Signed-off-by: Guillaume Missonnier <gmissonnier@kalray.eu>
    Co-developed-by: Guillaume Thouvenin <gthouvenin@kalray.eu>
    Signed-off-by: Guillaume Thouvenin <gthouvenin@kalray.eu>
    Co-developed-by: Jules Maselbas <jmaselbas@kalray.eu>
    Signed-off-by: Jules Maselbas <jmaselbas@kalray.eu>
    Co-developed-by: Julian Vetter <jvetter@kalray.eu>
    Signed-off-by: Julian Vetter <jvetter@kalray.eu>
    Co-developed-by: Julien Hascoet <jhascoet@kalray.eu>
    Signed-off-by: Julien Hascoet <jhascoet@kalray.eu>
    Co-developed-by: Julien Villette <jvillette@kalray.eu>
    Signed-off-by: Julien Villette <jvillette@kalray.eu>
    Co-developed-by: Marc Poulhiès <dkm@kataplop.net>
    Signed-off-by: Marc Poulhiès <dkm@kataplop.net>
    Co-developed-by: Luc Michel <lmichel@kalray.eu>
    Signed-off-by: Luc Michel <lmichel@kalray.eu>
    Co-developed-by: Marius Gligor <mgligor@kalray.eu>
    Signed-off-by: Marius Gligor <mgligor@kalray.eu>
    Co-developed-by: Yann Sionneau <ysionneau@kalray.eu>
    Signed-off-by: Yann Sionneau <ysionneau@kalray.eu>

---
+++ arch/kvx/kernel/common.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2017-2022 Kalray Inc.
+ * Author(s): Clement Leger
+ */
+
+#include <linux/percpu-defs.h>
+#include <linux/sched/task.h>
+
+DEFINE_PER_CPU(int, __preempt_count) = INIT_PREEMPT_COUNT;
+EXPORT_PER_CPU_SYMBOL(__preempt_count);
