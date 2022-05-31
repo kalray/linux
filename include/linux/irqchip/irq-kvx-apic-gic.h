2732d278df908fb8c2763f16256099315cb99892
Yann Sionneau <ysionneau@kalray.eu>
2022-05-31 16:51:07 +0200

    kvx: irqchip: Add support for kvx irq controllers
    Signed-off-by: Yann Sionneau <ysionneau@kalray.eu>

---
+++ include/linux/irqchip/irq-kvx-apic-gic.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2018 Kalray Inc.
+ * Author: Clement Leger
+ */
+
+#ifndef KVX_APIC_GIC_H
+#define KVX_APIC_GIC_H
+
+
+
+/* GIC enable register definitions */
+#define KVX_GIC_ENABLE_OFFSET     0x0
+#define KVX_GIC_ENABLE_ELEM_SIZE  0x1
+#define KVX_GIC_INPUT_IT_COUNT 0x9D
+#define KVX_GIC_ELEM_SIZE 0x400
+
+/* GIC status lac register definitions */
+#define KVX_GIC_STATUS_LAC_OFFSET     0x120
+#define KVX_GIC_STATUS_LAC_ELEM_SIZE  0x8
+#define KVX_GIC_STATUS_LAC_ARRAY_SIZE 0x3
+
+#endif /* KVX_APIC_GIC_H */
