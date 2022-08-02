a7740b7d074275b284ab63e33deb0ffc28b0bedc
Thomas Costis <tcostis@kalray.eu>
2022-08-02 15:08:00 +0200

    kvx: eth: fix 40G cables
    Summary:
    Better handle 40G limited qsfp cables. Also update autoneg advertised
    speed in that case.
    
    Ref T19736
    
    Test Plan: eth + acceptance
    
    Reviewers: O39 ethernet!, tcostis
    
    Maniphest Tasks: T19736
    
    Differential Revision: https://phab.kalray.eu/D22474

--- drivers/net/ethernet/kalray/kvx-net.h
+++ drivers/net/ethernet/kalray/kvx-net.h
@@ -184,6 +184,7 @@ void kvx_setup_link(struct kvx_eth_netdev *ndev);
 int kvx_eth_hw_sysfs_init(struct kvx_eth_hw *hw);
 int kvx_eth_netdev_sysfs_init(struct kvx_eth_netdev *ndev);
 void kvx_eth_netdev_sysfs_uninit(struct kvx_eth_netdev *ndev);
+void kvx_eth_update_cable_modes(struct kvx_eth_netdev *ndev);
 void kvx_eth_get_formated_speed(int speed, int *speed_fmt, char **unit);
 
 int configure_rtm(struct kvx_eth_hw *hw, unsigned int lane_id,
