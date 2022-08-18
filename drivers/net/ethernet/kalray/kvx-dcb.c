00b3c8ba92d475983e27d42265de8299a76cc09e
Ashley Lesdalons <ashley.lesdalons@kalray.eu>
2022-08-18 10:50:21 +0200

    eth: kvx dcbnl rework
    Summary:
    This commit is a rework of the DCBNL callbacks implemented. From the comments made in ticket T19491, the following changes are made:
    - use of the same getter and setter methods for enabling/disabling pfc classes, global pfc and global pause
    - fix bugs found in ticket
    
    Ref T19491
    
    Test Plan: test pfc in CI
    
    Reviewers: O39 ethernet!
    
    Maniphest Tasks: T19491
    
    Differential Revision: https://phab.kalray.eu/D22876

--- drivers/net/ethernet/kalray/kvx-dcb.c
+++ drivers/net/ethernet/kalray/kvx-dcb.c
@@ -6,7 +6,6 @@
  *
  * Copyright (C) 2020 Kalray Inc.
  */
-
 #include <linux/netdevice.h>
 #include <net/dcbnl.h>
 
@@ -17,14 +16,12 @@ void kvx_net_init_dcb(struct net_device *netdev)
 {
 	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
 
-	ndev->dcb_cfg.cap = (DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_CEE |
-			     DCB_CAP_DCBX_VER_IEEE);
+	/* enable IEEE by default */
+	ndev->dcb_cfg.dcbx_mode = DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_IEEE;
 }
 
 static u8 kvx_net_dcbnl_getcap(struct net_device *netdev, int capid, u8 *cap)
 {
-	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
-
 	switch (capid) {
 	case DCB_CAP_ATTR_PG:
 	case DCB_CAP_ATTR_PFC:
@@ -34,7 +31,8 @@ static u8 kvx_net_dcbnl_getcap(struct net_device *netdev, int capid, u8 *cap)
 		*cap = 1 << (KVX_ETH_PFC_CLASS_NB - 1);
 		break;
 	case DCB_CAP_ATTR_DCBX:
-		*cap = ndev->dcb_cfg.cap;
+		*cap = DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_CEE |
+		       DCB_CAP_DCBX_VER_IEEE;
 		break;
 	default:
 		*cap = DCB_CAP_ATTR_UNDEFINED;
@@ -48,25 +46,25 @@ static u8 kvx_net_dcbnl_getdcbx(struct net_device *netdev)
 {
 	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
 
-	netdev_dbg(netdev, "%s\n", __func__);
-	return ndev->dcb_cfg.cap;
+	return ndev->dcb_cfg.dcbx_mode;
 }
 
 static u8 kvx_net_dcbnl_setdcbx(struct net_device *netdev, u8 mode)
 {
 	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
 
-	netdev_dbg(netdev, "%s mode: %u\n", __func__, mode);
+	if (mode == ndev->dcb_cfg.dcbx_mode)
+		return 0;
+
 	/* no support for lld_managed modes or cee+ieee */
 	if ((mode & DCB_CAP_DCBX_LLD_MANAGED) ||
 	    ((mode & DCB_CAP_DCBX_VER_IEEE) && (mode & DCB_CAP_DCBX_VER_CEE)) ||
-	    !(mode & DCB_CAP_DCBX_HOST))
+	    !(mode & DCB_CAP_DCBX_HOST)) {
+		netdev_err(netdev, "lld_managed and cee+ieee modes are not supported\n");
 		return 1;
+	}
 
-	if (mode == ndev->dcb_cfg.cap)
-		return 0;
-
-	ndev->dcb_cfg.cap = mode;
+	ndev->dcb_cfg.dcbx_mode = mode;
 
 	return 0;
 }
@@ -75,17 +73,12 @@ static u8 kvx_net_dcbnl_getstate(struct net_device *netdev)
 {
 	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
 
-	netdev_dbg(netdev, "%s\n", __func__);
 	return ndev->dcb_cfg.state;
 }
 
 static u8 kvx_net_dcbnl_setstate(struct net_device *netdev, u8 state)
 {
 	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
-
-	netdev_dbg(netdev, "%s state: %u\n", __func__, state);
-	if (!(ndev->dcb_cfg.cap & DCB_CAP_DCBX_VER_CEE))
-		return 1;
 
 	ndev->dcb_cfg.state = state;
 
@@ -119,89 +112,18 @@ static void kvx_net_dcbnl_getpfccfg(struct net_device *netdev, int priority,
 	}
 
 	*setting = lb_f->cl_f[priority].pfc_ena;
-	netdev_dbg(netdev, "%s prio[%d]: %u\n", __func__, priority, *setting);
 }
 
-static void kvx_net_dcbnl_setpfccfg(struct net_device *netdev, int priority,
-				       u8 setting)
-{
-	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
-	struct kvx_eth_lane_cfg *cfg = &ndev->cfg;
-	struct kvx_eth_lb_f *lb_f = &cfg->hw->lb_f[cfg->id];
-	struct kvx_eth_tx_f *tx_f;
-
-	netdev_dbg(netdev, "%s prio[%d]: %u\n", __func__, priority, setting);
-	if (priority < 0 || priority >= KVX_ETH_PFC_CLASS_NB) {
-		netdev_err(netdev, "Invalid priority\n");
-		return;
-	}
-
-	lb_f->cl_f[priority].pfc_ena = setting;
-
-	kvx_eth_cl_f_cfg(ndev->hw, &lb_f->cl_f[priority]);
-	list_for_each_entry(tx_f, &cfg->tx_fifo_list, node)
-		kvx_eth_tx_f_cfg(ndev->hw, tx_f);
-}
-
-static u8 kvx_net_dcbnl_getpfcstate(struct net_device *netdev)
-{
-	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
-	struct kvx_eth_lane_cfg *cfg = &ndev->cfg;
-	struct kvx_eth_lb_f *lb_f = &cfg->hw->lb_f[cfg->id];
-	int i = 0;
-
-	netdev_dbg(netdev, "%s\n", __func__);
-	if (lb_f->pfc_f.global_pause_en)
-		return 0;
-
-	if (lb_f->pfc_f.global_pfc_en)
-		return 1;
-
-	for (i = 0; i < KVX_ETH_PFC_CLASS_NB; ++i) {
-		if (lb_f->cl_f[i].pfc_ena)
-			return 1;
-	}
-
-	return 0;
-}
-
-/** kvx_dcbnl_setpfcstate() - Set global PFC state
- */
-static void kvx_net_dcbnl_setpfcstate(struct net_device *netdev, u8 state)
-{
-	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
-	struct kvx_eth_lane_cfg *cfg = &ndev->cfg;
-	struct kvx_eth_lb_f *lb_f = &cfg->hw->lb_f[cfg->id];
-	struct kvx_eth_tx_f *tx_f;
-
-	netdev_dbg(netdev, "%s state: %u\n", __func__, state);
-	lb_f->pfc_f.global_pause_en = 0;
-	/* No PFC -> enable default Flow Control */
-	if (state == 0)
-		lb_f->pfc_f.global_pause_en = MLO_PAUSE_TXRX_MASK;
-
-	kvx_eth_pfc_f_cfg(ndev->hw, &lb_f->pfc_f);
-	list_for_each_entry(tx_f, &cfg->tx_fifo_list, node) {
-		tx_f->pfc_en = 0;
-		tx_f->pause_en = 1;
-		if (state) {
-			tx_f->pfc_en = 1;
-			tx_f->pause_en = 0;
-		}
-		kvx_eth_tx_f_cfg(ndev->hw, tx_f);
-	}
-}
-
-static int kvx_net_dcbnl_ieee_getpfc(struct net_device *netdev,
-				     struct ieee_pfc *pfc)
+static int kvx_net_dcbnl_get_pfc(struct net_device *netdev,
+				 struct ieee_pfc *pfc)
 {
 	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
 	struct kvx_eth_lane_cfg *cfg = &ndev->cfg;
 	struct kvx_eth_lb_f *lb_f = &cfg->hw->lb_f[cfg->id];
 	int i;
 
-	netdev_dbg(netdev, "%s\n", __func__);
 	pfc->pfc_cap = KVX_ETH_PFC_CLASS_NB;
+
 	if (lb_f->pfc_f.global_pause_en) {
 		pfc->pfc_en = 0;
 	} else if (lb_f->pfc_f.global_pfc_en) {
@@ -213,6 +135,110 @@ static int kvx_net_dcbnl_ieee_getpfc(struct net_device *netdev,
 				pfc->pfc_en |= 1 << i;
 		}
 	}
+
+	return 0;
+}
+
+static int kvx_net_dcbnl_set_pfc(struct net_device *netdev,
+				 struct ieee_pfc *pfc)
+{
+	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
+	struct kvx_eth_lane_cfg *cfg = &ndev->cfg;
+	struct kvx_eth_lb_f *lb_f = &cfg->hw->lb_f[cfg->id];
+	struct kvx_eth_tx_f *tx_f;
+	u32 pfc_cl_ena = 0, modified = false;
+	unsigned int i, val;
+
+	netdev_dbg(netdev, "%s  pfc_en=%u\n", __func__, pfc->pfc_en);
+
+	for (i = 0; i < KVX_ETH_PFC_CLASS_NB; ++i) {
+		val = !!(pfc->pfc_en & BIT(i));
+		if (lb_f->cl_f[i].pfc_ena != val) {
+			lb_f->cl_f[i].pfc_ena = val;
+			kvx_eth_cl_f_cfg(ndev->hw, &lb_f->cl_f[i]);
+			modified = true;
+		}
+		if (lb_f->cl_f[i].pfc_ena)
+			pfc_cl_ena |= BIT(i);
+	}
+
+	if (modified) {
+		if (pfc_cl_ena == (1 << KVX_ETH_PFC_CLASS_NB) - 1) {
+			netdev_warn(netdev, "Global PFC enabled\n");
+			lb_f->pfc_f.global_pfc_en = 1;
+			lb_f->pfc_f.global_pause_en = 0;
+		} else if (pfc_cl_ena == 0) {
+			netdev_warn(netdev, "Global pause enabled\n");
+			lb_f->pfc_f.global_pfc_en = 0;
+			lb_f->pfc_f.global_pause_en = 1;
+		} else {
+			netdev_warn(netdev, "Global pause/PFC disabled\n");
+			lb_f->pfc_f.global_pfc_en = 0;
+			lb_f->pfc_f.global_pause_en = 0;
+		}
+		kvx_eth_pfc_f_cfg(ndev->hw, &lb_f->pfc_f);
+
+		list_for_each_entry(tx_f, &cfg->tx_fifo_list, node) {
+			tx_f->pfc_en = !!pfc_cl_ena;
+			tx_f->pause_en = lb_f->pfc_f.global_pause_en;
+			kvx_eth_tx_f_cfg(ndev->hw, tx_f);
+		}
+	}
+
+	return 0;
+}
+
+static void kvx_net_dcbnl_setpfccfg(struct net_device *netdev, int priority,
+				       u8 setting)
+{
+	struct ieee_pfc pfc;
+
+	if (!setting)
+		return;
+
+	if (priority < 0 || priority >= KVX_ETH_PFC_CLASS_NB) {
+		netdev_err(netdev, "Invalid priority\n");
+		return;
+	}
+
+	kvx_net_dcbnl_get_pfc(netdev, &pfc);
+
+	if (setting != ((pfc.pfc_en >> priority) & 1)) {
+		pfc.pfc_en ^= BIT(priority);
+		kvx_net_dcbnl_set_pfc(netdev, &pfc);
+	}
+}
+
+static u8 kvx_net_dcbnl_getpfcstate(struct net_device *netdev)
+{
+	struct ieee_pfc pfc;
+
+	kvx_net_dcbnl_get_pfc(netdev, &pfc);
+	return !!pfc.pfc_en;
+}
+
+/** kvx_dcbnl_setpfcstate() - Set global PFC state
+ */
+static void kvx_net_dcbnl_setpfcstate(struct net_device *netdev, u8 state)
+{
+	struct ieee_pfc pfc;
+
+	pfc.pfc_en = 0;
+	if (state)
+		pfc.pfc_en = (1 << KVX_ETH_PFC_CLASS_NB) - 1;
+
+	kvx_net_dcbnl_set_pfc(netdev, &pfc);
+}
+
+static int kvx_net_dcbnl_ieee_getpfc(struct net_device *netdev,
+				     struct ieee_pfc *pfc)
+{
+	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
+	int ret, i;
+
+	ret = kvx_net_dcbnl_get_pfc(netdev, pfc);
+	if (ret < 0)
+		return ret;
 
 	for (i = 0; i < KVX_ETH_PFC_CLASS_NB; i++) {
 		pfc->requests[i] = ndev->stats.rx.cbfcpauseframesreceived[i];
@@ -226,41 +252,16 @@ static int kvx_net_dcbnl_ieee_getpfc(struct net_device *netdev,
 static int kvx_net_dcbnl_ieee_setpfc(struct net_device *netdev,
 				   struct ieee_pfc *pfc)
 {
-	struct kvx_eth_netdev *ndev = netdev_priv(netdev);
-	struct kvx_eth_lane_cfg *cfg = &ndev->cfg;
-	struct kvx_eth_lb_f *lb_f = &cfg->hw->lb_f[cfg->id];
-	struct kvx_eth_tx_f *tx_f;
-	bool pfc_cl_ena = false;
-	int i;
-
-	netdev_dbg(netdev, "%s\n", __func__);
-	if (!(ndev->dcb_cfg.cap & DCB_CAP_DCBX_VER_IEEE))
-		return -EINVAL;
-
-	for (i = 0; i < KVX_ETH_PFC_CLASS_NB; ++i) {
-		lb_f->cl_f[i].pfc_ena = !!(pfc->pfc_en & BIT(i));
-		if (lb_f->cl_f[i].pfc_ena)
-			pfc_cl_ena = true;
-		kvx_eth_cl_f_cfg(ndev->hw, &lb_f->cl_f[i]);
-	}
-	list_for_each_entry(tx_f, &cfg->tx_fifo_list, node) {
-		kvx_eth_tx_f_cfg(ndev->hw, tx_f);
-	}
-
-	/* No class enabled, restore default Flow Control */
-	if (pfc_cl_ena) {
-		lb_f->pfc_f.global_pfc_en = 0;
-		lb_f->pfc_f.global_pause_en = 1;
-		kvx_eth_pfc_f_cfg(ndev->hw, &lb_f->pfc_f);
-	}
-
-	return 0;
+	return kvx_net_dcbnl_set_pfc(netdev, pfc);
 }
 
 static const struct dcbnl_rtnl_ops dcbnl_ops = {
-	.getcap      = kvx_net_dcbnl_getcap,
+	/* DCBX configuration */
 	.getdcbx     = kvx_net_dcbnl_getdcbx,
 	.setdcbx     = kvx_net_dcbnl_setdcbx,
+
+	/* CEE std */
+	.getcap      = kvx_net_dcbnl_getcap,
 	.getstate    = kvx_net_dcbnl_getstate,
 	.setstate    = kvx_net_dcbnl_setstate,
 	.getnumtcs   = kvx_net_dcbnl_getnumtcs,
@@ -268,6 +269,8 @@ static const struct dcbnl_rtnl_ops dcbnl_ops = {
 	.getpfccfg   = kvx_net_dcbnl_getpfccfg,
 	.getpfcstate = kvx_net_dcbnl_getpfcstate,
 	.setpfcstate = kvx_net_dcbnl_setpfcstate,
+
+	/* IEEE 802.1Qaz std */
 	.ieee_getpfc = kvx_net_dcbnl_ieee_getpfc,
 	.ieee_setpfc = kvx_net_dcbnl_ieee_setpfc,
 };
@@ -276,4 +279,3 @@ void kvx_set_dcb_ops(struct net_device *netdev)
 {
 	netdev->dcbnl_ops = &dcbnl_ops;
 }
-
