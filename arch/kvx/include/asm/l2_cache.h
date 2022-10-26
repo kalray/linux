d40368de10702f48662d6e75dcd6a3c2f41ae61e
Julian Vetter <jvetter@kalrayinc.com>
2022-10-26 20:47:14 +0200

    kvx: l2: Replaced communication with l2 cache firmware on RM
    Summary:
    Updated L2 cache subsystem for CV2:
    * Created an new L2 cache driver file for CV2 -> Removed the L2 cache protocol
      between RM and PE and replaced it by HW instructions
    * New CV2 L2 cache driver relies on new DTB node
      `kalray,kvx-secure-cluster-registers`
    * For CV2 user can select the size of the L2 cache (between 2 MBytes and 4
      Mbytes)
    * Split off secure cluster register definitions from power controller
    * Use `num_possible_cpus()` instead of `CONFIG_SMP` to determine whether we can
      run without the L2 cache enabled (fix D19826) will ensure correct behaviour
      when the user provides `nr_cpus=X`
    
    Ref: T17824
    
    Test Plan: tbd
    
    Reviewers: O51 Linux Coolidge, ysionneau, jmaselbas
    
    Reviewed By: O51 Linux Coolidge, ysionneau, jmaselbas
    
    Subscribers: jhascoet, lbouchot, #linux_coolidge_cc
    
    Maniphest Tasks: T17824
    
    Differential Revision: https://phab.kalray.eu/D19499

--- arch/kvx/include/asm/l2_cache.h
+++ arch/kvx/include/asm/l2_cache.h
@@ -1,54 +1,22 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (C) 2017-2022 Kalray Inc.
- * Author(s): Clement Leger
+ * Author(s): Julian Vetter
+ *            Clement Leger
  */
 
 #ifndef _ASM_KVX_L2_CACHE_H
 #define _ASM_KVX_L2_CACHE_H
 
-#include <linux/bits.h>
-#include <linux/jump_label.h>
-
 #ifdef CONFIG_L2_CACHE
 
-#include <asm/cache.h>
-#include <asm/l2_cache_defs.h>
-
-void l2_cache_push_area_cmd(u64 cmd_type, phys_addr_t start,
-			    unsigned long size);
-
-extern struct static_key_false l2_enabled;
-
-static bool
-l2_cache_send_cmd(u64 cmd_type, phys_addr_t start, unsigned long size)
-{
-	if (static_branch_likely(&l2_enabled)) {
-		l2_cache_push_area_cmd(cmd_type, start, size);
-		return true;
-	}
-
-	return false;
-}
-
-static inline bool
-l2_cache_wbinval_range(phys_addr_t start, unsigned long size)
-{
-	return l2_cache_send_cmd(L2_CMD_OP_CMD_PURGE_AREA, start, size);
-}
-
-static inline bool
-l2_cache_wb_range(phys_addr_t start, unsigned long size)
-{
-	return l2_cache_send_cmd(L2_CMD_OP_CMD_FLUSH_AREA, start, size);
-}
-
-static inline bool
-l2_cache_inval_range(phys_addr_t start, unsigned long size)
-{
-	/* We don't need invalidation to be synced (ie 0) */
-	return l2_cache_send_cmd(L2_CMD_OP_CMD_INVAL_AREA, start, size);
-}
+#if defined(CONFIG_KVX_SUBARCH_KV3_1)
+#include <asm/v1/l2_cache.h>
+#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
+#include <asm/v2/l2_cache.h>
+#else
+#error "Unsupported arch"
+#endif
 
 #else
 
--- arch/kvx/include/asm/pwr_ctrl.h
+++ arch/kvx/include/asm/pwr_ctrl.h
@@ -3,6 +3,7 @@
  * Copyright (C) 2017-2022 Kalray Inc.
  * Author(s): Clement Leger
  *            Marius Gligor
+ *            Julian Vetter
  */
 
 #ifndef _ASM_KVX_PWR_CTRL_H
@@ -16,26 +17,22 @@ void kvx_pwr_ctrl_cpu_poweron(unsigned int cpu);
 
 #endif
 
+#define PWR_CTRL_ADDR                           0xA40000
+
 /* Power controller vector register definitions */
-#define KVX_PWR_CTRL_VEC_OFFSET 0x1000
-#define KVX_PWR_CTRL_VEC_WUP_SET_OFFSET     0x10
-#define KVX_PWR_CTRL_VEC_WUP_CLEAR_OFFSET     0x20
+#define KVX_PWR_CTRL_VEC_OFFSET                 0x1000
+#define KVX_PWR_CTRL_VEC_WUP_SET_OFFSET         0x10
+#define KVX_PWR_CTRL_VEC_WUP_CLEAR_OFFSET       0x20
 
 /* Power controller PE reset PC register definitions */
-#define KVX_PWR_CTRL_RESET_PC_OFFSET               0x2000
+#define KVX_PWR_CTRL_RESET_PC_OFFSET            0x2000
 
 /* Power controller global register definitions */
-#if defined(CONFIG_KVX_SUBARCH_KV3_1)
-#define KVX_PWR_CTRL_GLOBAL_OFFSET 0x4040
-#elif defined(CONFIG_KVX_SUBARCH_KV3_2)
-#define KVX_SEC_CLUSTER_ADDR 0xCC2000
-#define KVX_PWR_CTRL_GLOBAL_OFFSET 0x40
-#else
-#error Unsupported arch
-#endif
+#define KVX_PWR_CTRL_GLOBAL_OFFSET              0x4040
 
-#define KVX_PWR_CTRL_GLOBAL_SET_OFFSET     0x10
-#define KVX_PWR_CTRL_GLOBAL_SET_PE_EN_SHIFT           0x1
+#define KVX_PWR_CTRL_GLOBAL_SET_OFFSET          0x10
+#define KVX_PWR_CTRL_GLOBAL_CLEAR_OFFSET        0x20
+#define KVX_PWR_CTRL_GLOBAL_SET_PE_EN_SHIFT     0x1
 
 #define PWR_CTRL_WUP_SET_OFFSET  \
 		(KVX_PWR_CTRL_VEC_OFFSET + \
@@ -45,8 +42,15 @@ void kvx_pwr_ctrl_cpu_poweron(unsigned int cpu);
 		(KVX_PWR_CTRL_VEC_OFFSET + \
 		 KVX_PWR_CTRL_VEC_WUP_CLEAR_OFFSET)
 
-#define PWR_CTRL_GLOBAL_CONFIG_OFFSET \
+#define PWR_CTRL_GLOBAL_CONFIG_SET_OFFSET \
 		(KVX_PWR_CTRL_GLOBAL_OFFSET + \
 		 KVX_PWR_CTRL_GLOBAL_SET_OFFSET)
+
+#define PWR_CTRL_GLOBAL_CONFIG_CLEAR_OFFSET \
+		(KVX_PWR_CTRL_GLOBAL_OFFSET + \
+		 KVX_PWR_CTRL_GLOBAL_CLEAR_OFFSET)
+
+#define PWR_CTRL_GLOBAL_CONFIG_PE_EN \
+	(1 << KVX_PWR_CTRL_GLOBAL_SET_PE_EN_SHIFT)
 
 #endif /* _ASM_KVX_PWR_CTRL_H */
