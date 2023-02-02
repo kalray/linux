d0bef7c70888a1a8a83e6c32f00eb5a7e87bda66
Guillaume Pommeret <gpommeret@zephyr.lin.mbt.kalray.eu>
2023-02-02 16:30:20 +0100

    kvx: eth: cv2 - udpate for lb RFS handling: sysfs, irq
    Summary:
    We update the content of the kvx-net sysfs in order to allow
    simple access to the EthRx RFS block of the load balancer.
    This includes:
      param_fk_cmd: set to 1 for writing the fk_part @ fk_idx
      ctrl_hash_rss_ena: same as register bit
      ctrl_rfs_ena: same as register bit
      seed_command: 0 to write the part in hash0 seed @row/idx
                    1 to write the part in hash1 seed @row/idx
      fk_command: same as bitfield in register
      counter of RFS table corruption counter
    Also added, handling of the Irq ethernet rx_error with a
    simple counter increment.
    
    Ref T20054
    
    Depends On D27200
    Depends On D27199
    Depends On D27198
    Depends On D24240
    Depends On D24142
    Depends On D24139
    Depends On D23978
    Depends On D23774
    
    Test Plan: CI: CV1 testsuite, CV2 validsuite (partial)
    
    Reviewers: O39 ethernet!, O129 linux drivers
    
    Maniphest Tasks: T20054
    
    Differential Revision: https://phab.kalray.eu/D27201

--- drivers/net/ethernet/kalray/kvx-net-hw.c
+++ drivers/net/ethernet/kalray/kvx-net-hw.c
@@ -141,7 +141,7 @@ void kvx_eth_hw_change_mtu(struct kvx_eth_hw *hw, int lane, int mtu)
 	kvx_eth_writel(hw, mtu, TX_OFFSET + TX_LANE +
 		       lane * TX_LANE_ELEM_SIZE + TX_LANE_MTU);
 #else
-	kvx_eth_tx_writel(hw, mtu, KVX_ETH_TX_STAGE_TWO_GRP_OFFSET +
+	kvx_tx_writel(hw, mtu, KVX_ETH_TX_STAGE_TWO_GRP_OFFSET +
 		KVX_ETH_TX_STAGE_TWO_GRP_ELEM_SIZE * lane +
 		KVX_ETH_TX_STAGE_TWO_MTU_OFFSET);
 #endif
@@ -379,34 +379,125 @@ static void lb_f_update(void *data)
 #else
 static void lb_f_update(void *data)
 {
-	struct kvx_eth_lb_f *lb = (struct kvx_eth_lb_f *)data;
+		struct kvx_eth_lb_f *lb = (struct kvx_eth_lb_f *)data;
 	u32 reg = KVX_ETH_LBA_STATUS_COUNTERS_GRP_OFFSET +
 		lb->id * KVX_ETH_LBA_STATUS_COUNTERS_GRP_ELEM_SIZE;
 
-	lb->drop_mtu_err_cnt = kvx_eth_rxlbana_readl(lb->hw,
+	lb->drop_mtu_err_cnt = kvx_lbana_readl(lb->hw,
 				 reg + KVX_ETH_LBA_STATUS_COUNTERS_MTU_ERROR_DROP_CNT_OFFSET);
-	lb->drop_fcs_err_cnt = kvx_eth_rxlbana_readl(lb->hw,
+	lb->drop_fcs_err_cnt = kvx_lbana_readl(lb->hw,
 				 reg + KVX_ETH_LBA_STATUS_COUNTERS_MAC_ERROR_DROP_CNT_OFFSET);
-	lb->drop_crc_err_cnt = kvx_eth_rxlbana_readl(lb->hw,
+	lb->drop_crc_err_cnt = kvx_lbana_readl(lb->hw,
 				 reg + KVX_ETH_LBA_STATUS_COUNTERS_CRC_DROP_CNT_OFFSET);
-	lb->drop_total_cnt = kvx_eth_rxlbana_readl(lb->hw,
+	lb->drop_total_cnt = kvx_lbana_readl(lb->hw,
 				 reg + KVX_ETH_LBA_STATUS_COUNTERS_TOTAL_DROP_CNT_OFFSET);
-	lb->default_hit_cnt = kvx_eth_rxlbana_readl(lb->hw,
+	lb->default_hit_cnt = kvx_lbana_readl(lb->hw,
 				 RX_LB_DEFAULT_RULE_HIT_CNT(lb->id));
-	lb->keep_all_crc_error_pkt = kvx_eth_rxlbana_readl(lb->hw,
+	lb->keep_all_crc_error_pkt = kvx_lbana_readl(lb->hw,
 				 RX_LB_ERROR_CTRL(lb->id));
 	/* added */
-	lb->default_dispatch_info = kvx_eth_rxlbana_readl(lb->hw,
+	lb->default_dispatch_info = kvx_lbana_readl(lb->hw,
 				 RX_LB_DEFAULT_RULE_DISPATCH_INFO(lb->id));
-	lb->default_flow_type = kvx_eth_rxlbana_readl(lb->hw,
+	lb->default_flow_type = kvx_lbana_readl(lb->hw,
 				 RX_LB_DEFAULT_FLOW_TYPE(lb->id));
 }
 void kvx_eth_rx_dlv_pfc_f_update(void *data)
 {
 	struct kvx_eth_rx_dlv_pfc_f *rx_dlv_pfc = (struct kvx_eth_rx_dlv_pfc_f *)data;
 
-	rx_dlv_pfc->total_drop_cnt = kvx_eth_rxlbdel_readl(rx_dlv_pfc->hw, KVX_ETH_LBD_PFC_CFG_GRP_OFFSET
+	rx_dlv_pfc->total_drop_cnt = kvx_lbdel_readl(rx_dlv_pfc->hw, KVX_ETH_LBD_PFC_CFG_GRP_OFFSET
 							+ KVX_ETH_LBD_PFC_CFG_TOTAL_DROP_CNT_OFFSET);
+}
+void kvx_eth_lb_rfs_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_lb_rfs_f *lb_rfs)
+{
+	u32 val;
+	u32 reg;
+
+	if (lb_rfs->param_fk_cmd == RFS_PARAM_FK_CMD_WRITE) {
+		kvx_lbrfs_writel(hw, lb_rfs->param_fk_part,
+					KVX_ETH_LBR_MGMT_FLOW_KEY_OFFSET +
+					KVX_ETH_LBR_MGMT_FLOW_KEY_ELEM_SIZE * lb_rfs->param_fk_idx);
+		lb_rfs->param_fk_cmd = RFS_PARAM_FK_NO_CMD;
+	}
+	if (lb_rfs->ctrl_hash_rss_ena != RFS_HASH_RSS_NO_CMD) {
+		updatel_bits(hw, ETH_RX_LB_RFS, KVX_ETH_LBR_CONTROL_OFFSET,
+			KVX_ETH_LBR_CONTROL_ENABLE_RSS_HASH_MASK,
+				((lb_rfs->ctrl_hash_rss_ena<<KVX_ETH_LBR_CONTROL_ENABLE_RSS_HASH_SHIFT)
+				& KVX_ETH_LBR_CONTROL_ENABLE_RSS_HASH_MASK));
+		lb_rfs->ctrl_hash_rss_ena = RFS_HASH_RSS_NO_CMD;
+	}
+	if (lb_rfs->ctrl_rfs_ena != RFS_CTRL_RFS_NO_CMD) {
+		updatel_bits(hw, ETH_RX_LB_RFS, KVX_ETH_LBR_CONTROL_OFFSET,
+			KVX_ETH_LBR_CONTROL_ENABLE_MASK,
+				((lb_rfs->ctrl_rfs_ena<<KVX_ETH_LBR_CONTROL_ENABLE_SHIFT)
+				& KVX_ETH_LBR_CONTROL_ENABLE_MASK));
+		/* enable irq */
+		val = lb_rfs->ctrl_rfs_ena;
+		kvx_lbrfs_writel(hw, val,
+					KVX_ETH_LBR_INTERRUPT_ENABLE_OFFSET);
+		lb_rfs->ctrl_rfs_ena = RFS_CTRL_RFS_NO_CMD;
+	}
+	if (lb_rfs->seed_command != RFS_CTRL_SEED_NO_CMD) {
+		/* 0: write on seed 0, 1: write on seed 1 */
+		if (lb_rfs->seed_command == RFS_WRITE_IN_SEED_0)
+			reg = KVX_ETH_LBR_HASH0_ROW0_SEED_OFFSET;
+		else
+			reg = KVX_ETH_LBR_HASH1_ROW0_SEED_OFFSET;
+		reg += lb_rfs->seed_row * (KVX_ETH_LBR_HASH0_ROW1_SEED_OFFSET - KVX_ETH_LBR_HASH0_ROW0_SEED_OFFSET);
+		reg += lb_rfs->seed_idx * KVX_ETH_LBR_HASH0_ROW0_SEED_ELEM_SIZE;
+		kvx_lbrfs_writel(hw, lb_rfs->seed_part,
+			  reg);
+		lb_rfs->seed_command = RFS_CTRL_SEED_NO_CMD;
+	}
+	if (lb_rfs->fk_command != RFS_FK_NO_CMD) {
+		/* writing of every parameters except fk written by part */
+		kvx_lbrfs_writel(hw, lb_rfs->param_ftype,
+					KVX_ETH_LBR_MGMT_FLOW_TYPE_OFFSET);
+		kvx_lbrfs_writel(hw, lb_rfs->param_dpatch_info,
+					KVX_ETH_LBR_MGMT_DISPATCH_INFO_OFFSET);
+		kvx_lbrfs_writel(hw, lb_rfs->param_flow_id,
+					KVX_ETH_LBR_MGMT_FLOW_ID_OFFSET);
+		kvx_lbrfs_writel(hw, (lb_rfs->fk_command << KVX_ETH_LBR_MGMT_CTRL_OPERATION_SHIFT) |
+						KVX_ETH_LBR_MGMT_CTRL_RUN_MASK |
+						(1 << KVX_ETH_LBR_MGMT_CTRL_DISABLE_S2F_MGMT_SHIFT),
+						KVX_ETH_LBR_MGMT_CTRL_OFFSET);
+		lb_rfs->fk_command = RFS_FK_NO_CMD;
+	}
+}
+static void kvx_eth_lb_rfs_f_update(void *data)
+{
+	struct kvx_eth_lb_rfs_f *lb_rfs = (struct kvx_eth_lb_rfs_f *)data;
+	struct kvx_eth_hw *hw = lb_rfs->hw;
+
+	lb_rfs->version = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_VERSION_OFFSET);
+	/* no read access to param for flow key insertion */
+	/* status part */
+	lb_rfs->status = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_RFS_STATUS_OFFSET);
+	lb_rfs->status_tables = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_RFS_TABLES_STATUS_OFFSET);
+	lb_rfs->status_wmark = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_WATERMARK_STATUS_OFFSET);
+	lb_rfs->status_mgmt = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_MGMT_STATUS_OFFSET);
+	lb_rfs->status_fk_part = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_MGMT_FLOW_KEY_STATUS_OFFSET + (lb_rfs->status_fk_idx<<2));
+	lb_rfs->status_ftype = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_MGMT_FLOW_TYPE_STATUS_OFFSET);
+	lb_rfs->status_dpatch_info = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_MGMT_DISPATCH_INFO_OFFSET);
+	lb_rfs->status_flow_id = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_MGMT_FLOW_ID_STATUS_OFFSET);
+	/* corruption_status part */
+	lb_rfs->corr_status = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_CORRUPTION_STATUS_OFFSET);
+	lb_rfs->corr_fk_part = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_CORRUPTION_FLOW_KEY_OFFSET + (lb_rfs->corr_fk_idx<<2));
+	lb_rfs->corr_tables = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_CORRUPTION_TABLES_STATUS_OFFSET);
+	lb_rfs->corr_fk_type = kvx_lbrfs_readl(hw,
+		KVX_ETH_LBR_CORRUPTION_FLOW_TYPE_OFFSET);
 }
 #endif
 
@@ -432,6 +523,17 @@ void kvx_eth_lb_f_init(struct kvx_eth_hw *hw, struct kvx_eth_lane_cfg *cfg)
 		hw->lut_entry_f[i].id = i;
 		hw->lut_entry_f[i].update = lut_entry_f_update;
 	}
+#ifdef CONFIG_KVX_SUBARCH_KV3_2
+	hw->lb_rfs_f.hw = hw;
+	hw->lb_rfs_f.update = kvx_eth_lb_rfs_f_update;
+	hw->lb_rfs_f.param_fk_cmd = RFS_PARAM_FK_NO_CMD;
+	hw->lb_rfs_f.fk_command = RFS_FK_NO_CMD;
+	hw->lb_rfs_f.ctrl_hash_rss_ena = RFS_HASH_RSS_NO_CMD;
+	hw->lb_rfs_f.ctrl_rfs_ena = RFS_CTRL_RFS_NO_CMD;
+	hw->lb_rfs_f.seed_command = RFS_CTRL_SEED_NO_CMD;
+	hw->rx_dlv_pfc_f.update = kvx_eth_rx_dlv_pfc_f_update;
+	hw->lb_rfs_f.it_tbl_corrupt_cnt = 0;
+#endif
 }
 
 #ifdef CONFIG_KVX_SUBARCH_KV3_1
@@ -464,24 +566,24 @@ static void kvx_eth_parser_f_update(void *data)
 	struct kvx_eth_parser_f *p = (struct kvx_eth_parser_f *)data;
 	u32 off = KVX_ETH_LBA_PARSER_GRP_OFFSET + KVX_ETH_LBA_PARSER_GRP_ELEM_SIZE * p->id;
 
-	p->disp_policy = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_DISPATCH_POLICY_OFFSET);
-	p->disp_info = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_DISPATCH_INFO_OFFSET);
-	p->flow_type = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_FLOW_TYPE_OFFSET);
-	p->flow_key_ctrl = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_FLOW_KEY_CTRL_OFFSET);
-	p->hit_cnt = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_HIT_CNT_OFFSET);
-	p->ctrl = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_CTRL_OFFSET);
-	p->status = kvx_eth_rxlbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_STATUS_OFFSET);
+	p->disp_policy = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_DISPATCH_POLICY_OFFSET);
+	p->disp_info = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_DISPATCH_INFO_OFFSET);
+	p->flow_type = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_FLOW_TYPE_OFFSET);
+	p->flow_key_ctrl = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_FLOW_KEY_CTRL_OFFSET);
+	p->hit_cnt = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_HIT_CNT_OFFSET);
+	p->ctrl = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_CTRL_OFFSET);
+	p->status = kvx_lbana_readl(p->hw, off + KVX_ETH_LBA_PARSER_STATUS_OFFSET);
 }
 
 void kvx_eth_parser_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_parser_f *p)
 {
 	u32 off = KVX_ETH_LBA_PARSER_GRP_OFFSET + KVX_ETH_LBA_PARSER_GRP_ELEM_SIZE * p->id;
 
-	kvx_eth_rxlbana_writel(hw, p->disp_policy, off +  KVX_ETH_LBA_PARSER_DISPATCH_POLICY_OFFSET);
-	kvx_eth_rxlbana_writel(hw, p->disp_info,  off + KVX_ETH_LBA_PARSER_DISPATCH_INFO_OFFSET);
-	kvx_eth_rxlbana_writel(hw, p->flow_type,  off + KVX_ETH_LBA_PARSER_FLOW_TYPE_OFFSET);
-	kvx_eth_rxlbana_writel(hw, p->flow_key_ctrl,  off + KVX_ETH_LBA_PARSER_FLOW_KEY_CTRL_OFFSET);
-	kvx_eth_rxlbana_writel(hw, p->ctrl,  off + KVX_ETH_LBA_PARSER_CTRL_OFFSET);
+	kvx_lbana_writel(hw, p->disp_policy, off +  KVX_ETH_LBA_PARSER_DISPATCH_POLICY_OFFSET);
+	kvx_lbana_writel(hw, p->disp_info,  off + KVX_ETH_LBA_PARSER_DISPATCH_INFO_OFFSET);
+	kvx_lbana_writel(hw, p->flow_type,  off + KVX_ETH_LBA_PARSER_FLOW_TYPE_OFFSET);
+	kvx_lbana_writel(hw, p->flow_key_ctrl,  off + KVX_ETH_LBA_PARSER_FLOW_KEY_CTRL_OFFSET);
+	kvx_lbana_writel(hw, p->ctrl,  off + KVX_ETH_LBA_PARSER_CTRL_OFFSET);
 }
 
 void kvx_eth_parser_f_init(struct kvx_eth_hw *hw, struct kvx_eth_lane_cfg *cfg)
@@ -618,16 +720,16 @@ void kvx_eth_lbana_set_default(struct kvx_eth_hw *hw, u8 dispatch_info)
 	int i;
 
 	for (i = 0; i < KVX_ETH_LANE_NB; i++) {
-		kvx_eth_rxlbana_writel(hw, dispatch_info, RX_LB_DEFAULT_RULE_DISPATCH_INFO(i));
-		kvx_eth_rxlbana_writel(hw,
+		kvx_lbana_writel(hw, dispatch_info, RX_LB_DEFAULT_RULE_DISPATCH_INFO(i));
+		kvx_lbana_writel(hw,
 			hw->lb_f[i].keep_all_crc_error_pkt &
 				KVX_ETH_LBA_CONTROL_LB_ERROR_CTRL_KEEP_ALL_CRC_ERROR_PKT_MASK,
 			RX_LB_ERROR_CTRL(i));
 	}
 
 	for (i = 0; i < KVX_ETH_PHYS_PARSER_NB; ++i) {
-		kvx_eth_rxlbana_writel(hw, POLICY_PARSER, RX_LB_PARSER_DISPATCH_POLICY(i));
-		kvx_eth_rxlbana_writel(hw, dispatch_info, RX_LB_PARSER_DISPATCH_INFO(i));
+		kvx_lbana_writel(hw, POLICY_PARSER, RX_LB_PARSER_DISPATCH_POLICY(i));
+		kvx_lbana_writel(hw, dispatch_info, RX_LB_PARSER_DISPATCH_INFO(i));
 	}
 }
 #endif
@@ -691,9 +793,9 @@ void kvx_eth_lb_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_lb_f *lb)
 #else
 void kvx_eth_lb_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_lb_f *lb)
 {
-	kvx_eth_rxlbana_writel(hw, lb->default_dispatch_info,
+	kvx_lbana_writel(hw, lb->default_dispatch_info,
 		RX_LB_DEFAULT_RULE_DISPATCH_INFO(lb->id));
-	kvx_eth_rxlbana_writel(hw,
+	kvx_lbana_writel(hw,
 		lb->keep_all_crc_error_pkt &
 		      KVX_ETH_LBA_CONTROL_LB_ERROR_CTRL_KEEP_ALL_CRC_ERROR_PKT_MASK,
 		RX_LB_ERROR_CTRL(lb->id));
--- drivers/net/ethernet/kalray/v2/kvx-net-tx.c
+++ drivers/net/ethernet/kalray/v2/kvx-net-tx.c
@@ -29,66 +29,66 @@ void kvx_eth_tx_init(struct kvx_eth_hw *hw)
 	u32 base;
 
 	/* Default stage one config */
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_STAGE_ONE_CFG_1_FIFO_8K,
+	kvx_tx_writel(hw, KVX_ETH_TX_STAGE_ONE_CFG_1_FIFO_8K,
 		KVX_ETH_TX_STAGE_ONE_GRP_OFFSET + KVX_ETH_TX_STAGE_ONE_CONFIG_OFFSET);
 
 	/* Enable credit bus */
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_CREDIT_ENABLE_ALL,
+	kvx_tx_writel(hw, KVX_ETH_TX_CREDIT_ENABLE_ALL,
 		KVX_ETH_TX_CREDIT_GRP_OFFSET + KVX_ETH_TX_CREDIT_ENABLE_OFFSET);
 
 	for (i = 0; i < KVX_ETH_LANE_NB; i++) {
 		base = KVX_ETH_TX_STAGE_TWO_GRP_OFFSET + KVX_ETH_TX_STAGE_TWO_GRP_ELEM_SIZE * i;
 
 		/* Drop in case all error */
-		kvx_eth_tx_writel(hw, KVX_ETH_TX_STAGE_TWO_DROP_DISABLE_NONE,
+		kvx_tx_writel(hw, KVX_ETH_TX_STAGE_TWO_DROP_DISABLE_NONE,
 				base + KVX_ETH_TX_STAGE_TWO_DROP_DISABLE_OFFSET);
 		/* Default MTU to MAX */
-		kvx_eth_tx_writel(hw, KVX_ETH_MAX_MTU,
+		kvx_tx_writel(hw, KVX_ETH_MAX_MTU,
 				base + KVX_ETH_TX_STAGE_TWO_MTU_OFFSET);
 		/* Counter: count all drops */
-		kvx_eth_tx_writel(hw, 0xF,
+		kvx_tx_writel(hw, 0xF,
 				base + KVX_ETH_TX_STAGE_TWO_DROP_CNT_MSK_OFFSET);
 		/* Counter: count drops from all target */
-		kvx_eth_tx_writel(hw, KVX_ETH_TX_STAGE_TWO_CNT_SUBSCR_TGT_ALL, base +
+		kvx_tx_writel(hw, KVX_ETH_TX_STAGE_TWO_CNT_SUBSCR_TGT_ALL, base +
 				KVX_ETH_TX_STAGE_TWO_DROP_CNT_SUBSCR_OFFSET);
 		/* CBS disable */
 		for (j = 0; j < KVX_ETH_TX_TGT_NB; j++) {
-			kvx_eth_tx_writel(hw, KVX_ETH_TX_CBS_DISABLE,
+			kvx_tx_writel(hw, KVX_ETH_TX_CBS_DISABLE,
 					KVX_ETH_TX_CBS_GRP_OFFSET + KVX_ETH_TX_CBS_GRP_ELEM_SIZE * i +
 					KVX_ETH_TX_CBS_CBS_ENABLE_OFFSET + KVX_ETH_TX_CBS_CBS_ENABLE_ELEM_SIZE * j);
 		}
 		/* TAS disable */
 		for (j = 0; j < KVX_ETH_TX_TAS_NB; j++) {
-			kvx_eth_tx_writel(hw, KVX_ETH_TX_TAS_DISABLE,
+			kvx_tx_writel(hw, KVX_ETH_TX_TAS_DISABLE,
 					KVX_ETH_TX_TAS_GRP_OFFSET + KVX_ETH_TX_TAS_GRP_ELEM_SIZE * i +
 					KVX_ETH_TX_TAS_TAS_ENABLE_OFFSET + KVX_ETH_TX_TAS_TAS_ENABLE_ELEM_SIZE * j);
 		}
 		/* PFC/XOFF disable */
 		for (j = 0; j < KVX_ETH_TX_TGT_NB; j++) {
-			kvx_eth_tx_writel(hw, KVX_ETH_TX_PFC_XOFF_DIS_GLBL_PAUS_DIS,
+			kvx_tx_writel(hw, KVX_ETH_TX_PFC_XOFF_DIS_GLBL_PAUS_DIS,
 					KVX_ETH_TX_PFC_GRP_OFFSET + KVX_ETH_TX_PFC_GRP_ELEM_SIZE * i +
 					KVX_ETH_TX_PFC_XOFF_SUBSCR_ELEM_SIZE * j);
 		}
 		/* All target fifos to prio 0 */
-		kvx_eth_tx_writel(hw, 0x0,
+		kvx_tx_writel(hw, 0x0,
 				KVX_ETH_TX_PBDWRR_GRP_OFFSET + KVX_ETH_TX_PBDWRR_GRP_ELEM_SIZE * i
 				+ KVX_ETH_TX_PBDWRR_PRIORITY_OFFSET);
 		/* Map traffic to preemptable lane */
-		kvx_eth_tx_writel(hw, KVX_ETH_TX_PBDWRR_CONFIG_DWRR_DISABLE,
+		kvx_tx_writel(hw, KVX_ETH_TX_PBDWRR_CONFIG_DWRR_DISABLE,
 				KVX_ETH_TX_PBDWRR_GRP_OFFSET + KVX_ETH_TX_PBDWRR_GRP_ELEM_SIZE * i +
 				KVX_ETH_TX_PBDWRR_CONFIG_OFFSET);
 
 		/* Map traffic to preemptable lane */
-		kvx_eth_tx_writel(hw, 0x0,
+		kvx_tx_writel(hw, 0x0,
 				KVX_ETH_TX_EXP_NPRE_GRP_OFFSET + KVX_ETH_TX_EXP_NPRE_GRP_ELEM_SIZE * i +
 				KVX_ETH_TX_EXP_NPRE_CONFIG_OFFSET);
 	}
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_TDM_CONFIG_BY4_AGG,
+	kvx_tx_writel(hw, KVX_ETH_TX_TDM_CONFIG_BY4_AGG,
 			KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_CONFIG_OFFSET);
 	/* Enable correct fcs */
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_FCS_ENABLE_ALL,
+	kvx_tx_writel(hw, KVX_ETH_TX_FCS_ENABLE_ALL,
 			KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_FCS_OFFSET);
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_ERRFCS_DISABLE_ALL,
+	kvx_tx_writel(hw, KVX_ETH_TX_ERRFCS_DISABLE_ALL,
 			KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_ERR_OFFSET);
 }
 
@@ -106,43 +106,43 @@ void kvx_eth_tx_cfg_speed_settings(struct kvx_eth_hw *hw, struct kvx_eth_lane_cf
 		}
 	}
 	/* update the stage one configuration (max depth according to used lanes) */
-	kvx_eth_tx_writel(hw, stage_one_config, KVX_ETH_TX_STAGE_ONE_GRP_OFFSET + KVX_ETH_TX_STAGE_ONE_CONFIG_OFFSET);
+	kvx_tx_writel(hw, stage_one_config, KVX_ETH_TX_STAGE_ONE_GRP_OFFSET + KVX_ETH_TX_STAGE_ONE_CONFIG_OFFSET);
 	/* update the TDM configuration */
-	kvx_eth_tx_writel(hw, tdm_config, KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_CONFIG_OFFSET);
+	kvx_tx_writel(hw, tdm_config, KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_CONFIG_OFFSET);
 }
 void kvx_eth_tx_stage_one_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_stage_one_f *stage_one)
 {
 	/* credit not writable via sysfs */
-	kvx_eth_tx_writel(hw, stage_one->config,
+	kvx_tx_writel(hw, stage_one->config,
 				KVX_ETH_TX_STAGE_ONE_GRP_OFFSET	+ KVX_ETH_TX_STAGE_ONE_CONFIG_OFFSET);
 }
 static void kvx_eth_tx_stage_one_f_update(void *data)
 {
 	struct kvx_eth_tx_stage_one_f *stage_one = (struct kvx_eth_tx_stage_one_f *)data;
 
-	stage_one->credit = kvx_eth_tx_readl(stage_one->hw,
+	stage_one->credit = kvx_tx_readl(stage_one->hw,
 				KVX_ETH_TX_CREDIT_GRP_OFFSET + KVX_ETH_TX_CREDIT_ENABLE_OFFSET);
-	stage_one->config = kvx_eth_tx_readl(stage_one->hw,
+	stage_one->config = kvx_tx_readl(stage_one->hw,
 				KVX_ETH_TX_STAGE_ONE_GRP_OFFSET	+ KVX_ETH_TX_STAGE_ONE_CONFIG_OFFSET);
 }
 
 void kvx_eth_tx_tdm_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_tdm_f *tdm)
 {
 	/* config not writable via sysfs */
-	kvx_eth_tx_writel(hw, tdm->fcs,
+	kvx_tx_writel(hw, tdm->fcs,
 				KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_FCS_OFFSET);
-	kvx_eth_tx_writel(hw, tdm->err,
+	kvx_tx_writel(hw, tdm->err,
 				KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_ERR_OFFSET);
 }
 static void kvx_eth_tx_tdm_f_update(void *data)
 {
 	struct kvx_eth_tx_tdm_f *tdm = (struct kvx_eth_tx_tdm_f *)data;
 
-	tdm->config = kvx_eth_tx_readl(tdm->hw,
+	tdm->config = kvx_tx_readl(tdm->hw,
 				KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_CONFIG_OFFSET);
-	tdm->fcs = kvx_eth_tx_readl(tdm->hw,
+	tdm->fcs = kvx_tx_readl(tdm->hw,
 				KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_FCS_OFFSET);
-	tdm->err = kvx_eth_tx_readl(tdm->hw,
+	tdm->err = kvx_tx_readl(tdm->hw,
 				KVX_ETH_TX_TDM_GRP_OFFSET + KVX_ETH_TX_TDM_ERR_OFFSET);
 }
 void kvx_eth_tx_pfc_xoff_subsc_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_pfc_xoff_subsc_f *subsc)
@@ -152,7 +152,7 @@ void kvx_eth_tx_pfc_xoff_subsc_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_pf
 			KVX_ETH_TX_PFC_XOFF_SUBSCR_OFFSET +
 			KVX_ETH_TX_PFC_XOFF_SUBSCR_ELEM_SIZE * subsc->tgt_id;
 
-	kvx_eth_tx_writel(hw, subsc->xoff_subsc, off);
+	kvx_tx_writel(hw, subsc->xoff_subsc, off);
 }
 static void kvx_eth_tx_pfc_xoff_subsc_f_update(void *data)
 {
@@ -162,20 +162,20 @@ static void kvx_eth_tx_pfc_xoff_subsc_f_update(void *data)
 			KVX_ETH_TX_PFC_GRP_ELEM_SIZE * subsc->lane_id +
 			KVX_ETH_TX_PFC_XOFF_SUBSCR_OFFSET +
 			KVX_ETH_TX_PFC_XOFF_SUBSCR_ELEM_SIZE * subsc->tgt_id;
-	subsc->xoff_subsc = kvx_eth_tx_readl(subsc->hw, off);
+	subsc->xoff_subsc = kvx_tx_readl(subsc->hw, off);
 }
 void kvx_eth_tx_stage_two_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_stage_two_f *tx_stage_two)
 {
 	u32 off = KVX_ETH_TX_STAGE_TWO_GRP_OFFSET + KVX_ETH_TX_STAGE_TWO_GRP_ELEM_SIZE * tx_stage_two->lane_id;
 
-	kvx_eth_tx_writel(hw, tx_stage_two->drop_disable,
+	kvx_tx_writel(hw, tx_stage_two->drop_disable,
 			off + KVX_ETH_TX_STAGE_TWO_DROP_DISABLE_OFFSET);
 	/* MTU not writable via sysfs */
-	kvx_eth_tx_writel(hw, tx_stage_two->drop_cnt_mask,
+	kvx_tx_writel(hw, tx_stage_two->drop_cnt_mask,
 			off + KVX_ETH_TX_STAGE_TWO_DROP_CNT_MSK_OFFSET);
-	kvx_eth_tx_writel(hw, tx_stage_two->drop_cnt_subscr,
+	kvx_tx_writel(hw, tx_stage_two->drop_cnt_subscr,
 			off + KVX_ETH_TX_STAGE_TWO_DROP_CNT_SUBSCR_OFFSET);
-	kvx_eth_tx_writel(hw, tx_stage_two->drop_cnt,
+	kvx_tx_writel(hw, tx_stage_two->drop_cnt,
 			off + KVX_ETH_TX_STAGE_TWO_DROP_CNT_OFFSET);
 }
 static void kvx_eth_tx_stage_two_wmark_f_update(void *data)
@@ -184,7 +184,7 @@ static void kvx_eth_tx_stage_two_wmark_f_update(void *data)
 	u32 off = KVX_ETH_TX_STAGE_TWO_GRP_OFFSET + KVX_ETH_TX_STAGE_TWO_GRP_ELEM_SIZE * tx_wmark->lane_id +
 			KVX_ETH_TX_STAGE_TWO_WMARK_OFFSET + KVX_ETH_TX_STAGE_TWO_WMARK_ELEM_SIZE * tx_wmark->tgt_id;
 
-	tx_wmark->wmark = kvx_eth_tx_readl(tx_wmark->hw, off);
+	tx_wmark->wmark = kvx_tx_readl(tx_wmark->hw, off);
 }
 static void kvx_eth_tx_stage_two_drop_status_f_update(void *data)
 {
@@ -192,31 +192,31 @@ static void kvx_eth_tx_stage_two_drop_status_f_update(void *data)
 	u32 off = KVX_ETH_TX_STAGE_TWO_GRP_OFFSET + KVX_ETH_TX_STAGE_TWO_GRP_ELEM_SIZE * tx_drop_status->lane_id +
 			KVX_ETH_TX_STAGE_TWO_DROP_STATUS_OFFSET + KVX_ETH_TX_STAGE_TWO_DROP_STATUS_ELEM_SIZE * tx_drop_status->tgt_id;
 
-	tx_drop_status->drop_status = kvx_eth_tx_readl(tx_drop_status->hw, off);
+	tx_drop_status->drop_status = kvx_tx_readl(tx_drop_status->hw, off);
 }
 static void kvx_eth_tx_stage_two_f_update(void *data)
 {
 	struct kvx_eth_tx_stage_two_f *tx_stage_two = (struct kvx_eth_tx_stage_two_f *)data;
 	u32 off = KVX_ETH_TX_STAGE_TWO_GRP_OFFSET + KVX_ETH_TX_STAGE_TWO_GRP_ELEM_SIZE * tx_stage_two->lane_id;
 
-	tx_stage_two->drop_disable = kvx_eth_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_DISABLE_OFFSET);
-	tx_stage_two->mtu = kvx_eth_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_MTU_OFFSET);
-	tx_stage_two->drop_cnt_mask = kvx_eth_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_CNT_MSK_OFFSET);
-	tx_stage_two->drop_cnt_subscr = kvx_eth_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_CNT_SUBSCR_OFFSET);
-	tx_stage_two->drop_cnt = kvx_eth_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_CNT_OFFSET);
+	tx_stage_two->drop_disable = kvx_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_DISABLE_OFFSET);
+	tx_stage_two->mtu = kvx_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_MTU_OFFSET);
+	tx_stage_two->drop_cnt_mask = kvx_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_CNT_MSK_OFFSET);
+	tx_stage_two->drop_cnt_subscr = kvx_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_CNT_SUBSCR_OFFSET);
+	tx_stage_two->drop_cnt = kvx_tx_readl(tx_stage_two->hw, off  + KVX_ETH_TX_STAGE_TWO_DROP_CNT_OFFSET);
 }
 static void kvx_eth_tx_exp_npre_f_update(void *data)
 {
 	struct kvx_eth_tx_exp_npre_f *tx_exp_npre = (struct kvx_eth_tx_exp_npre_f *)data;
 	u32 off = KVX_ETH_TX_EXP_NPRE_GRP_OFFSET + KVX_ETH_TX_EXP_NPRE_GRP_ELEM_SIZE * tx_exp_npre->lane_id;
 
-	tx_exp_npre->config = kvx_eth_tx_readl(tx_exp_npre->hw, off  + KVX_ETH_TX_EXP_NPRE_CONFIG_OFFSET);
+	tx_exp_npre->config = kvx_tx_readl(tx_exp_npre->hw, off  + KVX_ETH_TX_EXP_NPRE_CONFIG_OFFSET);
 }
 void kvx_eth_tx_exp_npre_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_exp_npre_f *tx_exp_npre)
 {
 	u32 off = KVX_ETH_TX_EXP_NPRE_GRP_OFFSET + KVX_ETH_TX_EXP_NPRE_GRP_ELEM_SIZE * tx_exp_npre->lane_id;
 
-	kvx_eth_tx_writel(hw, tx_exp_npre->config,
+	kvx_tx_writel(hw, tx_exp_npre->config,
 			off + KVX_ETH_TX_EXP_NPRE_CONFIG_OFFSET);
 }
 static void kvx_eth_tx_pbrr_priority_f_update(void *data)
@@ -225,14 +225,14 @@ static void kvx_eth_tx_pbrr_priority_f_update(void *data)
 	u32 off = KVX_ETH_TX_PBRR_GRP_OFFSET + KVX_ETH_TX_PBRR_GRP_ELEM_SIZE * tx_pbrr_prio->lane_id +
 			KVX_ETH_TX_PBRR_PRIORITY_ELEM_SIZE * tx_pbrr_prio->tgt_id;
 
-	tx_pbrr_prio->priority = kvx_eth_tx_readl(tx_pbrr_prio->hw, off);
+	tx_pbrr_prio->priority = kvx_tx_readl(tx_pbrr_prio->hw, off);
 }
 void kvx_eth_tx_pbrr_priority_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_pbrr_priority_f *tx_pbrr_prio)
 {
 	u32 off = KVX_ETH_TX_PBRR_GRP_OFFSET + KVX_ETH_TX_PBRR_GRP_ELEM_SIZE * tx_pbrr_prio->lane_id +
 			KVX_ETH_TX_PBRR_PRIORITY_ELEM_SIZE * tx_pbrr_prio->tgt_id;
 
-	kvx_eth_tx_writel(hw, tx_pbrr_prio->priority, off);
+	kvx_tx_writel(hw, tx_pbrr_prio->priority, off);
 }
 static void kvx_eth_tx_pbdwrr_priority_f_update(void *data)
 {
@@ -241,7 +241,7 @@ static void kvx_eth_tx_pbdwrr_priority_f_update(void *data)
 			KVX_ETH_TX_PBDWRR_PRIORITY_OFFSET +
 			KVX_ETH_TX_PBDWRR_PRIORITY_ELEM_SIZE * tx_pbdwrr_prio->tgt_id;
 
-	tx_pbdwrr_prio->priority = kvx_eth_tx_readl(tx_pbdwrr_prio->hw, off);
+	tx_pbdwrr_prio->priority = kvx_tx_readl(tx_pbdwrr_prio->hw, off);
 }
 void kvx_eth_tx_pbdwrr_priority_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_pbdwrr_priority_f *tx_pbdwrr_prio)
 {
@@ -249,7 +249,7 @@ void kvx_eth_tx_pbdwrr_priority_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_p
 			KVX_ETH_TX_PBDWRR_PRIORITY_OFFSET +
 			KVX_ETH_TX_PBDWRR_PRIORITY_ELEM_SIZE * tx_pbdwrr_prio->tgt_id;
 
-	kvx_eth_tx_writel(hw, tx_pbdwrr_prio->priority, off);
+	kvx_tx_writel(hw, tx_pbdwrr_prio->priority, off);
 }
 static void kvx_eth_tx_pbdwrr_quantum_f_update(void *data)
 {
@@ -258,18 +258,18 @@ static void kvx_eth_tx_pbdwrr_quantum_f_update(void *data)
 			KVX_ETH_TX_PBDWRR_QUANTUM_OFFSET +
 			KVX_ETH_TX_PBDWRR_QUANTUM_ELEM_SIZE * tx_pbdwrr_quantum->tgt_id;
 
-	tx_pbdwrr_quantum->quantum = kvx_eth_tx_readl(tx_pbdwrr_quantum->hw, off);
+	tx_pbdwrr_quantum->quantum = kvx_tx_readl(tx_pbdwrr_quantum->hw, off);
 }
 void kvx_eth_tx_pbdwrr_quantum_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_pbdwrr_quantum_f *tx_pbdwrr_quantum)
 {
 	u32 grp_off = KVX_ETH_TX_PBDWRR_GRP_OFFSET + KVX_ETH_TX_PBDWRR_GRP_ELEM_SIZE * tx_pbdwrr_quantum->lane_id;
 
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_PBDWRR_INIT_QUANTUM_PROGRAM,
+	kvx_tx_writel(hw, KVX_ETH_TX_PBDWRR_INIT_QUANTUM_PROGRAM,
 		grp_off + KVX_ETH_TX_PBDWRR_INIT_QUANTUM_OFFSET);
-	kvx_eth_tx_writel(hw, tx_pbdwrr_quantum->quantum,
+	kvx_tx_writel(hw, tx_pbdwrr_quantum->quantum,
 		grp_off + KVX_ETH_TX_PBDWRR_QUANTUM_OFFSET +
 		KVX_ETH_TX_PBDWRR_QUANTUM_ELEM_SIZE * tx_pbdwrr_quantum->tgt_id);
-	kvx_eth_tx_writel(hw, KVX_ETH_TX_PBDWRR_INIT_QUANTUM_DONE,
+	kvx_tx_writel(hw, KVX_ETH_TX_PBDWRR_INIT_QUANTUM_DONE,
 		grp_off + KVX_ETH_TX_PBDWRR_INIT_QUANTUM_OFFSET);
 }
 static void kvx_eth_tx_pbdwrr_f_update(void *data)
@@ -277,13 +277,13 @@ static void kvx_eth_tx_pbdwrr_f_update(void *data)
 	struct kvx_eth_tx_pbdwrr_f *tx_pbdwrr = (struct kvx_eth_tx_pbdwrr_f *)data;
 	u32 grp_off = KVX_ETH_TX_PBDWRR_GRP_OFFSET + KVX_ETH_TX_PBDWRR_GRP_ELEM_SIZE * tx_pbdwrr->lane_id;
 
-	tx_pbdwrr->config = kvx_eth_tx_readl(tx_pbdwrr->hw, grp_off + KVX_ETH_TX_PBDWRR_CONFIG_OFFSET);
+	tx_pbdwrr->config = kvx_tx_readl(tx_pbdwrr->hw, grp_off + KVX_ETH_TX_PBDWRR_CONFIG_OFFSET);
 }
 void kvx_eth_tx_pbdwrr_f_cfg(struct kvx_eth_hw *hw, struct kvx_eth_tx_pbdwrr_f *tx_pbdwrr)
 {
 	u32 grp_off = KVX_ETH_TX_PBDWRR_GRP_OFFSET + KVX_ETH_TX_PBDWRR_GRP_ELEM_SIZE * tx_pbdwrr->lane_id;
 
-	kvx_eth_tx_writel(hw, tx_pbdwrr->config,
+	kvx_tx_writel(hw, tx_pbdwrr->config,
 		grp_off + KVX_ETH_TX_PBDWRR_CONFIG_OFFSET);
 }
 void kvx_eth_tx_f_init(struct kvx_eth_hw *hw)
