57b7bc098927c84e46dfbc0d97a068259cde49b1
Yann Sionneau <ysionneau@kalray.eu>
2023-06-19 16:04:11 +0200

    kvx: remove support for legacy syscalls
    Summary:
    Remove support for legacy syscalls.
    
    As asked by Arnd Bergmann
    
    Ref T20585
    
    Test Plan: CI
    
    Reviewers: O51 Linux Coolidge!
    
    Subscribers: jmaselbas, #linux_coolidge_cc
    
    Maniphest Tasks: T20585
    
    Differential Revision: https://phab.kalray.eu/D26735

--- arch/kvx/include/uapi/asm/unistd.h
+++ arch/kvx/include/uapi/asm/unistd.h
@@ -4,9 +4,6 @@
  * Author(s): Clement Leger
  */
 
-#define __ARCH_WANT_RENAMEAT
-#define __ARCH_WANT_NEW_STAT
-#define __ARCH_WANT_SET_GET_RLIMIT
 #define __ARCH_WANT_SYS_CLONE3
 
 #include <asm-generic/unistd.h>
