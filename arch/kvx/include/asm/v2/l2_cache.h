c401a739be4851f20426824407c8ccaead90acef
Florent Malassagne <fmalassagne@kalrayinc.com>
2023-10-04 17:46:49 +0200

    l2_cache.h: Fix l2_cache_XXX_range()
    Summary:
    -start variable is modified but also used as original
    -case in l2_cache_inval_range() if buffer size less than cache line size
    
    Ref T19776
    
    Test Plan: manual
    
    Reviewers: O51 Linux Coolidge, O39 ethernet, lmorhet
    
    Reviewed By: O51 Linux Coolidge, lmorhet
    
    Subscribers: ysionneau, lmorhet, #linux_coolidge_cc
    
    Maniphest Tasks: T19776
    
    Differential Revision: https://phab.kalray.eu/D31767

--- arch/kvx/include/asm/v2/l2_cache.h
+++ arch/kvx/include/asm/v2/l2_cache.h
@@ -17,13 +17,13 @@ extern struct static_key_false l2_enabled;
 static inline bool
 l2_cache_wbinval_range(phys_addr_t start, unsigned long size)
 {
-	phys_addr_t addr;
+	phys_addr_t addr = start;
 
 	if (static_branch_likely(&l2_enabled)) {
 		__builtin_kvx_fence();
 
-	start = ALIGN_DOWN(start, L2_CACHE_LINE_SIZE);
-	for (addr = start;
+	addr = ALIGN_DOWN(addr, L2_CACHE_LINE_SIZE);
+	for (;
 	     addr < (start + size - L2_CACHE_LINE_SIZE);
 	     addr += L2_CACHE_LINE_SIZE) {
 		writeq(addr,
@@ -45,13 +45,13 @@ l2_cache_wbinval_range(phys_addr_t start, unsigned long size)
 static inline bool
 l2_cache_wb_range(phys_addr_t start, unsigned long size)
 {
-	phys_addr_t addr;
+	phys_addr_t addr = start;
 
 	if (static_branch_likely(&l2_enabled)) {
 		__builtin_kvx_fence();
 
-		start = ALIGN_DOWN(start, L2_CACHE_LINE_SIZE);
-		for (addr = start;
+		addr = ALIGN_DOWN(addr, L2_CACHE_LINE_SIZE);
+		for (;
 		     addr < (start + size - L2_CACHE_LINE_SIZE);
 		     addr += L2_CACHE_LINE_SIZE) {
 			writeq(addr,
@@ -73,7 +73,8 @@ l2_cache_wb_range(phys_addr_t start, unsigned long size)
 static inline bool
 l2_cache_inval_range(phys_addr_t start, unsigned long size)
 {
-	phys_addr_t addr;
+	phys_addr_t addr = start;
+	bool force_purge_last_line = false;
 
 	if (static_branch_likely(&l2_enabled)) {
 		__builtin_kvx_fence();
@@ -82,17 +83,25 @@ l2_cache_inval_range(phys_addr_t start, unsigned long size)
 		 * If the start address is not aligned the first
 		 * line needs to be purged instead of invalidated
 		 */
-		if (!IS_ALIGNED(start, L2_CACHE_LINE_SIZE)) {
-			writeq(ALIGN_DOWN(start, L2_CACHE_LINE_SIZE),
-			       (void *)(kvx_debug_regs +
-			       KVX_DEBUG_REGS_L2CM_PURGE_BY_ADDR_NONBLOCKING));
-			/* Afterwards the start address needs to be adjusted */
-			start = ALIGN_DOWN(start, L2_CACHE_LINE_SIZE) +
-				L2_CACHE_LINE_SIZE;
+		if (!IS_ALIGNED(addr, L2_CACHE_LINE_SIZE)) {
+			addr = ALIGN_DOWN(addr, L2_CACHE_LINE_SIZE);
+			/* If there is only one line */
+			if ((addr + L2_CACHE_LINE_SIZE) >= (start + size)) {
+				/*
+				 * Only one line, will be treated next as last line
+				 * but this flag is used because end could be aligned
+				 */
+				force_purge_last_line = true;
+			} else {
+				writeq(addr,
+				       (void *)(kvx_debug_regs +
+				       KVX_DEBUG_REGS_L2CM_PURGE_BY_ADDR_NONBLOCKING));
+				/* Afterwards the start address needs to be adjusted */
+				addr += L2_CACHE_LINE_SIZE;
+			}
 		}
 
-			start = ALIGN_DOWN(start, L2_CACHE_LINE_SIZE);
-			for (addr = start;
+			for (;
 			     addr < (start + size - L2_CACHE_LINE_SIZE);
 			     addr += L2_CACHE_LINE_SIZE) {
 				writeq(addr,
@@ -104,7 +113,7 @@ l2_cache_inval_range(phys_addr_t start, unsigned long size)
 		 * Same for the last entry if the size is not aligned to L2
 		 * cache line size we need to purge to not lose entries
 		 */
-		if (IS_ALIGNED(size, L2_CACHE_LINE_SIZE)) {
+		if ((IS_ALIGNED(start + size, L2_CACHE_LINE_SIZE)) && (!force_purge_last_line)) {
 			writeq(addr,
 			       (void *)(kvx_debug_regs +
 			       KVX_DEBUG_REGS_L2CM_INVAL_BY_ADDR_BLOCKING));
