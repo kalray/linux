b823a422c5306c73f420ba695ed11ec89c44b9ca
Ashley Lesdalons <ashley.lesdalons@kalray.eu>
2023-11-20 15:27:08 +0100

    kvx qsfp: wait for eeprom cache init using completion
    Summary:
    In some cases, the driver needs to read some data from the eeprom cache
    before the initialization of the latter. The current solution used is to
    check the value of the first byte of the cache, but this is not enough.
    It does not protect from reading the cache while the cache is being
    initialized. Therefore, this commit implements a completion in order to
    make sure the eeprom cache is fully initialized before reading it.
    eeprom_cache_readb() is removed in order to promote direct cache access.
    
    Ref T21631
    
    Test Plan: CI
    
    Reviewers: O129 linux drivers
    
    Maniphest Tasks: T21631
    
    Differential Revision: https://phab.kalray.eu/D33945

--- drivers/net/ethernet/kalray/kvx-qsfp.h
+++ drivers/net/ethernet/kalray/kvx-qsfp.h
@@ -26,6 +26,7 @@
 #define EEPROM_PAGE_SIZE                    128
 #define EEPROM_PAGE0_SIZE                   256
 #define WAIT_STATE_READY_TIMEOUT_MS         100
+#define WAIT_EEPROM_INIT_TIMEOUT_MS         200
 
 #define SFP_IDENTIFIER_OFFSET               0
 #define SFP_PAGE_OFFSET                     0x7f
@@ -149,7 +150,7 @@
 		int ret = kvx_qsfp_eeprom_refresh(qsfp, qsfp_eeprom_offset(m), \
 						  len);                        \
 		if (ret != len)                                                \
-			dev_err(qsfp->dev, "eeprom read failure: ##m##\n");    \
+			dev_err(qsfp->dev, "eeprom read failure: %s\n", #m);   \
 		ret == len ? 0 : -EINVAL;                                      \
 	})
 #define qsfp_write_eeprom(qsfp, data, m)                                       \
@@ -158,7 +159,7 @@
 		int ret = kvx_qsfp_eeprom_write(qsfp, data,                    \
 						qsfp_eeprom_offset(m), len);   \
 		if (ret != len)                                                \
-			dev_err(qsfp->dev, "eeprom write failure: ##mm##\n");  \
+			dev_err(qsfp->dev, "eeprom write failure: %s\n", #m);  \
 		ret == qsfp_eeprom_len(m) ? 0 : -EINVAL;                       \
 	})
 #define qsfp_update_eeprom(qsfp, m)                                            \
@@ -304,6 +305,23 @@ struct kvx_qsfp_options {
 	u8 offset2;
 } __packed;
 
+struct kvx_qsfp_spec_compliance {
+	u8 ethernet;
+	u8 sonet;
+	u8 sas_sata;
+	u8 gigabit_ethernet;
+	u8 fibre_channel[4];
+} __packed;
+
+struct kvx_qsfp_ctrl_options_ad {
+	u8 max_equalizer_emphasis;
+	u8 rxout_amplitude;
+	u8 reserved;
+	u8 txdis_rxlosl_cap;
+	u8 max_tc_stabilization_time;
+	u8 max_ctle_settling_time;
+} __packed;
+
 struct kvx_qsfp_eeprom_cache {
 	/* lower page 0 */
 	u8 id;
@@ -315,7 +333,8 @@ struct kvx_qsfp_eeprom_cache {
 	u8 reserved0[4];
 	struct kvx_qsfp_control control;
 	struct kvx_qsfp_device_flags_mask int_flags_mask;
-	u8 device_properties0[4];
+	u8 max_power_consumption;
+	u8 device_properties0[3];
 	u8 pcie[2];
 	u8 device_properties1[4];
 	u8 reserved1[2];
@@ -326,7 +345,21 @@ struct kvx_qsfp_eeprom_cache {
 	/* upper page 0 */
 	u8 identifier;
 	u8 ext_identifier;
-	u8 base_id[62];
+	u8 connector_type;
+	struct kvx_qsfp_spec_compliance spec_compliance;
+	u8 encoding;
+	u8 signaling_rate_nominal;
+	u8 ext_rate_select_comp;
+	u8 length[5];
+	u8 device_tech;
+	u8 vendor_name[16];
+	u8 ext_module;
+	u8 vendor_oui[3];
+	u8 vendor_pn[16];
+	u8 vendor_rev[2];
+	u8 wavelength_attenuation[4];
+	u8 max_case_temp;
+	u8 cc_base;
 	u8 link_codes;
 	struct kvx_qsfp_options options;
 	u8 vendor_sn[16];
@@ -346,7 +379,7 @@ struct kvx_qsfp_eeprom_cache {
 	/* page 03h (optional) */
 	u8 device_thresholds[48];
 	u8 channel_thresholds[48];
-	u8 txeq_rxout[6];
+	struct kvx_qsfp_ctrl_options_ad crtl_options_ad;
 	u8 channel_controls[12];
 	struct kvx_qsfp_channel_flags_mask channel_monitor_mask;
 	u8 reserved2[4];
@@ -370,6 +403,7 @@ struct kvx_qsfp_eeprom_cache {
  * @irq_event_task: task for running IRQ events
  * @sm_state: module current state
  * @sm_s_ready: completion for state QSFP_S_READY
+ * @eeprom_init: completion for eeprom cache initialization
  * @irq_event: used to notify @irq_event_task of an IRQ event
  * @cable_connected: true if qsfp cable is plugged in
  * @opt_features: optionnal features
@@ -389,7 +423,7 @@ struct kvx_qsfp {
 	struct work_struct sm_task;
 	struct delayed_work irq_event_task;
 	u8 sm_state;
-	struct completion sm_s_ready;
+	struct completion sm_s_ready, eeprom_init;
 	unsigned long irq_event;
 	bool cable_connected;
 	struct kvx_qsfp_opt_features opt_features;
