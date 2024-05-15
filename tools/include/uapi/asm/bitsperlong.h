dfdfa7f7a426f0c97380a89b16a6544d18baa040
Yann Sionneau <ysionneau@kalrayinc.com>
2024-05-15 17:37:49 +0200

    kvx: Add other common headers
    Add some other common headers for basic kvx support.
    
    Co-developed-by: Clement Leger <clement@clement-leger.fr>
    Signed-off-by: Clement Leger <clement@clement-leger.fr>
    Co-developed-by: Julian Vetter <jvetter@kalrayinc.com>
    Signed-off-by: Julian Vetter <jvetter@kalrayinc.com>
    Co-developed-by: Vincent Chardon <vincent.chardon@elsys-design.com>
    Signed-off-by: Vincent Chardon <vincent.chardon@elsys-design.com>
    Signed-off-by: Yann Sionneau <ysionneau@kalrayinc.com>
    ---
    
    Notes:
    
    V2 -> V3: remove __rm_firmware_regs_start declaration.

--- tools/include/uapi/asm/bitsperlong.h
+++ tools/include/uapi/asm/bitsperlong.h
@@ -13,6 +13,8 @@
 #include "../../../arch/ia64/include/uapi/asm/bitsperlong.h"
 #elif defined(__alpha__)
 #include "../../../arch/alpha/include/uapi/asm/bitsperlong.h"
+#elif defined(__kvx__)
+#include "../../../arch/kvx/include/uapi/asm/bitsperlong.h"
 #else
 #include <asm-generic/bitsperlong.h>
 #endif
