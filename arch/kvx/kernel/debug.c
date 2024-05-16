cb5295d499b59b9acf20a4084db74187eb0aaeab
Yann Sionneau <ysionneau@kalrayinc.com>
2024-05-16 16:55:47 +0200

    kvx: Add debugging related support
    Add kvx support for debugging
    
    Co-developed-by: Clement Leger <clement@clement-leger.fr>
    Signed-off-by: Clement Leger <clement@clement-leger.fr>
    Co-developed-by: Guillaume Thouvenin <thouveng@gmail.com>
    Signed-off-by: Guillaume Thouvenin <thouveng@gmail.com>
    Co-developed-by: Julian Vetter <jvetter@kalrayinc.com>
    Signed-off-by: Julian Vetter <jvetter@kalrayinc.com>
    Co-developed-by: Marius Gligor <mgligor@kalrayinc.com>
    Signed-off-by: Marius Gligor <mgligor@kalrayinc.com>
    Signed-off-by: Yann Sionneau <ysionneau@kalrayinc.com>
    ---
    
    Notes:
    V1 -> V2: No changes
    V2 -> V3:
    - remove useless do { } while (0) for one-line macro
    - change function arg order related to struct pt_regs
      because of 'generic entry' usage
    - improved comments for patch_insns_percpu

---
+++ arch/kvx/kernel/debug.c
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2017-2023 Kalray Inc.
+ * Author(s): Clement Leger
+ */
+
+#include <linux/entry-common.h>
+#include <linux/list.h>
+#include <linux/rculist.h>
+#include <linux/spinlock.h>
+#include <linux/rcupdate.h>
+
+#include <asm/debug.h>
+
+static DEFINE_SPINLOCK(debug_hook_lock);
+static LIST_HEAD(user_debug_hook);
+static LIST_HEAD(kernel_debug_hook);
+
+static struct list_head *debug_hook_list(bool user_mode)
+{
+	return user_mode ? &user_debug_hook : &kernel_debug_hook;
+}
+
+void debug_handler(struct pt_regs *regs, u64 ea)
+{
+	int ret;
+	struct debug_hook *hook;
+	struct list_head *list = debug_hook_list(user_mode(regs));
+
+	list_for_each_entry_rcu(hook, list, node) {
+		ret = hook->handler(regs, ea);
+		if (ret == DEBUG_HOOK_HANDLED)
+			return;
+	}
+
+	panic("Entered debug but no requester !");
+}
+
+void debug_hook_register(struct debug_hook *dbg_hook)
+{
+	struct list_head *list = debug_hook_list(dbg_hook->mode == MODE_USER);
+
+	spin_lock(&debug_hook_lock);
+	list_add_rcu(&dbg_hook->node, list);
+	spin_unlock(&debug_hook_lock);
+}
+
+void debug_hook_unregister(struct debug_hook *dbg_hook)
+{
+	spin_lock(&debug_hook_lock);
+	list_del_rcu(&dbg_hook->node);
+	spin_unlock(&debug_hook_lock);
+	synchronize_rcu();
+}
